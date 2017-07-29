7ed95eeed0088395c48e4a7ad63aad97ed215ccd
tom <tom@leoedu.com>
2017-07-29 16:42:48 +0800

    fix

--- app/Config/menu.php
+++ app/Config/menu.php
@@ -22,7 +22,7 @@ class menu{
                     ["power_id"=>10, "name"=>"转介绍列表",   "url"=>"/agent/agent_list"],
                     ["power_id"=>8, "name"=>"转介绍订单",   "url"=>"/agent/agent_order_list"],
                     ["power_id"=>11, "name"=>"转介绍提现列表",   "url"=>"/agent/agent_cash_list"],
-                    ["power_id"=>13, "name"=>"test",   "url"=>"/seller_student_new2/test_lesson_plan_list_new"],
+                    ["power_id"=>13, "name"=>"test",   "url"=>"/agent/check"],
                 ]
                 ],
             ]
