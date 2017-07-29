bc2a712285101c5ffefa4bf9fb447cf6b15b88b9
tom <tom@leoedu.com>
2017-07-29 14:13:38 +0800

    fix

--- app/Wx/Yxyx/WechatRequest.php
+++ app/Wx/Yxyx/WechatRequest.php
@@ -148,21 +148,34 @@ class WechatRequest  {
      * @return array
      */
     public static function text(&$request){
-//         $content = self::unicode2utf8('\ue032')."你来啦，真好。".self::unicode2utf8('\ue032')."
+         $content = $request['content'];
+        if($content == '优学优享'){
+            $tuwenList[] = array(
+                'title' => '理优1对1用户指南',
+                'description' => '',
+                'pic_url' => 'http://7u2f5q.com2.z0.glb.qiniucdn.com/0db16ea2f7fe8bea4d08d39dcd90478e1501039384123.jpg',
+                'url' => 'http://admin.yb1v1.com/article_wx/leo_yxyx_guide',
+            );
+            $item = array();
+            foreach($tuwenList as $tuwen){
+                $item[] = ResponsePassive::newsItem($tuwen['title'], $tuwen['description'], $tuwen['pic_url'], $tuwen['url']);
+            }
+            return  ResponsePassive::news($request['fromusername'], $request['tousername'], $item);
+        }else{
+            $content = self::unicode2utf8('\ue032')."你来啦，真好。".self::unicode2utf8('\ue032')."
 
-// 【分享】它使快乐增大，它使悲伤减小。
+【分享】它使快乐增大，它使悲伤减小。
 
-// 【菜单栏功能介绍】
-// -[我要邀请]-:生成邀请海报。
-// -[理优教育]-:内有理优简介、精品内容、学员反馈、每日卡片。
-// -[账号管理]-:
-// (1)绑定账号：绑定手机号即可开启优学优享功能。
-// (2)个人中心：查询用户等级、邀请人数、奖励情况等。
-// (3)常见问题：自助解决问题。
-// ";
-        $content = $request['content'];
-        return ResponsePassive::text($request['fromusername'], $request['tousername'], $content);
-
+【菜单栏功能介绍】
+-[我要邀请]-:生成邀请海报。
+-[理优教育]-:内有理优简介、精品内容、学员反馈、每日卡片。
+-[账号管理]-:
+(1)绑定账号：绑定手机号即可开启优学优享功能。
+(2)个人中心：查询用户等级、邀请人数、奖励情况等。
+(3)常见问题：自助解决问题。
+";
+            return ResponsePassive::text($request['fromusername'], $request['tousername'], $content);
+        }
     }
 
     /**
