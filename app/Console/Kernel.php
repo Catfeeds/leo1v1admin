6d2909622458c081e33624a0bd485aba83bd50f3
jim <jim@leoedu.com>
2017-08-02 14:41:27 +0800

    fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,6 +17,8 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         // Commands\Inspire::class,
+        //command:check_test_lesson_succ_flag_for_send_wx
+        Commands\check_test_lesson_succ_flag_for_send_wx::class,
         Commands\reset_lesson_online_user_status_by_stroke_time::class,
         Commands\update_lesson_call_time::class,
         Commands\todo_reset::class,
--- public/page_ts/teacher_level/teacher_lesson_record_info.ts
+++ public/page_ts/teacher_level/teacher_lesson_record_info.ts
@@ -78,6 +78,66 @@ $(function(){
         });
     });
 
+    $(".opt-first-lesson-record").on("click",function(){
+        var opt_data = $(this).get_opt_data();
+        $.do_ajax( "/teacher_level/get_teacher_test_lesson_info_record",{
+            "teacherid" :opt_data.teacherid,
+            "num":0
+        },function(resp){
+            var ret = resp.ret;
+            if(ret ==-1){
+                alert(resp.info);
+                return;
+            }else{
+                var lessonid = resp.data;
+                $.ajax({
+                    type     : "post",
+                    url      : "/tea_manage/get_lesson_reply",
+                    dataType : "json",
+                    data     : {"lessonid":lessonid},
+                    success  : function(result){
+                        if(result.ret == 0){
+                            console.log("http://admin.yb1v1.com/player/playback.html?draw="+encodeURIComponent(result.draw_url)
+                                        +"&audio="+encodeURIComponent(result.audio_url)
+                                        +"&start="+result.real_begin_time);
+                            if ( false && !$.check_in_phone() ) {
+
+                                // console.log("http://admin.yb1v1.com/player/playback.html?draw="+encodeURIComponent(result.draw_url)
+                                //             +"&audio="+encodeURIComponent(result.audio_url)
+                                //             +"&start="+result.real_begin_time);
+                                window.open("http://admin.yb1v1.com/player/playback.html?draw="+encodeURIComponent(result.draw_url)
+                                            +"&audio="+encodeURIComponent(result.audio_url)
+                                            +"&start="+result.real_begin_time,"_blank");
+                            }else{
+
+                                var w = $.check_in_phone()?329 : 558;
+                                var h = w/4*3;
+                                var html_node = $("<div style=\"text-align:center;\"> "
+                                                  +"<div id=\"drawing_list\" style=\"width:100%\">"
+                                                  +"</div><audio preload=\"none\"></audio></div>"
+                                                 );
+                                BootstrapDialog.show({
+                                    title    : '课程回放:lessonid:'+lessonid+", 学生:" + result.stu_nick,
+                                    message  : html_node,
+                                    closable : true,
+                                    onhide   : function(dialogRef){
+                                    }
+                                });
+                                Cwhiteboard = get_new_whiteboard(html_node.find("#drawing_list"));
+                                Cwhiteboard.loadData(w,h,result.real_begin_time,result.draw_url,result.audio_url,html_node);
+                            }
+                        }else{
+                            BootstrapDialog.alert(result.info);
+                        }
+                    }
+                });
+            }
+
+            
+
+        });
+    });
+
 
     $(".opt-fifth-lesson-video").on("click",function(){
         var opt_data = $(this).get_opt_data();
