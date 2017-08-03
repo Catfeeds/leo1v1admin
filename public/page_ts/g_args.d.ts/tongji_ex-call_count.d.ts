74a74fbc4e271a58e3bbb540b119b4a134e7017b
jim <xcwenn@qq.comm>
2017-08-03 11:08:13 +0800

    fix

---
+++ public/page_ts/g_args.d.ts/tongji_ex-call_count.d.ts
@@ -0,0 +1,71 @@
+interface GargsStatic {
+	date_type_config:	string;
+	date_type:	number;
+	opt_date_type:	number;
+	start_time:	string;
+	end_time:	string;
+	sys_invaild_flag:	number;//\App\Enums\Eboolean
+}
+declare module "g_args" {
+    export = g_args;
+}
+declare var g_args: GargsStatic;
+declare var g_account: string;
+declare var g_account_role: any;
+declare var g_adminid: any;
+interface RowData {
+	call_count	:any;
+	user_count	:any;
+}
+
+/*
+
+tofile: 
+	 mkdir -p ../tongji_ex; vi  ../tongji_ex/call_count.ts
+
+/// <reference path="../common.d.ts" />
+/// <reference path="../g_args.d.ts/tongji_ex-call_count.d.ts" />
+
+$(function(){
+    function load_data(){
+        $.reload_self_page ( {
+			date_type_config:	$('#id_date_type_config').val(),
+			date_type:	$('#id_date_type').val(),
+			opt_date_type:	$('#id_opt_date_type').val(),
+			start_time:	$('#id_start_time').val(),
+			end_time:	$('#id_end_time').val(),
+			sys_invaild_flag:	$('#id_sys_invaild_flag').val()
+        });
+    }
+
+	Enum_map.append_option_list("boolean",$("#id_sys_invaild_flag"));
+
+    $('#id_date_range').select_date_range({
+        'date_type' : g_args.date_type,
+        'opt_date_type' : g_args.opt_date_type,
+        'start_time'    : g_args.start_time,
+        'end_time'      : g_args.end_time,
+        date_type_config : JSON.parse( g_args.date_type_config),
+        onQuery :function() {
+            load_data();
+        }
+    });
+	$('#id_sys_invaild_flag').val(g_args.sys_invaild_flag);
+
+
+	$('.opt-change').set_input_change_event(load_data);
+});
+
+
+
+*/
+/* HTML ...
+
+        <div class="col-xs-6 col-md-2">
+            <div class="input-group ">
+                <span class="input-group-addon">boolean</span>
+                <select class="opt-change form-control" id="id_sys_invaild_flag" >
+                </select>
+            </div>
+        </div>
+*/
