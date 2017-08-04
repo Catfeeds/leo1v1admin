b28a6a0adf3c53f8d1b76e62935123cd92cb9218
jim <jim@leoedu.com>
2017-08-04 18:49:02 +0800

    fix

--- app/Models/t_parent_luck_draw_in_wx.php
+++ app/Models/t_parent_luck_draw_in_wx.php
@@ -11,7 +11,15 @@ class t_parent_luck_draw_in_wx extends \App\Models\Zgen\z_t_parent_luck_draw_in_
     public function get_prize_code_list($price){
         $where_arr = [
             ["price=%d",$price],
+            "receive_time<>''"
         ];
+
+        $sql = $this->gen_sql_new(" select prize_code from %s pl ".
+                                  " where %s",
+                                  self::DB_TABLE_NAME
+        );
+
+        return $this->main_get_row($sql);
     }
 
 }
