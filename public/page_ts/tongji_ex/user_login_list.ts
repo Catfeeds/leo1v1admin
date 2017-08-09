1daae84fd061dc99914dd4bc4e1737bfdbdfbe1d
jim <xcwenn@qq.comm>
2017-08-09 14:14:38 +0800

    fix

---
+++ public/page_ts/tongji_ex/user_login_list.ts
@@ -0,0 +1,35 @@
+/// <reference path="../common.d.ts" />
+/// <reference path="../g_args.d.ts/tongji_ex-user_login_list.d.ts" />
+
+$(function(){
+    function load_data(){
+        $.reload_self_page ( {
+			date_type_config:	$('#id_date_type_config').val(),
+			date_type:	$('#id_date_type').val(),
+			opt_date_type:	$('#id_opt_date_type').val(),
+			start_time:	$('#id_start_time').val(),
+			end_time:	$('#id_end_time').val(),
+			ip:	$('#id_ip').val(),
+			userid:	$('#id_userid').val()
+        });
+    }
+
+
+    $('#id_date_range').select_date_range({
+        'date_type' : g_args.date_type,
+        'opt_date_type' : g_args.opt_date_type,
+        'start_time'    : g_args.start_time,
+        'end_time'      : g_args.end_time,
+        date_type_config : JSON.parse( g_args.date_type_config),
+        onQuery :function() {
+            load_data();
+        }
+    });
+	$('#id_ip').val(g_args.ip);
+	$('#id_userid').val(g_args.userid);
+
+
+	$('.opt-change').set_input_change_event(load_data);
+});
+
+
