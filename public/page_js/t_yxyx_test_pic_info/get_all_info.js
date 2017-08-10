12bd79c8187506fa19adbcf68becc48b34f9cc8b
boby <boby@leoedu.com>
2017-08-10 19:24:41 +0800

    fix

--- public/page_js/t_yxyx_test_pic_info/get_all_info.js
+++ public/page_js/t_yxyx_test_pic_info/get_all_info.js
@@ -198,9 +198,9 @@ $(function(){
     });
     var pic_num = 1;
     function add_next_pic() {
-        $('#id_container_add').empty();
+        $('#id_container_add_tmp').empty();
         var new_input = '<input id="id_upload_add" value="上传第'+pic_num+'张图片" class="btn btn-primary add_pic_img" style="margin-bottom:5px;" type="button"/>';
-        $('#id_container_add').append(new_input);
+        $('#id_container_add_tmp').append(new_input);
         pic_num++;
     }
 });
