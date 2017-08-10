93f33b1389dbf5b53dbd81f8cee1544c4309a015
tom <tom@leoedu.com>
2017-08-10 16:36:31 +0800

    fix

--- app/Http/Controllers/agent.php
+++ app/Http/Controllers/agent.php
@@ -56,31 +56,6 @@ class agent extends Controller
         $type      = $this->get_in_int_val('type');
         $page_info = $this->get_in_page_info();
         $ret_info  = $this->t_agent->get_agent_info_new($page_info,$type);
-        // $data_map  = &$ret_info["list"];
-        // //试听信息
-        // $this->t_test_lesson_subject_require->switch_tongji_database();
-        // $test_lesson_list=$this->t_test_lesson_subject_require->tongji_test_lesson_origin_new();
-        // foreach ($test_lesson_list as  $test_item ) {
-        //     $check_value=$test_item["check_value"];
-        //     \App\Helper\Utils:: array_item_init_if_nofind( $data_map, $check_value,["check_value" => $check_value] );
-        //     $data_map[$check_value]["test_lesson_count"] = $test_item["test_lesson_count"];
-        //     $data_map[$check_value]["succ_test_lesson_count"] = $test_item["succ_test_lesson_count"];
-        // }
-        // $require_list=$this->t_test_lesson_subject_require->tongji_require_count_origin( $field_name,$start_time,$end_time,$adminid_list,$tmk_adminid,$origin_ex);
-        // foreach ($require_list as  $item ) {
-        //     $check_value=$item["check_value"];
-        //     \App\Helper\Utils:: array_item_init_if_nofind( $data_map, $check_value,["check_value" => $check_value] );
-        //     $data_map[$check_value]["require_count"] = $item["require_count"];
-        // }
-
-
-        if($type == 14){
-
-        }elseif($type == 15){
-
-        }elseif($type = 16){
-
-        }
         $userid_arr = [];
         foreach($ret_info['list'] as &$item){
             if($item['type'] == 1){
