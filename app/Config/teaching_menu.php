c9d4e7c8d2ded2a91811dbf23c3aad034a0102a6
jim <jim@leoedu.com>
2017-08-10 16:10:04 +0800

    fix

--- app/Config/teaching_menu.php
+++ app/Config/teaching_menu.php
@@ -77,7 +77,7 @@ class teaching_menu{
                 // ["power_id"=>2, "name"=>"兼职老师晋升-总监","url"=>"/teacher_level/get_teacher_advance_info"],
                 ["power_id"=>3, "name"=>"课时统计",   "url"=>"/tongji/test_lesson_ass_jy"],
                 ["power_id"=>22, "name"=>"助教换老师统计",   "url"=>"/tongji_ss/get_change_teacher_info"],
-                ["power_id"=>5, "name"=>"老师/助教退费责任统计","url"=>"/tongji_ss/get_refund_teacher_and_ass_info"],
+                ["power_id"=>23, "name"=>"老师/助教退费责任统计","url"=>"/tongji_ss/get_refund_teacher_and_ass_info"],
                 ["power_id"=>4, "name"=>"兼职老师上课考勤",   "url"=>"/tongji_ss/tongji_change_lesson_by_teacher_jy"],
 
             ]],
