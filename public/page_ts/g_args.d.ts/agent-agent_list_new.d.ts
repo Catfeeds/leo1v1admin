974de2f9e2298d2016e32528366e743ebf66c99d
tom <tom@leoedu.com>
2017-08-10 14:28:38 +0800

    fix

--- public/page_ts/g_args.d.ts/agent-agent_list_new.d.ts
+++ public/page_ts/g_args.d.ts/agent-agent_list_new.d.ts
@@ -1,13 +1,4 @@
 interface GargsStatic {
-	date_type_config:	string;
-	date_type:	number;
-	opt_date_type:	number;
-	start_time:	string;
-	end_time:	string;
-	userid:	number;
-	phone:	string;
-	parentid:	number;
-	agent_type:	number;
 	page_num:	number;
 	page_count:	number;
 }
@@ -57,33 +48,11 @@ tofile:
 $(function(){
     function load_data(){
         $.reload_self_page ( {
-			date_type_config:	$('#id_date_type_config').val(),
-			date_type:	$('#id_date_type').val(),
-			opt_date_type:	$('#id_opt_date_type').val(),
-			start_time:	$('#id_start_time').val(),
-			end_time:	$('#id_end_time').val(),
-			userid:	$('#id_userid').val(),
-			phone:	$('#id_phone').val(),
-			parentid:	$('#id_parentid').val(),
-			agent_type:	$('#id_agent_type').val()
+
         });
     }
 
 
-    $('#id_date_range').select_date_range({
-        'date_type' : g_args.date_type,
-        'opt_date_type' : g_args.opt_date_type,
-        'start_time'    : g_args.start_time,
-        'end_time'      : g_args.end_time,
-        date_type_config : JSON.parse( g_args.date_type_config),
-        onQuery :function() {
-            load_data();
-        }
-    });
-	$('#id_userid').val(g_args.userid);
-	$('#id_phone').val(g_args.phone);
-	$('#id_parentid').val(g_args.parentid);
-	$('#id_agent_type').val(g_args.agent_type);
 
 
 	$('.opt-change').set_input_change_event(load_data);
@@ -93,32 +62,4 @@ $(function(){
 
 */
 /* HTML ...
-
-        <div class="col-xs-6 col-md-2">
-            <div class="input-group ">
-                <span class="input-group-addon">userid</span>
-                <input class="opt-change form-control" id="id_userid" />
-            </div>
-        </div>
-
-        <div class="col-xs-6 col-md-2">
-            <div class="input-group ">
-                <span class="input-group-addon">phone</span>
-                <input class="opt-change form-control" id="id_phone" />
-            </div>
-        </div>
-
-        <div class="col-xs-6 col-md-2">
-            <div class="input-group ">
-                <span class="input-group-addon">parentid</span>
-                <input class="opt-change form-control" id="id_parentid" />
-            </div>
-        </div>
-
-        <div class="col-xs-6 col-md-2">
-            <div class="input-group ">
-                <span class="input-group-addon">agent_type</span>
-                <input class="opt-change form-control" id="id_agent_type" />
-            </div>
-        </div>
 */
