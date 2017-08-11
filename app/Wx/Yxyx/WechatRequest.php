04a70326613a06e227a553d98dbdc4447ca91853
tom <tom@leoedu.com>
2017-08-11 19:14:35 +0800

    fix

--- app/Wx/Yxyx/WechatRequest.php
+++ app/Wx/Yxyx/WechatRequest.php
@@ -588,7 +588,7 @@ class WechatRequest  {
         //获取该分类的信息
         $eventKey = $request['eventkey'];
         if($eventKey == 'http://www.leo1v1.com/wx-invite-article/index.html'){
-            \App\Helper\Utils::logger('yxyx_bbb');
+            \App\Helper\Utils::logger('yxyx_ccc');
             $openid = $request['fromusername'];
             $t_agent = new \App\Models\t_agent();
             $agent = $t_agent->get_agent_info_by_openid($openid);
@@ -596,10 +596,10 @@ class WechatRequest  {
                 $phone = $agent['phone'];
                 $eventKey.="?p_phone=$phone";
             }
+            \App\Helper\Utils::logger('yxyx_ccc_key:'.$evenKey);
         }
-        \App\Helper\Utils::logger('yxyx_bbb_key:'.$evenKey);
         $content = '收到跳转链接事件，您设置的key是' . $eventKey;
-        \App\Helper\Utils::logger('tiaozhuan111'.$eventKey);
+        \App\Helper\Utils::logger('tiaozhuan'.$eventKey);
 
         return ResponsePassive::text($request['fromusername'], $request['tousername'], $content);
     }
