b0afaa568eb2fcac3c7a33581cd94c1e6bff63b0
boby <boby@leoedu.com>
2017-08-11 17:54:55 +0800

    fix

--- public/page_ts/t_yxyx_test_pic_info/get_all_info.ts
+++ public/page_ts/t_yxyx_test_pic_info/get_all_info.ts
@@ -83,7 +83,7 @@ $(function(){
             closable        : true,
             closeByBackdrop : false,
             onshown         : function(dialog){
-                if(pic_num < 10) {
+                if(pic_num < 9) {
                     custom_qiniu_upload ("id_upload_add_tmp","id_container_add_tmp",
                                          g_args.qiniu_upload_domain_url , true,
                                          function (up, info, file){
@@ -248,7 +248,7 @@ $(function(){
         $('#id_container_add_tmp').empty();
         var new_input = '<input id="id_upload_add_tmp" value="已'+pic_num+'张图片" class="btn btn-primary add_pic_img" style="margin-bottom:5px;" type="button"/>';
         $('#id_container_add_tmp').append(new_input);
-        if (pic_num < 9) {
+        if (pic_num < 10) {
             custom_qiniu_upload ("id_upload_add_tmp","id_container_add_tmp",
                                  g_args.qiniu_upload_domain_url , true,
                                  function (up, info, file){
