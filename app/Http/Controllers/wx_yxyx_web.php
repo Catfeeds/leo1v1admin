3e0a6b74d538c49e5686ac77643802ee97203bd1
tom <tom@leoedu.com>
2017-08-11 21:14:20 +0800

    fix

--- app/Http/Controllers/wx_yxyx_web.php
+++ app/Http/Controllers/wx_yxyx_web.php
@@ -22,13 +22,13 @@ class wx_yxyx_web extends Controller
         parent::__construct();
         $agent_id = $this->get_agent_id();
         if($agent_id){
+            \App\Helper\Utils::logger('yxyx_xxx_id'.$agent_id);
             $agent = $this->t_agent->get_agent_info_by_id($agent_id);
             $web_html_url="http://wx-yxyx-web.leo1v1.com";
             $to_url      = $this->get_in_str_val("_url");
             $get_url_arr = preg_split("/\//", $to_url);
             $action      = $get_url_arr[2];
             $url = "$web_html_url/$action.html";
-            \App\Helper\Utils::logger('yxyx_xxx_id'.$agent['id']);
             if($action == 'bind' or !isset($agent['id'])){
                 $url = "$web_html_url/index.html#bind";
             }
