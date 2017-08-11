17f1ac52ed479fad7c1efddd4d85f9bec9582820
tom <tom@leoedu.com>
2017-08-11 21:19:07 +0800

    fix

--- app/Models/t_agent.php
+++ app/Models/t_agent.php
@@ -182,7 +182,7 @@ class t_agent extends \App\Models\Zgen\z_t_agent
 
     public function get_agent_info_by_id($id){
         $where_arr = array();
-        $this->where_arr_add_str_field($where_arr,"id",$id);
+        $this->where_arr_add_int_field($where_arr,"id",$id);
 
         $sql=$this->gen_sql_new ("select * "
                                  ." from %s where %s "
