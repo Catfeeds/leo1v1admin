07f34ab4e5b09a79f0cfe83a6b04fb2975da6b74
boby <boby@leoedu.com>
2017-08-11 16:11:16 +0800

    fix

--- resources/views/t_yxyx_test_pic_info/get_all_info.blade.php
+++ resources/views/t_yxyx_test_pic_info/get_all_info.blade.php
@@ -126,6 +126,7 @@
                         </div>
                         <div class="add_header_img"></div>
                         <div class="add_pic" style="display:none"></div>
+                        <span class="mark"></span>
                     </td>
 		            </tr>
 		        </tbody>
