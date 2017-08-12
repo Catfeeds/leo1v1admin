a18e4731465ad20f0be6ec031b7dfb3e4048ea36
boby <boby@leoedu.com>
2017-08-12 14:16:07 +0800

    fix

--- public/page_js/t_yxyx_test_pic_info/get_all_info.js
+++ public/page_js/t_yxyx_test_pic_info/get_all_info.js
@@ -92,7 +92,7 @@ $(function(){
         fun_str = fun_str + '<script> function set_up(obj){var id = $(obj).attr("data_id"); var new_id = parseInt(id)-1; $(obj).attr("data_id", new_id); $(obj).next().attr("data_id", new_id); if ( id > 1) {$(obj).parent().children("div:last-child").removeClass("order"+id); $(obj).parent().children("div:last-child").addClass("order"+new_id); var this_con  = "<div>"+$(obj).parent().html()+"</div>"; var this_prev = $(obj).parent().prev(); $(this_prev).find("span").eq(1).attr("data_id",id); $(this_prev).find("span").eq(2).attr("data_id",id); $(this_prev).children("div:last-child").removeClass("order"+new_id); $(this_prev).children("div:last-child").addClass("order"+id); $(obj).parent().remove(); $(this_prev).before(this_con);}} </script>';
 
         //压入图片下移函数
-        fun_str = fun_str + '<script> function set_down(obj){var id = $(obj).attr("data_id"); var new_id = parseInt(id)+1; $(obj).attr("data_id", new_id); $(obj).next().attr("data_id", new_id); if ( id < 10) {$(obj).parent().children("div:last-child").removeClass("order"+id); $(obj).parent().children("div:last-child").addClass("order"+new_id); var this_con  = "<div>"+$(obj).parent().html()+"</div>"; var this_next = $(obj).parent().next(); $(this_next).find("span").eq(1).attr("data_id",id); $(this_next).find("span").eq(2).attr("data_id",id); $(this_next).children("div:last-child").removeClass("order"+id); $(this_next).children("div:last-child").addClass("order"+new_id); $(obj).parent().remove(); $(this_next).after(this_con);}} </script>';
+        fun_str = fun_str + '<script> function set_down(obj){var id = $(obj).attr("data_id"); var new_id = parseInt(id)+1; $(obj).attr("data_id", new_id); $(obj).next().attr("data_id", new_id); var this_con  = "<div>"+$(obj).parent().html()+"</div>"; var this_next = $(obj).parent().next(); if ( $(this_next).find("span").eq(1).attr("data_id") ) {$(obj).parent().children("div:last-child").removeClass("order"+id); $(obj).parent().children("div:last-child").addClass("order"+new_id); $(this_next).find("span").eq(1).attr("data_id",id); $(this_next).find("span").eq(2).attr("data_id",id); $(this_next).children("div:last-child").removeClass("order"+id); $(this_next).children("div:last-child").addClass("order"+new_id); $(obj).parent().remove(); $(this_next).after(this_con);}} </script>';
         html_node.find("#id_container_add_tmp").after(fun_str);
         function set_up(obj){
             var id = $(obj).attr("data_id");
@@ -118,11 +118,11 @@ $(function(){
             var new_id = parseInt(id)+1;
             $(obj).attr("data_id", new_id);
             $(obj).next().attr("data_id", new_id);
-            if ( id < 10) {
+            var this_con  = "<div>"+$(obj).parent().html()+"</div>";
+            var this_next = $(obj).parent().next();
+            if ( $(this_next).find("span").eq(1).attr("data_id") ) {
                 $(obj).parent().children("div:last-child").removeClass("order"+id);
                 $(obj).parent().children("div:last-child").addClass("order"+new_id);
-                var this_con  = "<div>"+$(obj).parent().html()+"</div>";
-                var this_next = $(obj).parent().next();
                 $(this_next).find("span").eq(1).attr("data_id",id);
                 $(this_next).find("span").eq(2).attr("data_id",id);
                 $(this_next).children("div:last-child").removeClass("order"+id);
