c476fa632b40cdbb9f7a62e69b18312aa6563d9e
tom <tom@leoedu.com>
2017-08-12 14:00:39 +0800

    fix

--- app/Models/t_test_lesson_subject_require.php
+++ app/Models/t_test_lesson_subject_require.php
@@ -291,7 +291,7 @@ class t_test_lesson_subject_require extends \App\Models\Zgen\z_t_test_lesson_sub
             ." and lesson_start<=1499616000"
             // ." and 1498838400<lesson_end<=1498924800"
             ." order by lesson_start asc"
-            ." limit 0,1000 ";
+            ." limit 0,5000 ";
         // dd($sql);
         return $this->main_get_list($sql);
     }
