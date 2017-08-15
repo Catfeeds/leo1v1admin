3ba70c63e81c05a056c28899fca2b5887b57009e
jim <jim@leoedu.com>
2017-08-15 18:42:16 +0800

    fix

--- app/Http/Controllers/testbb.php
+++ app/Http/Controllers/testbb.php
@@ -116,19 +116,19 @@ class testbb extends Controller
         $url_teacher = "";
 
         foreach($teacher_list as $item){
-            $template_id_teacher  = "rSrEhyiqVmc2_NVI8L6fBSHLSCO9CJHly1AU-ZrhK-o";
-            $data_teacher['first']      = "";
-            $data_teacher['keyword1']   = "【PC老师端4.0.0】";
-            $data_teacher['keyword2']   = "
-1、新增扩科（扩年级）功能；
-2、白板中增加播放视频功能。
-下载地址（老师端后台）：
-http://www.leo1v1.com/login/teacher";
-            $data_teacher['keyword3']   = "$date_time";
+//             $template_id_teacher  = "rSrEhyiqVmc2_NVI8L6fBSHLSCO9CJHly1AU-ZrhK-o";
+//             $data_teacher['first']      = "";
+//             $data_teacher['keyword1']   = "【PC老师端4.0.0】";
+//             $data_teacher['keyword2']   = "
+// 1、新增扩科（扩年级）功能；
+// 2、白板中增加播放视频功能。
+// 下载地址（老师端后台）：
+// http://www.leo1v1.com/login/teacher";
+//             $data_teacher['keyword3']   = "$date_time";
 
-            $data_teacher['remark']     = "更新方法：输入下载地址→点击【下载】→【PC电脑】→【立即下载】";
+//             $data_teacher['remark']     = "更新方法：输入下载地址→点击【下载】→【PC电脑】→【立即下载】";
 
-            \App\Helper\Utils::send_teacher_msg_for_wx($item['wx_openid'],$template_id_teacher, $data_teacher,$url_teacher);
+//             \App\Helper\Utils::send_teacher_msg_for_wx($item['wx_openid'],$template_id_teacher, $data_teacher,$url_teacher);
 
         }
 
--- app/Jobs/send_wx_to_teacher_for_update_software.php
+++ app/Jobs/send_wx_to_teacher_for_update_software.php
@@ -16,7 +16,8 @@ class send_wx_to_teacher_for_update_software extends Job implements ShouldQueue
      *
      * @return void
      */
-    public function __construct()
+    public $wx_openid;
+    public function __construct($wx_openid)
     {
         //
     }
@@ -30,32 +31,32 @@ class send_wx_to_teacher_for_update_software extends Job implements ShouldQueue
     {
         //
 
-        $t_teacher_info  = new \App\Models\t_teacher_info();
+        // $t_teacher_info  = new \App\Models\t_teacher_info();
 
-        $teacher_list = $t_teacher_info->get_teacher_openid_list();
+        // $teacher_list = $t_teacher_info->get_teacher_openid_list();
 
-        $teacher_list = [];
+        $teacher_list = [
+            ["wx_openid"=>'oJ_4fxPmwXgLmkCTdoJGhSY1FTlc']
+        ];
 
         $date_time = date("Y-m-d");
 
         $url_teacher = "";
 
-        foreach($teacher_list as $item){
-            $template_id_teacher  = "rSrEhyiqVmc2_NVI8L6fBSHLSCO9CJHly1AU-ZrhK-o";
-            $data_teacher['first']      = "";
-            $data_teacher['keyword1']   = "【PC老师端4.0.0】";
-            $data_teacher['keyword2']   = "
+        $template_id_teacher  = "rSrEhyiqVmc2_NVI8L6fBSHLSCO9CJHly1AU-ZrhK-o";
+        $data_teacher['first']      = "";
+        $data_teacher['keyword1']   = "【PC老师端4.0.0】";
+        $data_teacher['keyword2']   = "
 1、新增扩科（扩年级）功能；
 2、白板中增加播放视频功能。
 下载地址（老师端后台）：
 http://www.leo1v1.com/login/teacher";
-            $data_teacher['keyword3']   = "$date_time";
+        $data_teacher['keyword3']   = "$date_time";
 
-            $data_teacher['remark']     = "更新方法：输入下载地址→点击【下载】→【PC电脑】→【立即下载】";
+        $data_teacher['remark']     = "更新方法：输入下载地址→点击【下载】→【PC电脑】→【立即下载】";
 
-            \App\Helper\Utils::send_teacher_msg_for_wx($item['wx_openid'],$template_id_teacher, $data_teacher,$url_teacher);
+        \App\Helper\Utils::send_teacher_msg_for_wx($wx_openid,$template_id_teacher, $data_teacher,$url_teacher);
 
-        }
 
     }
 }
