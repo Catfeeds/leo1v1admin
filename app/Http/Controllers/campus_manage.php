76f2746af48c369ab49bef71f6ff8b42c992f144
jim <jim@leoedu.com>
2017-08-15 11:24:15 +0800

    fix

---
+++ app/Http/Controllers/campus_manage.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use \App\Enums as E;
+
+use App\Helper\Utils;
+use Illuminate\Support\Facades\Cookie ;
+
+class campus_manage extends Controller
+{
+    use CacheNick;
+    use TeaPower;
+
+   
+    public function admin_campus_manage(){
+        
+    }
+
+
+}
