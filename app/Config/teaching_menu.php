1f0b42e5f0a53aa5a0e0bd197193f8c6a31d4a6a
jim <jim@leoedu.com>
2017-08-16 10:24:23 +0800

    fix

--- app/Config/teaching_menu.php
+++ app/Config/teaching_menu.php
@@ -12,13 +12,14 @@ class teaching_menu{
                  ["power_id"=>25, "name"=>"兼职老师流失模型",   "url"=>"/tongji_ss/get_teacher_appoinment_lecture_info"],
                  ["power_id"=>9,"name"=>"咨询师试听转化率", "url"=>"/tongji_ss/seller_test_lesson_info_tongji"],
                  ["power_id"=>12, "name"=>"教研以及全职老师常规学生详情",   "url"=>"/human_resource/research_qz_teacher_stu_info"],
+                 ["power_id"=>12, "name"=>"试听转化率统计","url"=>"/tongji_ss/tongji_seller_test_lesson_order_info_zs"],
+
              ]],
 
 
              ["power_id"=>25, "name"=>"招师部",  "list"=>[
                  ["power_id"=>1, "name"=>"首页",   "url"=>"/main_page/zs_teacher_new" ],
                  ["power_id"=>3, "name"=>"教师档案",   "url"=>"/human_resource/index_zs"],
-                 ["power_id"=>12, "name"=>"试听转化率统计","url"=>"/tongji_ss/tongji_seller_test_lesson_order_info_zs"],
 
                  ["power_id"=>4, "name"=>"教师试讲预约",   "url"=>"/human_resource/teacher_lecture_appointment_info_zs"],
                  ["power_id"=>2, "name"=>"教研老师信息",   "url"=>"/human_resource/reaearch_teacher_lesson_list"],
