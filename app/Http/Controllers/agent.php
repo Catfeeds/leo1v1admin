6f071907f16ec93fade53604cf3a869e8e29206b
tom <tom@leoedu.com>
2017-08-16 11:24:54 +0800

    fix

--- app/Http/Controllers/agent.php
+++ app/Http/Controllers/agent.php
@@ -58,18 +58,29 @@ class agent extends Controller
         $type      = $this->get_in_int_val('type');
         $page_info = $this->get_in_page_info();
         $ret_info  = $this->t_agent->get_agent_info_new($page_info,$type);
+        $userid_arr = [];
         // dd($ret_info);
         $ret_info_new['total_num'] = $ret_info['total_num'];
         $ret_info_new['per_page_count'] = $ret_info['per_page_count'];
         $ret_info_new['page_info'] = $ret_info['page_info'];
-        $userid_arr = [];
+        $ret_info_new['list'] = [];
+        $id_arr = array_unique(array_column($ret_info['list'],'id'));
         foreach($ret_info['list'] as &$item){
             if($item['type'] == 1){
                 $userid_arr[] = $item['userid'];
             }
             $item['agent_type'] = $item['type'];
             $item['create_time'] = date('Y-m-d H:i:s',$item['create_time']);
+
+            $id = $item['id'];
+            $id_arr_new = array_unique(array_column($ret_info_new['list'],'id'));
+            if(in_array($id,$id_arr_new)){
+            }else{
+                $ret_info_new['list'][] = $item;
+            }
+            // $id_arr =
         }
+        dd($ret_info_new['list']);
         if(count($userid_arr)>0){
             $test_info = $this->t_lesson_info_b2->get_suc_test_by_userid($userid_arr);
             foreach($ret_info['list'] as &$item){
