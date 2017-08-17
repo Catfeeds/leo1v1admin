b6a33d2b4c4702b9ef70ca0cf9add96fcaa89041
tom <tom@leoedu.com>
2017-08-17 16:34:46 +0800

    fix

--- resources/views/tea_manage/lesson_list.blade.php
+++ resources/views/tea_manage/lesson_list.blade.php
@@ -312,7 +312,8 @@
                         <td > {{$var["tea_cw_name"]}} </td>
                         <td > {{$var["lesson_intro"]}} </td>
                         <td > {{$var["performance"]}} </td>
-                        <td > {{$var["require_admin_nick"]}} </td>
+                        <!-- <td > {{$var["require_admin_nick"]}} </td> -->
+                        <td > {{$var["cc_account"]}} </td>
                         <td > {{$var["stu_user_agent"]}} </td>
                         <td >
                             {{$var["record_audio_server1"]}} <br/>
