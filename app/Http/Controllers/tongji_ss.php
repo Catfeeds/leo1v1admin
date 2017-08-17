80fefc2cf58aaec5398a492d7f6e0b3285abeed6
tom <tom@leoedu.com>
2017-08-17 11:25:26 +0800

    fix

--- app/Http/Controllers/tongji_ss.php
+++ app/Http/Controllers/tongji_ss.php
@@ -636,7 +636,7 @@ class tongji_ss extends Controller
             list($all_count,$assigned_count,$tmk_assigned_count,$tq_no_call_count,$tq_called_count,$tq_call_fail_count,
                  $tq_call_succ_valid_count,$tq_call_succ_invalid_count,$tq_call_fail_invalid_count,$have_intention_a_count,
                  $have_intention_b_count,$have_intention_c_count,$require_count,$test_lesson_count,$succ_test_lesson_count,
-                 $order_count,$user_count,$order_all_money) = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+                 $order_count,$user_count,$order_all_money) = [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]];
             $ret  = $this->t_agent->get_agent_info_new(null);
             $userid_arr = [];
 
@@ -681,77 +681,75 @@ class tongji_ss extends Controller
                 foreach($ret_new as $key=>&$item){
                     //已分配销售
                     if($item['admin_revisiterid']>0){
-                        $assigned_count++;
+                        $assigned_count[] = $item;
                     }
                     //TMK有效
                     if($item['tmk_student_status'] == 3){
-                        $tmk_assigned_count++;
+                        $tmk_assigned_count[] = $item;
                     }
                     //未拨打
                     if($item['global_tq_called_flag'] == 0){
-                        $tq_no_call_count++;
+                        $tq_no_call_count[] = $item;
                     }
                     //已拨打
                     if($item['global_tq_called_flag'] != 0){
-                        $tq_called_count++;
+                        $tq_called_count[] = $item;
                     }
                     //未接通
                     if($item['global_tq_called_flag'] == 1){
-                        $tq_call_fail_count++;
+                        $tq_call_fail_count[] = $item;
                     }
                     //已拨通-有效
                     if($item['global_tq_called_flag'] == 2 && $item['sys_invaild_flag'] == 0){
-                        $tq_call_succ_valid_count++;
+                        $tq_call_succ_valid_count[] = $item;
                     }
                     //已拨通-无效
                     if($item['global_tq_called_flag'] == 2 && $item['sys_invaild_flag'] == 1){
-                        $tq_call_succ_invalid_count++;
+                        $tq_call_succ_invalid_count[] = $item;
                     }
                     //未拨通-无效
                     if($item['global_tq_called_flag'] == 1 && $item['sys_invaild_flag'] == 1){
-                        $tq_call_fail_invalid_count++;
+                        $tq_call_fail_invalid_count[] = $item;
                     }
                     //有效意向(A)
                     if($item['global_tq_called_flag'] == 2 && $item['seller_student_status'] == 100){
-                        $have_intention_a_count++;
+                        $have_intention_a_count[] = $item;
                     }
                     //有效意向(B)
                     if($item['global_tq_called_flag'] == 2 && $item['seller_student_status'] == 101){
-                        $have_intention_b_count++;
+                        $have_intention_b_count[] = $item;
                     }
                     //有效意向(C)
                     if($item['global_tq_called_flag'] == 2 && $item['seller_student_status'] == 102){
-                        $have_intention_c_count++;
+                        $have_intention_c_count[] = $item;
                     }
                     //预约数&&上课数
                     if($item['accept_flag'] == 1 && $item['is_test_user'] == 0 && $item['require_admin_type'] == 2 ){
-                        $require_count++;
-                        $test_lesson_count++;
+                        $require_count[] = $item;
+                        $test_lesson_count[] = $item;
                     }
                     //试听成功数
                     if($item['accept_flag'] == 1 && $item['is_test_user'] == 0 && $item['require_admin_type'] == 2 && $item['lesson_user_online_status'] == 1 ){
-                        $succ_test_lesson_count++;
-                        $suc_test_arr[] = $item;
+                        $succ_test_lesson_count[] = $item;
                     }
                 }
             }
-            dd($suc_test_arr);
             if(isset($ret_info['list'][4]['all_count'])){
                 $ret_info['list'][4]['all_count'] = $all_count;
-                $ret_info['list'][4]['assigned_count'] = $assigned_count;
-                $ret_info['list'][4]['tmk_assigned_count'] = $tmk_assigned_count;
-                $ret_info['list'][4]['tq_no_call_count'] = $tq_no_call_count;
-                $ret_info['list'][4]['tq_called_count'] = $tq_called_count;
-                $ret_info['list'][4]['tq_call_fail_count'] = $tq_call_fail_count;
-                $ret_info['list'][4]['tq_call_succ_valid_count'] = $tq_call_succ_valid_count;
-                $ret_info['list'][4]['tq_call_succ_invalid_count'] = $tq_call_succ_invalid_count;
-                $ret_info['list'][4]['tq_call_fail_invalid_count'] = $tq_call_fail_invalid_count;
-                $ret_info['list'][4]['have_intention_a_count'] = $have_intention_a_count;
-                $ret_info['list'][4]['have_intention_b_count'] = $have_intention_b_count;
-                $ret_info['list'][4]['have_intention_c_count'] = $have_intention_c_count;
-                $ret_info['list'][4]['require_count'] = $require_count;
-                $ret_info['list'][4]['test_lesson_count'] = $test_lesson_count;
-                $ret_info['list'][4]['succ_test_lesson_count'] = $succ_test_lesson_count;
+                $ret_info['list'][4]['assigned_count'] = count($assigned_count);
+                $ret_info['list'][4]['tmk_assigned_count'] = count($tmk_assigned_count);
+                $ret_info['list'][4]['tq_no_call_count'] = count($tq_no_call_count);
+                $ret_info['list'][4]['tq_called_count'] = count($tq_called_count);
+                $ret_info['list'][4]['tq_call_fail_count'] = count($tq_call_fail_count);
+                $ret_info['list'][4]['tq_call_succ_valid_count'] = count($tq_call_succ_valid_count);
+                $ret_info['list'][4]['tq_call_succ_invalid_count'] = count($tq_call_succ_invalid_count);
+                $ret_info['list'][4]['tq_call_fail_invalid_count'] = count($tq_call_fail_invalid_count);
+                $ret_info['list'][4]['have_intention_a_count'] = count($have_intention_a_count);
+                $ret_info['list'][4]['have_intention_b_count'] = count($have_intention_b_count);
+                $ret_info['list'][4]['have_intention_c_count'] = count($have_intention_c_count);
+                $ret_info['list'][4]['require_count'] = count($require_count);
+                $ret_info['list'][4]['test_lesson_count'] = count($test_lesson_count);
+                $ret_info['list'][4]['succ_test_lesson_count'] = count($succ_test_lesson_count);
             }
         }
         return $this->pageView(__METHOD__,$ret_info,[
