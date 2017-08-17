31ef0b1d739dc176ef1fd9cec185980b1c8fd019
tom <tom@leoedu.com>
2017-08-17 10:53:06 +0800

    fix

--- app/Http/Controllers/tongji_ss.php
+++ app/Http/Controllers/tongji_ss.php
@@ -637,8 +637,11 @@ class tongji_ss extends Controller
                  $tq_call_succ_valid_count,$tq_call_succ_invalid_count,$tq_call_fail_invalid_count,$have_intention_a_count,
                  $have_intention_b_count,$have_intention_c_count,$require_count,$test_lesson_count,$succ_test_lesson_count,
                  $order_count,$user_count,$order_all_money) = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
-            $ret  = $this->t_agent->get_agent_info_new($type=null);
+            $ret  = $this->t_agent->get_agent_info_new($type);
+            $userid_arr = [];
+
             $ret_new = [];
+            $ret_info_new = [];
             $id_arr = array_unique(array_column($ret,'id'));
             foreach($ret as &$item){
                 if($item['type'] == 1){
@@ -656,7 +659,6 @@ class tongji_ss extends Controller
                 $id_arr_new = array_unique(array_column($ret_new,'id'));
                 if(in_array($id,$id_arr_new)){
                 }else{
-                    $ret_count[] = $item;
                     if($item['lesson_start']){
                         if($item['lesson_start']>$item['create_time']){
                             $ret_new[] = $item;
@@ -665,9 +667,15 @@ class tongji_ss extends Controller
                         $ret_new[] = $item;
                     }
                 }
+                $id_arr_new_two = array_unique(array_column($ret_info_new,'id'));
+                if(in_array($id,$id_arr_new_two)){
+                }else{
+                    $ret_info_new[] = $item;
+                }
+
             }
-            dd($ret_count);
-            $all_count = count($ret_count);
+            dd($ret_info_new);
+            $all_count = count($ret_info_count);
             if(count($userid_arr)>0){
                 foreach($ret_new as $key=>&$item){
                     //例子总数
