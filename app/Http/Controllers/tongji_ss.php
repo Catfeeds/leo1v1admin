b34a7f903146b2dd4d2e3516a4e0484b23dbfee2
tom <tom@leoedu.com>
2017-08-17 10:37:31 +0800

    fix

--- app/Http/Controllers/tongji_ss.php
+++ app/Http/Controllers/tongji_ss.php
@@ -637,7 +637,7 @@ class tongji_ss extends Controller
                  $tq_call_succ_valid_count,$tq_call_succ_invalid_count,$tq_call_fail_invalid_count,$have_intention_a_count,
                  $have_intention_b_count,$have_intention_c_count,$require_count,$test_lesson_count,$succ_test_lesson_count,
                  $order_count,$user_count,$order_all_money) = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
-            $ret  = $this->t_agent->get_agent_info_new($type=1);
+            $ret  = $this->t_agent->get_agent_info_new($type=null);
             $ret_new = [];
             $id_arr = array_unique(array_column($ret,'id'));
             foreach($ret as &$item){
@@ -652,8 +652,9 @@ class tongji_ss extends Controller
                     $item['lesson_start'] = '';
                 }
 
+                $id = $item['id'];
                 $id_arr_new = array_unique(array_column($ret_new,'id'));
-                if(in_array($item['id'],$id_arr_new)){
+                if(in_array($id,$id_arr_new)){
                 }else{
                     $ret_count[] = $item;
                     if($item['lesson_start']){
@@ -670,7 +671,7 @@ class tongji_ss extends Controller
             if(count($userid_arr)>0){
                 foreach($ret_new as $key=>&$item){
                     //例子总数
-                    $all_count = $key+1;
+                    // $all_count = $key+1;
                     //已分配销售
                     if($item['admin_revisiterid']>0){
                         $assigned_count++;
