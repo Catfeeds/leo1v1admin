c6c54b89e06164d90c07a142c954cbfffe4ea54b
jim <jim@leoedu.com>
2017-08-17 14:51:45 +0800

    fix

--- public/page_ts/tongji_ss/tongji_seller_test_lesson_order_info_for_jx.ts
+++ public/page_ts/tongji_ss/tongji_seller_test_lesson_order_info_for_jx.ts
@@ -1,21 +1,62 @@
 /// <reference path="../common.d.ts" />
 /// <reference path="../g_args.d.ts/tongji_ss-tongji_seller_test_lesson_order_info_for_jx.d.ts" />
 
-$(function(){
-    function load_data(){
-        $.reload_self_page ( {
-            // order_by_str: g_args.order_by_str,
-			      date_type_config:	$('#id_date_type_config').val(),
-			      date_type:	$('#id_date_type').val(),
-			      opt_date_type:	$('#id_opt_date_type').val(),
-			      start_time:	$('#id_start_time').val(),
-			      end_time:	$('#id_end_time').val(),
-			      adminid:	$('#id_adminid').val(),
-			      seller_groupid_ex:	$('#id_seller_groupid_ex').val()
-        });
-    }
+// $(function(){
+//     function load_data(){
+//         $.reload_self_page ( {
+//             // order_by_str: g_args.order_by_str,
+// 			      date_type_config:	$('#id_date_type_config').val(),
+// 			      date_type:	$('#id_date_type').val(),
+// 			      opt_date_type:	$('#id_opt_date_type').val(),
+// 			      start_time:	$('#id_start_time').val(),
+// 			      end_time:	$('#id_end_time').val(),
+// 			      adminid:	$('#id_adminid').val(),
+// 			      seller_groupid_ex:	$('#id_seller_groupid_ex').val()
+//         });
+//     }
 
-    console.log('排序'+g_args.order_by_str);
+//     console.log('排序'+g_args.order_by_str);
+
+//     $('#id_date_range').select_date_range({
+//         'date_type' : g_args.date_type,
+//         'opt_date_type' : g_args.opt_date_type,
+//         'start_time'    : g_args.start_time,
+//         'end_time'      : g_args.end_time,
+//         date_type_config : JSON.parse( g_args.date_type_config),
+//         onQuery :function() {
+//             load_data();
+//         }
+//     });
+//     $('#id_seller_groupid_ex').val(g_args.seller_groupid_ex);
+
+//     // $('#id_order_by_str').val(g_args.order_by_str);
+//     $("#id_seller_groupid_ex").init_seller_groupid_ex(g_adminid_right);
+
+//      $(".common-table").table_group_level_4_init();
+// 	//$('#id_adminid').val(g_args.adminid);
+
+
+// 	$('.opt-change').set_input_change_event(load_data);
+// });
+
+
+
+
+function load_data(){
+    $.reload_self_page ( {
+        order_by_str: g_args.order_by_str,
+        date_type_config:	$('#id_date_type_config').val(),
+			  date_type:	$('#id_date_type').val(),
+			  opt_date_type:	$('#id_opt_date_type').val(),
+			  start_time:	$('#id_start_time').val(),
+			  end_time:	$('#id_end_time').val(),
+			  adminid:	$('#id_adminid').val(),
+			  seller_groupid_ex:	$('#id_seller_groupid_ex').val()
+    });
+}
+
+$(function(){
+
 
     $('#id_date_range').select_date_range({
         'date_type' : g_args.date_type,
@@ -27,14 +68,13 @@ $(function(){
             load_data();
         }
     });
-    $('#id_seller_groupid_ex').val(g_args.seller_groupid_ex);
-
-    // $('#id_order_by_str').val(g_args.order_by_str);
-    $("#id_seller_groupid_ex").init_seller_groupid_ex(g_adminid_right);
-
-     $(".common-table").table_group_level_4_init();
-	//$('#id_adminid').val(g_args.adminid);
+    $('#id_order_by_str').val(g_args.order_by_str);
+    // $('#id_show_flag').val(g_args.show_flag);
+    // $('#id_seller_flag').val(g_args.seller_flag);
+    // $('#id_lesson_money').val(g_args.lesson_money);
 
 
-	$('.opt-change').set_input_change_event(load_data);
+
+
+    $('.opt-change').set_input_change_event(load_data);
 });
