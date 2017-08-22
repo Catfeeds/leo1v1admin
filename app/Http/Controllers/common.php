c0f7d69b17cba4fb15de585cac3df3bdabce9a9d
tom <tom@leoedu.com>
2017-08-22 16:06:58 +0800

    fix

--- app/Http/Controllers/common.php
+++ app/Http/Controllers/common.php
@@ -813,7 +813,7 @@ class common extends Controller
             return "";
         }
         $qiniu         = \App\Helper\Config::get_config("qiniu");
-        $phone_qr_name = $phone."_qr_agent_lh.png";
+        $phone_qr_name = $phone."_qr_agent_hg.png";
         $qiniu_url     = $qiniu['public']['url'];
         $is_exists     = \App\Helper\Utils::qiniu_file_stat($qiniu_url,$phone_qr_name);
         if(!$is_exists){
