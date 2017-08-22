78eeee2acfeb2928103487948dc6cf4bfc15810a
jim <jim@leoedu.com>
2017-08-22 19:01:12 +0800

    fix

--- app/Config/publish_version.php
+++ app/Config/publish_version.php
@@ -2,5 +2,5 @@
 namespace App\Config;
 class publish_version {
     //不要手动修改
-    static public $version="20170822-184848";
+    static public $version="20170822-185843";
 };
--- app/Http/Controllers/testbb.php
+++ app/Http/Controllers/testbb.php
@@ -160,17 +160,35 @@ class testbb extends Controller
         $pdf_name_tmp =$hostdir.'/'.time().'_'.rand().'.pdf';
 
 
+        if (ob_get_contents()) {
+        }
+        header('Content-Description: File Transfer');
+        if (headers_sent()) {
+        }
+        header('Cache-Control: private, must-revalidate, post-check=0, pre-check=0, max-age=1');
+        header('Pragma: public');
+        header('Expires: Sat, 26 Jul 1997 05:00:00 GMT'); // Date in the past
+        header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
+        // force download dialog
+        if (strpos(php_sapi_name(), 'cgi') === false) {
+            header('Content-Type: application/force-download');
+            header('Content-Type: application/octet-stream', false);
+            header('Content-Type: application/download', false);
+            header('Content-Type: application/pdf', false);
+        } else {
+            header('Content-Type: application/pdf');
+        }
+        // use the Content-Disposition header to supply a recommended filename
+        header('Content-Disposition: attachment; filename="'.basename($name).'"');
+        header('Content-Transfer-Encoding: binary');
+        TCPDF_STATIC::sendOutputData(file_get_contents($name), filesize($name));
+
+
+
 
         $pdf_info = $pdf->Output("$pdf_name_tmp", 'I');
 
         dd($pdf_info);
-
-
-
-
-
-
-
 
     }
 
