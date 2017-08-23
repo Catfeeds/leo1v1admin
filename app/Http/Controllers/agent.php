c858f51aad5088462a26655f070e942135226be1
tom <tom@leoedu.com>
2017-08-23 15:43:05 +0800

    fix

--- app/Http/Controllers/agent.php
+++ app/Http/Controllers/agent.php
@@ -520,10 +520,10 @@ class agent extends Controller
     public function get_user_info(){
         // $agent_id = 60;//月月
         // $agent_id = 54;//陈
-        $agent_id = 211;//Amanda
+        // $agent_id = 211;//Amanda
         // $agent_id = 1571;//三千笔墨绘你一世倾
         // $agent_id = 427;//周圣杰 Eros
-        // $agent_id = 1509;//王朝刚
+        $agent_id = 1509;//王朝刚
         $agent_info = $this->t_agent->get_agent_info_by_id($agent_id);
         if(isset($agent_info['phone'])){
             $phone = $agent_info['phone'];
