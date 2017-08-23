d4080f52adad291cbcc3faa9dbab51462b50917c
jim <jim@leoedu.com>
2017-08-23 19:36:09 +0800

    fix

--- app/Config/publish_version.php
+++ app/Config/publish_version.php
@@ -2,5 +2,5 @@
 namespace App\Config;
 class publish_version {
     //不要手动修改
-    static public $version="20170823-184358";
+    static public $version="20170823-193503";
 };
--- app/Models/t_lesson_info_b2.php
+++ app/Models/t_lesson_info_b2.php
@@ -3370,5 +3370,22 @@ class t_lesson_info_b2 extends \App\Models\Zgen\z_t_lesson_info
         return $this->main_get_list($sql);
     }
 
+    public function get_test_lesson_num($start_time,$end_time){
+        $where_arr=[
+            "l.lesson_type = 2",
+            "l.lesson_del_flag = 0",
+            // "tss.success_flag in (0,1)",
+            "l.lesson_user_online_status =1",
+            //"t.is_test_user=0"
+        ];
+
+        $this->where_arr_add_time_range($where_arr,"lesson_start",$start_time,$end_time);
+        $sql = $this->gen_sql_new("select teacherid,count(l.lessonid) num from %s where %s group by teacherid",
+                                  self::DB_TABLE_NAME,
+                                  $where_arr
+        );
+        return $this->main_get_list($sql);
+
+    }
 
 }
