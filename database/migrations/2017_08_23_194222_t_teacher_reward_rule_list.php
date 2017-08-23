54f88bcecc572e523e1a9db47ea00738263b48c7
jim <jim@leoedu.com>
2017-08-23 20:17:45 +0800

    fix

--- database/migrations/2017_08_23_194222_t_teacher_reward_rule_list.php
+++ database/migrations/2017_08_23_194222_t_teacher_reward_rule_list.php
@@ -15,11 +15,11 @@ class TTeacherRewardRuleList extends Migration
         //
         Schema::create('db_weiyi.t_teacher_reward_rule_list', function( Blueprint $table)
         {
-            t_field($table->integer("reward_type"),"奖金类型 1 课时奖励 2 推荐有奖 ");
+            t_field($table->integer("reward_count_type"),"累积奖金类型 1 课时奖励 2 推荐有奖 ");
             t_field($table->tinyInteger("rule_type"),"规则类型");
             t_field($table->integer("num"),"累计数量");
             t_field($table->integer("money"),"奖励金额");
-            $table->primary(["reward_type","rule_type","num"],"type_key");
+            $table->primary(["reward_count_type","rule_type"],"type_key");
         });
     }
 
@@ -31,5 +31,6 @@ class TTeacherRewardRuleList extends Migration
     public function down()
     {
         //
+        Schema::drop('db_weiyi.t_teacher_reward_rule_list');
     }
 }
