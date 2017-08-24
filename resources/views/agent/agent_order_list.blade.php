ed93f85a17fd2c6525bdfaaa06f9ebb0950fe953
tom <tom@leoedu.com>
2017-08-24 14:39:30 +0800

    fix

--- resources/views/agent/agent_order_list.blade.php
+++ resources/views/agent/agent_order_list.blade.php
@@ -62,7 +62,7 @@
                             >
                                 <!-- <a class="fa fa-edit opt-edit"  title="编辑"> </a> -->
                                 <a class="fa-user opt-user " title="上课记录" ></a>
-                                <a class="fa fa-times opt-del" style="display:none;" title="删除"> </a>
+                                <a class="fa fa-times opt-del" title="删除"> </a>
                             </div>
                         </td>
                     </tr>
