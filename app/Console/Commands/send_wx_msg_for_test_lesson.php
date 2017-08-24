ac1bcfdbeb1ce106f6a9f24e7eb215c30ea70e3e
jim <jim@leoedu.com>
2017-08-24 15:11:17 +0800

    fix

--- app/Console/Commands/send_wx_msg_for_test_lesson.php
+++ app/Console/Commands/send_wx_msg_for_test_lesson.php
@@ -68,7 +68,9 @@ class send_wx_msg_for_test_lesson extends Command
             $opt_time_tea = $task->t_lesson_opt_log->get_test_lesson_for_login($item['lessonid'],$item['teacherid'],$item['lesson_start'],$item['lesson_end']);
             $opt_time_stu = $task->t_lesson_opt_log->get_test_lesson_for_login($item['lessonid'],$item['userid'],$item['lesson_start'],$item['lesson_end']);
 
+            
             if($opt_time_stu>=$now){ // 判断学生是否超时 [5分钟]
+                $data_par = $this->get_data($item,1,2);
                 $this->send_wx_msg_admin($item,2);
             }
 
@@ -76,7 +78,7 @@ class send_wx_msg_for_test_lesson extends Command
                 $this->send_wx_msg_tea($item,2);
             }
 
-            
+
         }
 
         // 试听课超时15分钟
