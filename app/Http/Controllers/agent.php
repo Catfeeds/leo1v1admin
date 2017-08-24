5b18a682ef95c49571be4644027ca1c6c7417bc8
tom <tom@leoedu.com>
2017-08-24 15:10:51 +0800

    fix

--- app/Http/Controllers/agent.php
+++ app/Http/Controllers/agent.php
@@ -219,7 +219,8 @@ class agent extends Controller
     }
 
     public function check(){
-        $phone = '13508376529';
+        // $phone = '13508376529';
+        $phone = '18621663326';
         $userid = $this->t_phone_to_user->get_userid($phone);
         $type = $this->t_student_info->field_get_value($userid,'type');
         dd($type);
