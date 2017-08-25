4893d2d2340d4743c853900388243fe01817490b
boby <boby@leoedu.com>
2017-08-25 16:20:03 +0800

    fix

--- public/page_js/teacher_info/get_teacher_basic_info.js
+++ public/page_js/teacher_info/get_teacher_basic_info.js
@@ -8,9 +8,19 @@ $(function(){
         });
     }
 
-$('.opt-display').on('click', function(){
-    
-})
+    $('.opt-set').on('click', function(){
+        var old_status = $(this).attr('data-status');
+        if(old_status == 'full') {
+            $(this).attr('data-status','nofull');
+            $(this).html("设置饱和");
+        } else {
+            $(this).attr('data-status', 'full');
+            $(this).html("设置不饱和");
+        }
+        $('h3[data-status]').toggleClass('hide');
+        $('p[data-status]').toggleClass('hide');
+
+    })
     $('.direct-chat-contacts').css('backgroundColor','#fff');
 
 
