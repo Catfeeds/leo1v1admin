260635328a5899efd51cd492026d763112ac87bc
tom <tom@leoedu.com>
2017-08-25 17:20:48 +0800

    fix

--- app/Http/Controllers/agent.php
+++ app/Http/Controllers/agent.php
@@ -1132,6 +1132,8 @@ class agent extends Controller
     }
 
     public function update_agent_level(){
+        $this->task->t_agent->reset_user_info($id = 1634);
+        dd('a');
         $ret_info = $this->t_agent->get_agent_list();
         foreach($ret_info as $item){
             $id = $item['id'];
