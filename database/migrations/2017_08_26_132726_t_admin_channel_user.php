a5f49628a43633682b2d6164ed6f56bc6bcf97ae
sam <sam@leoedu.com>
2017-08-26 13:28:55 +0800

    fix

--- database/migrations/2017_08_26_132726_t_admin_channel_user.php
+++ database/migrations/2017_08_26_132726_t_admin_channel_user.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class TAdminChannelUserChange extends Migration
+class TAdminChannelUser extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class TAdminChannelUserChange extends Migration
     public function up()
     {
         //
-       Schema::create('db_weiyi_admin.t_admin_channel_user', function( Blueprint $table)
+        Schema::create('db_weiyi_admin.t_admin_channel_user', function( Blueprint $table)
         {
             t_field($table->integer("admin_id",true),"成员ID");
             t_field($table->integer("group_id"),"次渠道id");
