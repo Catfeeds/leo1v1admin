4c845dfe7ecd23d006efb8a98f94f39f25de8518
boby <boby@leoedu.com>
2017-08-29 18:31:49 +0800

    fix

---
+++ public/page_js/teacher_info/get_teacher_money_info.js
@@ -0,0 +1,43 @@
+
+/// <reference path="../common.d.ts" />
+/// <reference path="../g_args.d.ts/teacher_info-get_teacher_money_info.d.ts" />
+
+$(function(){
+    function load_data(){
+        $.reload_self_page ( {
+
+        });
+    }
+
+    $('button[data-key]').on('click', function() {
+        var key = $(this).attr('data-key');
+        $('.'+key).slideToggle('show');
+        $(this).children().toggleClass('fa-plus');
+        $(this).children().toggleClass('fa-minus');
+    });
+
+
+	  $('.opt-change').set_input_change_event(load_data);
+    
+    // LINE CHART
+    var line = new Morris.Line({
+        element: 'line-chart',
+        resize: true,
+        data: [
+            {x: '2017-01', y: 2666},
+            {x: '2017-02', y: 2778},
+            {x: '2017-03', y: 4912},
+            {x: '2017-04', y: 3767},
+            {x: '2017-05', y: 6810},
+            {x: '2017-06', y: 5670},
+        ],
+        xkey: 'x',
+        ykeys: ['y'],
+        labels: ['工资'],
+        lineColors: ['#3c8dbc'],
+        hideHover: 'auto'
+    });
+
+});
+
+
--- resources/views/teacher_info/get_teacher_money_info.blade.php
+++ resources/views/teacher_info/get_teacher_money_info.blade.php
@@ -1,50 +1,268 @@
 @extends('layouts.teacher_header')
 @section('content')
+    <section class="content">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="box box-info">
+                    <div class="box-header with-border">
+                        <h3 class="box-title">Line Chart</h3>
 
-    <section class="content ">
-        
-        <div>
-            <div class="row  row-query-list" >
-                <div class="col-xs-12 col-md-5"  data-title="时间段">
-                    <div  id="id_date_range" >
+                        <div class="box-tools pull-right">
+                            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                            </button>
+                        </div>
                     </div>
-                </div>
-
-                <div class="col-xs-6 col-md-2">
-                    <div class="input-group " >
-                        <span >xx</span>
-                        <input type="text" value=""  class="opt-change"  id="id_"  placeholder=""  />
+                    <div class="box-body chart-responsive">
+                        <div class="chart" id="line-chart" style="height: 300px;"></div>
                     </div>
+                    <!-- /.box-body -->
                 </div>
+                <!-- /.box -->
             </div>
-        </div>
-        <hr/>
-        <table     class="common-table"  > 
-            <thead>
-                <tr>
-                    <td>字段1 </td>
-                    <td> 操作  </td>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ( $table_data_list as $var )
-                    <tr>
-                        <td>{{@$var[""]}} </td>
-                        <td>
-                            <div
-                                {!!  \App\Helper\Utils::gen_jquery_data($var )  !!}
-                            >
-                                <a class="fa fa-edit opt-edit"  title="编辑"> </a>
-                                <a class="fa fa-times opt-del" title="删除"> </a>
 
+            <div class="col-sm-12">
+                <div class="box box-success">
+                    <div class="box-header with-border">
+                        <h3 class="box-title text-green">薪资情况</h3>
+
+                        <div class="box-tools pull-right">
+                            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="box-body">
+                        <div class="row">
+                            <div class="col-lg-3 col-xs-6">
+                                <!-- small box -->
+                                <div class="small-box bg-yellow">
+                                    <div class="col-xs-12 text-right">
+                                        <i class="fa fa-question"></i>
+                                    </div>
+                                    <div class="inner">
+                                        <p>教师等级</p>
+                                        <h4>高级教师</h4>
+                                    </div>
+                                </div>
                             </div>
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-        @include("layouts.page")
-    </section>
-    
-@endsection
+                            <!-- ./col -->
+                            <div class="col-lg-3 col-xs-6">
+                                <!-- small box -->
+                                <div class="small-box bg-blue">
+                                    <div class="col-xs-12 text-right">
+                                       初三 <i class="fa fa-plus"></i>
+                                    </div>
+                                    <div class="inner">
+                                        <p>基本薪资</p>
+                                        <h4>20.00/每节课</h4>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- ./col -->
+                            <div class="col-lg-3 col-xs-6">
+                                <!-- small box -->
+                                <div class="small-box bg-green">
+                                    <div class="col-xs-12 text-right">
+                                        <i class="fa fa-question"></i>
+                                    </div>
+                                    <div class="inner">
+                                        <p>本月课耗</p>
+                                        <h4>66.5课时</h4>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- ./col -->
+                            <div class="col-lg-3 col-xs-6">
+                                <!-- small box -->
+                                <div class="small-box bg-red">
+                                    <div class="col-xs-12 text-right">
+                                        <i class="fa fa-question"></i>
+                                    </div>
+                                    <div class="inner">
+                                        <p>课耗奖励</p>
+                                        <h4>每节课20元</h4>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- ./col -->
+                        </div>
+                        <!-- /.row -->
+                    </div>
+                    <!-- /.box-body -->
+                </div>
+                <!-- /.box -->
+            </div>
 
+            <div class="col-sm-12">
+                <div class="box box-info">
+                    <div class="box-header with-border">
+                        <h3 class="box-title text-blue">薪资详情</h3>
+
+                        <div class="box-tools pull-right">
+                            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="box-body">
+                        <table class="table table-bordered">
+                            <tr>
+                                <th style="width:25px"></th>
+                                <th>分类</th>
+                                <th>姓名</th>
+                                <th>时间</th>
+                                <th>状态</th>
+                                <th>金额</th>
+                                <th>操作</th>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <button data-key="key1" type="button" class="btn btn-box-tool"><i class="fa fa-plus"></i>
+                                    </button>
+
+                                </td>
+                                <td>推荐</td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <button data-key="key2" type="button" class="btn btn-box-tool"><i class="fa fa-plus"></i>
+                                    </button>
+
+                                </td>
+                                <td>奖金</td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr class="key2">
+                                <td></td>
+                                <td>荣誉榜奖金</td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr class="hide key2">
+                                <td></td>
+                                <td>试听课奖金</td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr class="key2">
+                                <td> </td>
+                                <td>试听培训奖金</td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+
+                            <tr>
+                                <td>
+                                    <button data-key="key3" type="button" class="btn btn-box-tool"><i class="fa fa-minus"></i>
+                                    </button>
+
+                                </td>
+                                <td>补偿</td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr class="key3">
+                                <td> </td>
+                                <td>90分钟课程补偿</td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr class="key3">
+                                <td> </td>
+                                <td>工资补偿</td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+
+                            <tr>
+                                <td>
+                                    <button data-key="key4" type="button" class="btn btn-box-tool"><i class="fa fa-plus"></i>
+                                    </button>
+
+                                </td>
+                                <td>课程扣款</td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <button data-key="key5" type="button" class="btn btn-box-tool"><i class="fa fa-plus"></i>
+                                    </button>
+
+                                </td>
+                                <td>平台管理费</td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <button data-key="key6" type="button" class="btn btn-box-tool"><i class="fa fa-plus"></i>
+                                    </button>
+
+                                </td>
+                                <td>试听课程</td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <button data-key="key7" type="button" class="btn btn-box-tool"><i class="fa fa-plus"></i>
+                                    </button>
+
+                                </td>
+                                <td>常规课程</td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+
+                        </table>
+                    </div>
+                    <!-- /.box-body -->
+                </div>
+                <!-- /.box -->
+            </div>
+
+
+
+
+        </div>
+    </section>
+
+@endsection
