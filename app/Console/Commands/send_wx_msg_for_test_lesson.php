c9afb0a693d8a05d74d3a2ca14a78ef39fb1b2e8
jim <jim@leoedu.com>
2017-08-29 12:10:38 +0800

    fix

--- app/Console/Commands/send_wx_msg_for_test_lesson.php
+++ app/Console/Commands/send_wx_msg_for_test_lesson.php
@@ -220,14 +220,14 @@ class send_wx_msg_for_test_lesson extends Command
             $logout_time_tea = $task->t_lesson_opt_log->get_logout_time($item['lessonid'],$item['teacherid']);
             $logout_time_stu = $task->t_lesson_opt_log->get_logout_time($item['lessonid'],$item['userid']);
 
-            if( $logout_time_tea<$item['lesson_start']){
-                $data_ass = $this->get_data($item,3,5,$item['teacher_nick'],'');
-                $this->send_wx_msg_ass($item,5,$data_ass);
+            if( $logout_time_tea>$item['lesson_end']-600){
+                $data_ass = $this->get_data($item,3,6);
+                $this->send_wx_msg_ass($item,6,$data_ass);
             }
 
-            if(!$logout_time_stu || $logout_time_stu<$item['lesson_start']){
-                $data_ass = $this->get_data($item,3,5,'',$item['stu_nick']);
-                $this->send_wx_msg_ass($item,5,$data_ass);
+            if($logout_time_stu>$item['lesson_end']-600){
+                $data_ass = $this->get_data($item,3,6);
+                $this->send_wx_msg_ass($item,6,$data_ass);
             }
 
         }
