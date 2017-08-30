de7ba2935a76be820feb886e1119766ef9b513d0
jim <jim@leoedu.com>
2017-08-30 12:55:25 +0800

    fix

---
+++ public/page_ts/g_args.d.ts/tongji_ss-tongji_teacher_1v1_lesson_time.d.ts
@@ -0,0 +1,54 @@
+interface GargsStatic {
+}
+declare module "g_args" {
+    export = g_args;
+}
+declare var g_args: GargsStatic;
+declare var g_account: string;
+declare var g_account_role: any;
+declare var g_adminid: any;
+interface RowData {
+	realname	:any;
+	seven	:any;
+	one	:any;
+	all	:any;
+	late	:any;
+	total	:any;
+	seven_hour	:any;
+	seven_day	:any;
+	one_hour	:any;
+	one_day	:any;
+	all_hour	:any;
+	all_day	:any;
+	late_hour	:any;
+	late_day	:any;
+	total_hour	:any;
+	total_day	:any;
+}
+
+/*
+
+tofile: 
+	 mkdir -p ../tongji_ss; vi  ../tongji_ss/tongji_teacher_1v1_lesson_time.ts
+
+/// <reference path="../common.d.ts" />
+/// <reference path="../g_args.d.ts/tongji_ss-tongji_teacher_1v1_lesson_time.d.ts" />
+
+$(function(){
+    function load_data(){
+        $.reload_self_page ( {
+
+        });
+    }
+
+
+
+
+	$('.opt-change').set_input_change_event(load_data);
+});
+
+
+
+*/
+/* HTML ...
+*/
