32afe7e88824ec9bf92a36346e8d183fa89cc60b
tom <tom@leoedu.com>
2017-08-30 10:31:20 +0800

    fix

--- app/Http/Controllers/tongji.php
+++ app/Http/Controllers/tongji.php
@@ -1168,7 +1168,7 @@ class tongji extends Controller
         }
         //$ret_info=\App\Helper\Common::gen_admin_member_data($res);
 
-        // $ret_info=\App\Helper\Common::gen_admin_member_data($res,[],0, strtotime( date("Y-m-01",$start_time )   ));
+        $ret_info=\App\Helper\Common::gen_admin_member_data($res,[],0, strtotime( date("Y-m-01",$start_time )   ));
         foreach( $ret_info as &$item ) {
             E\Emain_type::set_item_value_str($item);
             $item['become_member_long_time'] = isset($item['become_member_time'])?(int)(@(time()-$item['become_member_time'])/86400):'';
