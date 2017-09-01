efa2e6c57b53ab595121f13bbd7f034e4236229f
boby <boby@leoedu.com>
2017-09-01 15:59:33 +0800

    fix

--- app/Http/Controllers/test_boby.php
+++ app/Http/Controllers/test_boby.php
@@ -303,57 +303,6 @@ class test_boby extends Controller
 
     }
 
-    public function get_teacher_lesson(){//p 2
-        $teacherid = 18011110026;
-
-        if (!$teacherid) {
-            return $this->output_err("信息有误，未查询到老师信息！");
-        }
-        $end_time   = strtotime(date("Y-m-01",time()));
-        $start_time = strtotime("-1 month",$end_time);
-        $ret_info   = $this->t_teacher_info->get_tea_lesson_info($teacherid, $start_time, $end_time);
-        $ret_info['normal_count'] = $ret_info['normal_count']/100;
-        $ret_info['test_count']   = $ret_info['test_count']/100;
-        $ret_info['other_count']  = $ret_info['other_count']/100;
-        return $this->output_succ(["lesson_info"=>$ret_info]);
-    }
-
-
-    public function get_teacher_student(){//p4
-        // $teacherid = $this->get_in_int_val("teacherid");
-        // $teacherid = $this->get_teacherid();
-        $teacherid = 108226;
-        if (!$teacherid) {
-            return $this->output_err("信息有误，未查询到老师信息！");
-        }
-        $end_time   = strtotime(date("Y-m-01",time()));
-        $start_time = strtotime("-1 month",$end_time);
-        $ret_info   = $this->t_teacher_info->get_student_by_teacherid($teacherid,$start_time, $end_time);
-        $face       = [];
-        foreach ($ret_info as $item) {
-            $face[] = @$item['face'];
-        }
-        $stu_info['stu_num'] = count($ret_info);
-        $stu_info['face']    = $face;
-        return $this->output_succ(["stu_info"=>$stu_info]);
-    }
-
-    public function get_tea_lesson_some_info(){//p5
-        $teacherid = $this->get_in_int_val("teacherid");
-        // $teacherid = $this->get_wx_teacherid();
-        // $teacherid = 18011110026;
-        $teacherid = 108226;
-        if (!$teacherid) {
-            return $this->output_err("信息有误，未查询到老师信息！");
-        }
-        $end_time   = strtotime(date("Y-m-01",time()));
-        $start_time = strtotime("-1 month",$end_time);
-        $ret_info = $this->t_teacher_info->get_teacher_lesson_detail($teacherid,$start_time, $end_time);
-        foreach ($ret_info as &$item) {
-            $item = intval($item);
-        }
-        return $this->output_succ(["list"=>$ret_info]);
-    }
 
 
     //获取某个月在读学生，上课-堂数——人数
@@ -388,14 +337,19 @@ class test_boby extends Controller
     }
     //添加给老师添加公开课学生
     public function add_stu_to_tea_open_lesson(){
-        $start_time = strtotime('2017-08-05');
-        $end_time = strtotime('2017-09-01');
-        $userid_list = $this->t_order_info->get_userid_by_pay_time($start_time, $end_time);
+        // $start_time = strtotime('2017-08-05');
+        // $end_time = strtotime('2017-09-01');
+        // $userid_list = $this->t_order_info->get_userid_by_pay_time($start_time, $end_time);
 
         $teacherid = "(55161,176999)";
         $start_time = strtotime('2017-09-01');
         $end_time = strtotime('2017-10-01');
         $lessonid_list = $this->t_lesson_info_b2->get_lessonid_by_teacherid($start_time, $end_time, $teacherid);
+
+        foreach ($lessinid_list as $v) {
+            $this->t_open_lesson_user->delete_open_lesson_by_lessonid( $v['lessonid'] );
+        }
+        exit;
         $g200 = [];
         $g300 = [];
         foreach($lessonid_list as $v){
--- app/Models/t_order_info.php
+++ app/Models/t_order_info.php
@@ -2832,7 +2832,7 @@ class t_order_info extends \App\Models\Zgen\z_t_order_info
             ." from %s o"
             ." left join %s s on o.userid=s.userid"
             ." where %s"
-            // ." group by s.userid"
+            ." group by s.userid"
             ,self::DB_TABLE_NAME
             ,t_student_info::DB_TABLE_NAME
             ,$where_arr
