19c18548285ce02dcb3d9412016ecbfd4a9dcb8d
jim <jim@leoedu.com>
2017-09-01 14:13:48 +0800

    fix

--- app/Http/Controllers/testbb.php
+++ app/Http/Controllers/testbb.php
@@ -131,8 +131,7 @@ class testbb extends Controller
         $pdf_file_path = 'http:\/\/7tszue.com2.z0.glb.qiniucdn.com\/2d5e65b05f28090c07f9b1e994b1e7151504012597909.pdf?e=1504239357&token=yPmhHAZNeHlKndKBLvhwV3fw4pzNBVvGNU5ne6Px:SXTiWrNfY_mRJajzzUjXn6Sxcd4=';
         // $this->set_in_value('file_url',$file_url);
         // return $this->get_pdf_download_url();
-        $ss = basename($pdf_url);
-        dd($ss);
+        $pdf_url = 111111111;
 
         $savePathFile = public_path('wximg').'/'.$pdf_url;
 
