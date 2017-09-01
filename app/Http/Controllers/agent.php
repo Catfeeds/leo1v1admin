bb6efe786504c66c96677d8845cda492b793aba8
tom <tom@leoedu.com>
2017-09-01 19:01:44 +0800

    fix

--- app/Http/Controllers/agent.php
+++ app/Http/Controllers/agent.php
@@ -231,7 +231,7 @@ class agent extends Controller
     }
 
     public function check(){
-        $this->t_agent->reset_user_info($id=1762);
+        $this->t_agent->reset_user_info_new($id=1762);
         // $this->agent_add();
         // $wx_openid = 'oAJiDwBbbqiTwnU__f6ce5tNpWYs';
         // $template_id = 'zZ6yq8hp2U5wnLaRacon9EHc26N96swIY_9CM8oqSa4';
