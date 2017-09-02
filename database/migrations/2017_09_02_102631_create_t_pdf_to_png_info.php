febe1a339eb433acd0785070d975c6de61aa789a
jim <jim@leoedu.com>
2017-09-02 16:08:52 +0800

    fix

--- database/migrations/2017_09_02_102631_create_t_pdf_to_png_info.php
+++ database/migrations/2017_09_02_102631_create_t_pdf_to_png_info.php
@@ -14,19 +14,19 @@ class CreateTPdfToPngInfo extends Migration
     {
         //
 
-        Schema::create('db_weiyi.t_pdf_to_png_info', function( Blueprint $table)
+        Schema::create('db_weiyi.t_wx_share', function( Blueprint $table)
         {
             $table->increments("id");
 
-            t_field($table->integer("lessonid"),"课程id");
-            t_field($table->integer("id_do_flag"),"是否处理 0:未处理 1:已处理");
-            t_field($table->integer("create_time"),"添加时间");
-            t_field($table->integer("deal_time"),"处理时间");
-            t_field($table->string("pdf_url"),'pdf课件链接');
+            t_field($table->integer("teacherid"),"老师id");
+            t_field($table->integer("type"),"是否处理 0:未处理 1:已处理");
+            // t_field($table->integer("create_time"),"添加时间");
+            // t_field($table->integer("deal_time"),"处理时间");
+            // t_field($table->string("pdf_url"),'pdf课件链接');
 
-            $table->index(["lessonid"]);
-            $table->index(["create_time"]);
-            $table->index(["deal_time"]);
+            // $table->index(["lessonid"]);
+            // $table->index(["create_time"]);
+            // $table->index(["deal_time"]);
         });
 
     }
