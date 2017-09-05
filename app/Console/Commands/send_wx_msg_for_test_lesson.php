a40ebf9e2ceab274be81b1f1429eaad20ef4c3cf
jim <jim@leoedu.com>
2017-09-05 15:11:27 +0800

    fix

--- app/Console/Commands/send_wx_msg_for_test_lesson.php
+++ app/Console/Commands/send_wx_msg_for_test_lesson.php
@@ -138,21 +138,23 @@ class send_wx_msg_for_test_lesson extends Command
 
         // 旷课
 
-        foreach($absenteeism_lesson_list as $index=>$item){
-            $logout_time_tea = $task->t_lesson_opt_log->get_logout_time($item['lessonid'],$item['teacherid']);
-            $logout_time_stu = $task->t_lesson_opt_log->get_logout_time($item['lessonid'],$item['userid']);
+        if(!empty($absenteeism_lesson_list)){
+            foreach($absenteeism_lesson_list as $index=>$item){
+                $logout_time_tea = $task->t_lesson_opt_log->get_logout_time($item['lessonid'],$item['teacherid']);
+                $logout_time_stu = $task->t_lesson_opt_log->get_logout_time($item['lessonid'],$item['userid']);
 
-            if(!$logout_time_tea || $logout_time_tea<$item['lesson_start']){
-                $data_ass = $this->get_data($item,3,4,$item['teacher_nick'],'');
-                $this->send_wx_msg_ass($item,4,$data_ass);
+                // if(!$logout_time_tea || $logout_time_tea<$item['lesson_start']){
+                    $data_ass = $this->get_data($item,3,4,$item['teacher_nick'],'');
+                    $this->send_wx_msg_ass($item,4,$data_ass);
+                // }
+
+                // if(!$logout_time_stu || $logout_time_stu<$item['lesson_start']){
+                    $data_ass = $this->get_data($item,3,4,'',$item['stu_nick']);
+                    $this->send_wx_msg_ass($item,4,$data_ass);
+                // }
+
             }
-
-            if(!$logout_time_stu || $logout_time_stu<$item['lesson_start']){
-                $data_ass = $this->get_data($item,3,4,'',$item['stu_nick']);
-                $this->send_wx_msg_ass($item,4,$data_ass);
-            }
-
-        }
+       }
         /*
 
         // 试听课正常结束
