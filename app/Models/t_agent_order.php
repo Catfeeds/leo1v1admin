3458483b25636027672f890ab7c253c125e7bfa3
tom <tom@leoedu.com>
2017-09-05 18:28:25 +0800

    fix

--- app/Models/t_agent_order.php
+++ app/Models/t_agent_order.php
@@ -12,7 +12,7 @@ class t_agent_order extends \App\Models\Zgen\z_t_agent_order
     {
         $where_arr = [];
         if($start_time && $end_time){
-            $this->where_arr_add_time_range($where_arr,'a.create_time',$start_time,$end_time);
+            $this->where_arr_add_time_range($where_arr,'ao.create_time',$start_time,$end_time);
         }
         $sql = $this->gen_sql_new ("select ao.*,"
                                    ." o.price "
