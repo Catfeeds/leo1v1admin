82fd40a912ef37cc1e828dce462b8c4b7616d4fe
jim <jim@leoedu.com>
2017-09-05 17:23:02 +0800

    fix

--- database/migrations/2017_09_05_165658_create_t_student_subject_list.php
+++ database/migrations/2017_09_05_165658_create_t_student_subject_list.php
@@ -16,7 +16,7 @@ class CreateTStudentSubjectList extends Migration
         Schema::create('db_weiyi.t_student_subject_list', function( Blueprint $table)
         {
             t_field($table->integer("userid"),"学生");
-            t_field($table->integer("subejct"),"科目");
+            t_field($table->integer("subject"),"科目");
             t_field($table->integer("editionid"),"教材版本");
             $table->primary(["userid","subject"]);
 
