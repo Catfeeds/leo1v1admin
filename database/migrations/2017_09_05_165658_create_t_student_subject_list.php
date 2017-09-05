502731ab63be413c67fa4c849a74849f370e755b
jim <jim@leoedu.com>
2017-09-05 17:01:01 +0800

    fix

--- database/migrations/2017_09_05_165658_create_t_student_subject_list.php
+++ database/migrations/2017_09_05_165658_create_t_student_subject_list.php
@@ -13,6 +13,14 @@ class CreateTStudentSubjectList extends Migration
     public function up()
     {
         //
+        Schema::create('db_weiyi.t_student_subject_list', function( Blueprint $table)
+        {
+            t_field($table->integer("userid"),"学生");
+            t_field($table->integer("subejct"),"科目");
+
+
+        });
+
     }
 
     /**
