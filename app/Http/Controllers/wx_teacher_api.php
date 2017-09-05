493ceb724ad8ca7bbee910a36f0fb23a3bfcaded
jim <jim@leoedu.com>
2017-09-05 18:26:49 +0800

    fix

--- app/Http/Controllers/wx_teacher_api.php
+++ app/Http/Controllers/wx_teacher_api.php
@@ -492,6 +492,12 @@ class wx_teacher_api extends Controller
 
     public function teacher_day_luck_draw(){ //教师节抽奖活动//
 
+
+        /*
+
+
+        **/
+
         $teacherid = $this->get_teacherid();
 
         // 计算目前的奖金总额
