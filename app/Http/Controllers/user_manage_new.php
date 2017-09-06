4961b0859f684a16e403f92e3a48d332e5313488
tom <tom@leoedu.com>
2017-09-06 14:57:15 +0800

    fix

--- app/Http/Controllers/user_manage_new.php
+++ app/Http/Controllers/user_manage_new.php
@@ -1424,6 +1424,7 @@ class user_manage_new extends Controller
                 $item['leave_member_num'] = '';
             }
         }
+        dd($ret_info);
         \App\Helper\Utils::logger("OUTPUT");
 
         return $this->pageView(__METHOD__,\App\Helper\Utils::list_to_page_info($ret_info));
