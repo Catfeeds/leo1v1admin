e70da3fbeaccf6c4cc3228afccf7e06e63ecd862
jim <jim@leoedu.com>
2017-09-12 16:00:28 +0800

    fix

--- app/Models/t_child_order_info.php
+++ app/Models/t_child_order_info.php
@@ -8,6 +8,11 @@ class t_child_order_info extends \App\Models\Zgen\z_t_child_order_info
 		parent::__construct();
 	}
 
+    public function get_all_child_order_info($orderid){
+        $sql = $this->gen_sql_new("select * from %s where parent_orderid = %u",self::DB_TABLE_NAME,$orderid);
+        return $this->main_get_list($orderid);
+    }
+
 }
 
 
