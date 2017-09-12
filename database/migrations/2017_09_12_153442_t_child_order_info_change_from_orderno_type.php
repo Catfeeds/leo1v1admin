83d362617319a7d785d38f1e119393e53077a141
jim <jim@leoedu.com>
2017-09-12 15:36:13 +0800

    fix

--- database/migrations/2017_09_12_153442_t_child_order_info_change_from_orderno_type.php
+++ database/migrations/2017_09_12_153442_t_child_order_info_change_from_orderno_type.php
@@ -13,6 +13,16 @@ class TChildOrderInfoChangeFromOrdernoType extends Migration
     public function up()
     {
         //
+        Schema::table('db_weiyi.t_child_order_info', function( Blueprint $table)
+        {
+            $table->dropColumn("from_orderno");
+        });
+        Schema::table('db_weiyi.t_child_order_info', function( Blueprint $table)
+        {
+            t_field($table->string("from_orderno") ->nullable(), "第三方订单id");
+            $table->unique("from_orderno");
+        });
+
     }
 
     /**
