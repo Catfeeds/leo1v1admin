c4633bbedc0cef70afe7d8a0340d454ba0018c2c
sam <sam@leoedu.com>
2017-09-12 10:09:07 +0800

    fix

--- app/Models/t_teacher_lecture_appointment_info.php
+++ app/Models/t_teacher_lecture_appointment_info.php
@@ -1128,15 +1128,18 @@ class t_teacher_lecture_appointment_info extends \App\Models\Zgen\z_t_teacher_le
             "s.type=12",//第2次面试
             "s.trial_train_status =1",
             ["s.add_time>%u",$start_time,-1],
-            ["s.add_time<%u",$end_time,-1]
+            ["s.add_time<%u",$end_time,-1],
+            "m.phone>0",
         ];
         $sql = $this->gen_sql_new("select count(distinct(l.phone)) as through_num"
                                   ." from %s l "
                                   ." left join %s t on t.phone = l.phone"
+                                  ." left join %s m on l.phone = m.phone "
                                   ." left join %s s on s.teacherid  = t.teacherid"
                                   ." where %s "
                                   ,self::DB_TABLE_NAME
                                   ,t_teacher_info::DB_TABLE_NAME
+                                  ,t_manager_info::DB_TABLE_NAME
                                   ,t_teacher_record_list::DB_TABLE_NAME
                                   ,$where_arr);
         return $this->main_get_value($sql);
