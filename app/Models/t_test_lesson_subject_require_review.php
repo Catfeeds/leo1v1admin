846b369e3e4a2d73aaf67fe70e982fb9166ed828
tom <tom@leoedu.com>
2017-09-12 13:32:58 +0800

    fix

--- app/Models/t_test_lesson_subject_require_review.php
+++ app/Models/t_test_lesson_subject_require_review.php
@@ -10,10 +10,13 @@ class t_test_lesson_subject_require_review extends \App\Models\Zgen\z_t_test_les
     public function get_all_list($page_info){
         $where_arr = [
         ];
-        $sql=$this->gen_sql_new (" select *"
-                                 ." from %s "
+        $sql=$this->gen_sql_new (" select r.*,"
+                                 ." s.phone,s.nick "
+                                 ." from %s r "
+                                 ." left join %s s on s.userid = r.userid"
                                  ." where %s "
                                  ,self::DB_TABLE_NAME
+                                 ,t_student_info::DB_TABLE_NAME
                                  ,$where_arr
         );
         return $this->main_get_list_by_page($sql,$page_info);
--- public/page_ts/test_lesson_review/test_lesson_review_list.ts
+++ public/page_ts/test_lesson_review/test_lesson_review_list.ts
@@ -7,6 +7,31 @@ $(function(){
 
         });
     }
+    $(".opt-edit").on("click",function(){
+        var opt_data = $(this).get_opt_data();
+        var $group_suc_flag = $("<select/>");
+        var $master_suc_flag = $("<select/>");
+        Enum_map.append_option_list("group_suc_flag",$group_suc_flag ,true );
+        Enum_map.append_option_list("master_suc_flag",$master_suc_flag ,true );
+        $group_suc_flag.val(opt_data.group_suc_flag);
+        $master_suc_flag.val(opt_data.master_suc_flag);
+        var arr=[
+            ["组长审核",  $group_suc_flag],
+            ["主管审核",  $master_suc_flag],
+        ];
+
+        $.show_key_value_table("排课审核", arr ,{
+            label: '确认',
+            cssClass: 'btn-warning',
+            action: function(dialog) {
+                $.do_ajax("/test_lesson_review/test_lesson_review_edit",{
+                    "id":opt_data.id,
+                    "group_suc_flag" : $group_suc_flag.val(),
+                    "master_suc_flag" : $master_suc_flag.val()
+                })
+            }
+        })
+    });
 
     $('.opt-change').set_input_change_event(load_data);
 });
