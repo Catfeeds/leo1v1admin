ba1d98e5ee553a629f47b53be6a8374c4ac731ba
jim <jim@leoedu.com>
2017-09-13 18:33:54 +0800

    fix

--- app/Console/Commands/get_ass_stu_info_update.php
+++ app/Console/Commands/get_ass_stu_info_update.php
@@ -129,21 +129,14 @@ class get_ass_stu_info_update extends Command
 
         $xq_revisit_second = $task->t_revisit_info->get_ass_xq_revisit_info_new($month_middle,$end_time,$userid_list_second_all,false);
 
-        $warning_info    = $task->t_month_ass_student_info->get_ass_month_info($start_time);
-
-
-        $last_month  = strtotime(date('Y-m-01',$start_time-100));
-        $ass_last_month    = $task->t_month_ass_student_info->get_ass_month_info($last_month,-1,1);
-        $assistant_renew_list = $task->t_manager_info->get_all_assistant_renew_list_new($start_time,$end_time);
-
         $new_info = $task->t_student_info->get_ass_new_stu_first_revisit_info($start_time,$end_time);
         $new_revisit=[];
-        foreach($new_info as $v){
-            @$new_revisit[$v["uid"]]["new_num"]++;
-            if($v["revisit_time"]>0){
-                @$new_revisit[$v["uid"]]["first_num"]++;
+        foreach($new_info as $vu){
+            @$new_revisit[$vu["uid"]]["new_num"]++;
+            if($vu["revisit_time"]>0){
+                @$new_revisit[$vu["uid"]]["first_num"]++;
             }else{
-                @$new_revisit[$v["uid"]]["un_first_num"]++;
+                @$new_revisit[$vu["uid"]]["un_first_num"]++;
             }
         }
 
@@ -154,14 +147,6 @@ class get_ass_stu_info_update extends Command
         foreach ($student_finish as $key => $value) {  
             $student_finish_detail[$value['uid']] = $value['num']; 
         }
-        /*
-          $student_all = $task->t_student_info->get_ass_first_revisit_info();//在册学生数
-          $student_all_detail = [];
-          foreach ($student_all as $key => $value) {  
-          $student_all_detail[$value['uid']] = $value['num']; 
-          }
-        */
-        //dd($new_revisit);
         $refund_score = $task->get_ass_refund_score($start_time,$end_time);
 
         $lesson_money_all = $task->t_manager_info->get_assistant_lesson_money_info_all($start_time,$end_time);
