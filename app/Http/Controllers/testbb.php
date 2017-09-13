73e0a46cd6ec29ecfabffa1d5874d1f25318dff6
jim <jim@leoedu.com>
2017-09-13 18:12:28 +0800

    f

--- app/Http/Controllers/testbb.php
+++ app/Http/Controllers/testbb.php
@@ -262,17 +262,13 @@ class testbb extends Controller
         Schema::create('db_weiyi.t_admin_manager_group_name', function( Blueprint $table)
         {
             $table->increments("groupid");
-            t_field($table->tinyInteger("child_order_type"),"子合同类型");
-            t_field($table->tinyInteger("pay_status"),"付款状态 0,未付;1,已付");
-            t_field($table->integer("add_time"),"创建时间");
-            t_field($table->integer("pay_time"),"付款时间");
-            t_field($table->integer("parent_orderid"),"父合同id");
-            t_field($table->integer("price"),"合同金额");
-            t_field($table->string("channel"),"付款渠道");
-            t_field($table->string("from_orderno")->nullable(),"第三方订单id");
-            $table->unique("from_orderno");
-            $table->index("add_time");
-            $table->index("parent_orderid");
+            t_field($table->integer("main_type"),"部门类型");
+            t_field($table->string("group_name"),"组名");
+            t_field($table->integer("master_adminid"),"总监id");
+            t_field($table->string("main_assign_percent"),"组名");
+            t_field($table->integer("campus_id"),"校区id");
+
+            $table->index(["main_type","groupid"]);
         });
 
     }
