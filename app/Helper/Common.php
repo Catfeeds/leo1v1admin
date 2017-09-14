0c38e015f383188f0dc98731c02808dc01bdec51
jim <jim@leoedu.com>
2017-09-14 12:10:01 +0800

    fix

--- app/Helper/Common.php
+++ app/Helper/Common.php
@@ -1293,11 +1293,15 @@ class Common {
                     && ($self_flag || !in_array( $k,$no_need_sum_list ) )  ) {
                     if ($self_flag) {
                         $arr[$k]=$v;
+                        \App\Helper\Utils::logger(" first11 $k ~ $v");
+
                     }else{
                         if (!isset($arr[$k])) {
                             $arr[$k]=0;
                         }
                         $arr[$k]+=$v;
+                        \App\Helper\Utils::logger(" first22 $k ~ $v");
+
                     }
                 }
             }
