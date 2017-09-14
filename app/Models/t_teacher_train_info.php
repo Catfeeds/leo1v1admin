94e7a520b5905a79859fa56eb3592c22ae933bef
sam <sam@leoedu.com>
2017-09-14 16:07:24 +0800

    fix

---
+++ app/Models/t_teacher_train_info.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Models;
+use \App\Enums as E;
+class t_teacher_train_info extends \App\Models\Zgen\z_t_teacher_train_info
+{
+	public function __construct()
+	{
+		parent::__construct();
+	}
+}
