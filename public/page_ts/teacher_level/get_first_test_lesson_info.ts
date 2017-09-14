b36d99ac004097efcdac9234349c21ad02ecddb0
sam <sam@leoedu.com>
2017-09-14 12:23:56 +0800

    fix

--- public/page_ts/teacher_level/get_first_test_lesson_info.ts
+++ public/page_ts/teacher_level/get_first_test_lesson_info.ts
@@ -233,8 +233,6 @@ $(function(){
                         train_type.push($(this).val());
                     });
 
-                    alert(train_type);
-                    alert(JSON.stringify(train_type));
                     $.do_ajax("/teacher_level/set_teacher_record_info",{
                         "teacherid"    : teacherid,
                         "lesson_invalid_flag"    : id_lesson_invalid_flag.val(),
