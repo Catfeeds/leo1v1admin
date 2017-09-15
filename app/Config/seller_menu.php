c5d1b4b24c11c9822fe2aa02deb2a171c66fd7a7
tom <tom@leoedu.com>
2017-09-15 10:29:38 +0800

    fix

--- app/Config/seller_menu.php
+++ app/Config/seller_menu.php
@@ -38,8 +38,8 @@ class seller_menu{
                     ["power_id"=>70, "name"=>"教学质量反馈列表",   "url"=>"/tea_manage_new/get_seller_ass_record_info_seller"],
                 ]],
                 ["power_id"=>14, "name"=>"合同", "list"=>[
-                    ["power_id"=>1, "name"=>"合同-待付费",   "url"=>"/user_manage/contract_list_seller_add"],
-                    ["power_id"=>2, "name"=>"合同-已付费",   "url"=>"/user_manage/contract_list_seller_payed"],
+                    // ["power_id"=>1, "name"=>"合同-待付费",   "url"=>"/user_manage/contract_list_seller_add"],
+                    // ["power_id"=>2, "name"=>"合同-已付费",   "url"=>"/user_manage/contract_list_seller_payed"],
                     // ["power_id"=>22, "name"=>"合同",   "url"=>"/user_manage/contract_list_seller_mix"],
                     ["power_id"=>22, "name"=>"合同",   "url"=>"/user_manage_new/contract_list_seller_payed_new"],
                     // ["power_id"=>29, "name"=>"合同-已付费-主管",   "url"=>"/user_manage_new/contract_list_seller_payed_new"],
