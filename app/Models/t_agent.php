b208e2bf37a5e25856b0059722ae836755edd2c0
jim <jim@leoedu.com>
2017-09-16 19:06:40 +0800

    fix

--- app/Models/t_agent.php
+++ app/Models/t_agent.php
@@ -1494,8 +1494,8 @@ class t_agent extends \App\Models\Zgen\z_t_agent
         $child_order_count= $level_count_info["l1_order_count"] +$level_count_info["l2_order_count"];
 
         //总提成信息
-        $all_yxyx_money      = $order_all_money +  $l1_agent_status_all_money;
-        $all_open_cush_money = $order_open_all_money +  $l1_agent_status_all_open_money;
+        $all_yxyx_money      = $order_all_money +  $l1_agent_status_all_money+ $l2_agent_status_all_money;
+        $all_open_cush_money = $order_open_all_money +  $l1_agent_status_all_open_money+ $l2_agent_status_all_open_money;
         $all_have_cush_money = $this->task->t_agent_cash->get_have_cash($id);
 
 
