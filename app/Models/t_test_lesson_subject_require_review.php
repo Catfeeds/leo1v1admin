69874df592776f5cf8712e7292c71030425a2a31
tom <tom@leoedu.com>
2017-09-16 15:38:55 +0800

    fix

--- app/Models/t_test_lesson_subject_require_review.php
+++ app/Models/t_test_lesson_subject_require_review.php
@@ -41,9 +41,8 @@ class t_test_lesson_subject_require_review extends \App\Models\Zgen\z_t_test_les
     }
 
     public function get_week_test_lesson_count($adminid,$start_time,$end_time){
-        $where_arr = [
-            ['adminid',$adminid,-1],
-        ];
+        $where_arr = [];
+        $this->where_arr_add_int_field($where_arr,'adminid',$adminid);
         $this->where_arr_add_time_range($where_arr,'create_time',$start_time,$end_time);
         $sql = $this->gen_sql_new(
             " select count(id) "
@@ -55,16 +54,5 @@ class t_test_lesson_subject_require_review extends \App\Models\Zgen\z_t_test_les
         return $this->main_get_value($sql);
     }
 
-    public function get_list_by_adminid($adminid){
-        $where_arr = [];
-        $this->where_arr_add_int_field($where_arr,'adminid',$adminid);
-        $sql=$this->gen_sql_new (" select * "
-                                 ." from %s "
-                                 ." where %s "
-                                 ,self::DB_TABLE_NAME
-                                 ,$where_arr
-        );
-        return $this->main_get_list($sql);
-    }
 
 }
