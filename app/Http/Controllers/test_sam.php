566e53de9cf8edf022affc2c2f7d4baa22957841
sam <sam@leoedu.com>
2017-09-20 19:05:00 +0800

    fix

--- app/Http/Controllers/test_sam.php
+++ app/Http/Controllers/test_sam.php
@@ -90,12 +90,15 @@ class test_sam  extends Controller
           }
           $arr = [];
           foreach ($list as $key => $value) {
-              $arr[$key]['teacherid'] = $key;
-              $arr[$key]['realname']  = $this->t_teacher_info->get_realname($teacherid);
-              $arr[$key]['lesson_count'] = $value;
-              $arr[$key]['day_num'] = floor($value/10.5);
-              $arr[$key]['attendance_time'] = 1507478400;//2017/10/9 0:0:0
-              $arr[$key]['cross_time'] = "10.9-".date('m-d',1507478400+$arr[$key]['day_num']*86400);
+              @$arr[$key]['teacherid'] = $key;
+              $teacher_info = $this->t_manager_info->get_teacher_info_by_adminid($key);                   
+              $teacherid = $teacher_info["teacherid"];
+              $realname = $this->t_teacher_info->get_realname($teacherid);
+              @$arr[$key]['realname']  = $this->t_teacher_info->get_realname($teacherid);
+              @$arr[$key]['lesson_count'] = $value;
+              @$arr[$key]['day_num'] = floor($value/10.5);
+              @$arr[$key]['attendance_time'] = 1507478400;//2017/10/9 0:0:0
+              @$arr[$key]['cross_time'] = "10.9-".date('m-d',1507478400+$arr[$key]['day_num']*86400);
           }
           dd($arr);
 
