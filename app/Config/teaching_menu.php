8352b346c3cd5bde5a7c1a7601dea496125947eb
boby <boby@leoedu.com>
2017-09-22 16:00:14 +0800

    fix

--- app/Config/teaching_menu.php
+++ app/Config/teaching_menu.php
@@ -132,6 +132,8 @@ class teaching_menu{
                         [ "name"=>"考勤信息",   "url"=>"/user_manage_new/get_fulltime_teacher_attendance_info"],
                         ["name"=>"试听课转化详情-教研", "url"=>"/tongji_ss/research_teacher_lesson_detail_info"],
                         [ "name"=>"投诉老师列表",   "url"=>"/tea_manage_new/get_teacher_complaints_info"],
+                        [ "name"=>"投诉处理-家长",   "url"=>"/user_manage/complaint_department_deal_parent_tea"],
+                        [ "name"=>"兼职老师薪资处理",   "url"=>"/user_manage/complaint_department_deal_teacher_tea"],
 
                         [ "name"=>"试听转化率模型",  "list"=>[
                             [ "name"=>"试听转化率黑名单",   "url"=>"/tongji_ss/get_test_lesson_low_tra_teacher"],
