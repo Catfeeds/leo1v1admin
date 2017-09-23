10a94d53497d9119a8687fc6b47b1fd244e48aa4
jim <jim@leoedu.com>
2017-09-23 21:03:38 +0800

    fix

--- app/Http/Controllers/test_code.php
+++ app/Http/Controllers/test_code.php
@@ -2014,18 +2014,18 @@ class test_code extends Controller
          * {{remark.DATA}}
          */
         $template_id      = "E9JWlTQUKVWXmUUJq_hvXrGT3gUvFLN6CjYE1gzlSY0";
-        $data['keyword3'] = "19:30";
+        $data['keyword3'] = "21:00";
         $data['remark']   = "感谢您长期以来对理优平台的辛劳付出与长久陪伴！";
         foreach($list as $val){
             $this->t_teacher_info->field_update_list($val['teacherid'],[
                 // "teacher_money_type" => $val['teacher_money_type_simulate'],
                 // "level"              => $val['level_simulate'],
-                "teacher_money_type" => $val['teacher_money_type_simulate'],
-                "level"              => $val['level_simulate'],
+                "teacher_money_type" => 6,
+                "level"              => 1,
             ]);
 
             if($val['wx_openid']!=""){
-                $level_str = E\Enew_level::v2s($val['level_simulate']);
+                $level_str = E\Enew_level::v2s(1);
                 $data['first'] = "恭喜您，您等级已经调整为".$level_str;
                 $data['keyword1'] = mb_substr($val['realname'],0,1)."老师";
                 $data['keyword2'] = $level_str;
