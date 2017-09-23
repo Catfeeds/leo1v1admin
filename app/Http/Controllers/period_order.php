b69128982b21370b93328dbefeffd6bb8541ac5c
jim <jim@leoedu.com>
2017-09-23 14:28:07 +0800

    fix

--- app/Http/Controllers/period_order.php
+++ app/Http/Controllers/period_order.php
@@ -13,7 +13,12 @@ class period_order extends Controller
     use TeaPower;
 
     public function get_all_payed_order_info(){
-         
+        list($start_time, $end_time)=$this->get_in_date_range(0,0,0,[],3);
+        $pay_status = $this->get_in_int_val("pay_status");
+        $page_info = $this->get_in_page_info();
+        
+        $list = $this->t_child_order_info->get_all_period_order_info($start_time,$end_time,$page_info,$pay_status);
+  
     }
 
 }
