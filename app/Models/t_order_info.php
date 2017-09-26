24c1f7fac89c6b0653f9b0ee7f785f944137bf36
tom <tom@leoedu.com>
2017-09-26 10:30:55 +0800

    fix

--- app/Models/t_order_info.php
+++ app/Models/t_order_info.php
@@ -1510,17 +1510,21 @@ class t_order_info extends \App\Models\Zgen\z_t_order_info
                                   ." sum(if(stu_from_type=0,price,0)) new_price,"
                                   ." sum(if(stu_from_type=10,price,0)) normal_price,"
                                   ." sum(if(stu_from_type=11,price,0)) extend_price, "
-                                  ." sum(if(t1.check_money_flag=1,price,0)) all_price_suc,"
-                                  ." sum(if(t1.check_money_flag=0,price,0)) all_price_fail"
+                                  // ." sum(if(t1.check_money_flag=1,price,0)) all_price_suc,"
+                                  // ." sum(if(t1.check_money_flag=0,price,0)) all_price_fail"
+                                  ." sum(if(co.parent_orderid and co.pay_status=1,co.price,if(t1.check_money_flag=1,price,0))) all_price_suc,"
+                                  ." sum(if(co.parent_orderid and co.pay_status=0,co.price,if(t1.check_money_flag=0,price,0)) all_price_fail"
                                   ." from %s t1 "
                                   ." left join %s t2 on t1.userid = t2.userid "
                                   ." left join %s t3 on t1.sys_operator = t3.account "
                                   ." left join %s c on t1.orderid = c.orderid "
+                                  ." left join %s co on co.parent_orderid = t1.orderid "
                                   ." where %s group by t1.sys_operator ",
                                   self::DB_TABLE_NAME,
                                   t_student_info::DB_TABLE_NAME,
                                   t_manager_info::DB_TABLE_NAME,
                                   t_course_order::DB_TABLE_NAME,
+                                  t_child_order_info::DB_TABLE_NAME,
                                   $where_arr
         );
         return $this->main_get_list_as_page($sql,function($item){
