d2e8113b044f73fc92253e5ead2570c834e896e6
jim <jim@leoedu.com>
2017-09-26 18:30:13 +0800

    fix

--- app/Config/teaching_menu.php
+++ app/Config/teaching_menu.php
@@ -70,6 +70,7 @@ class teaching_menu{
                         [ "name"=>"助教换老师统计",   "url"=>"/tongji_ss/get_change_teacher_info"],
                         ["name"=>"老师推荐申请",   "url"=>"/tea_manage_new/get_seller_require_commend_teacher_info_yy"],
                         [ "name"=>"投诉处理-QC",   "url"=>"/user_manage/qc_complaint_tea"],
+                        [ "name"=>"微信推送",   "url"=>"/user_manage_new/wx_monitor_new_yy"],
                     ]],
 
 
