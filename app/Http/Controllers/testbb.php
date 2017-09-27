fee8329af3589233b016e2193d7ff87d08347ab7
jim <jim@leoedu.com>
2017-09-27 16:21:47 +0800

    fix

--- app/Http/Controllers/testbb.php
+++ app/Http/Controllers/testbb.php
@@ -238,6 +238,7 @@ class testbb extends Controller
 
 
     public function get_num(){
+        $this->switch_tongji_database();
         $teacherid = $this->t_lesson_info_b3->get_on_teacherid();
     }
 
