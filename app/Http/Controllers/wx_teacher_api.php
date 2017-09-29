a55bfe345846639045c44c4dc29dcdc04caa841a
jim <jim@leoedu.com>
2017-09-29 17:52:31 +0800

    fix

--- app/Http/Controllers/wx_teacher_api.php
+++ app/Http/Controllers/wx_teacher_api.php
@@ -588,19 +588,15 @@ class wx_teacher_api extends Controller
         foreach($b as $val){
             $begin_time = strtotime($val);
             $end_time   = $begin_time+86400;
-            $tea_time   = $this->t_lesson_info_b2->get_teacher_time_by_lessonid($lessonid, $begin_time, $end_time);
-            array_push($time_info['teacher_lesson_time'],$tea_time);
-            // foreach($tea_time as $v){
-                
-            // }
+            $tea_time[] = $this->t_lesson_info_b2->get_teacher_time_by_lessonid($lessonid, $begin_time, $end_time);
         }
 
-        // foreach($tea_time as $v){
-        //     foreach($v as $vv){
-        //         $teacher_time[] = $vv;
-        //     }
-        // }
-        dd( $time_info['teacher_lesson_time']);
+        foreach($tea_time as $v){
+            foreach($v as $vv){
+                $teacher_time[] = $vv;
+            }
+        }
+        $time_info['teacher_lesson_time'] = $teacher_time;
 
         $time_info['has_do'] = 0;  // 未处理
         return $this->output_succ(['data'=>$time_info]);
@@ -618,27 +614,27 @@ class wx_teacher_api extends Controller
             $ret_info['has_do'] = 1;// 已处理
             return $this->output_succ(['data'=>$ret_info]);
         }else{
-            $time_info = $this->t_lesson_info_b3->get_lesson_info($lessonid);
-            $time_info['subject'] = E\Esubject::get_desc($time_info['subject']);
-            $time_info['grade'] = E\Egrade::get_desc($time_info['grade']);
-            $time_info['parent_modify_time']  = $this->t_lesson_time_modify->get_parent_modify_time($lessonid);
+            // $time_info = $this->t_lesson_info_b3->get_lesson_info($lessonid);
+            // $time_info['subject'] = E\Esubject::get_desc($time_info['subject']);
+            // $time_info['grade'] = E\Egrade::get_desc($time_info['grade']);
+            // $time_info['parent_modify_time']  = $this->t_lesson_time_modify->get_parent_modify_time($lessonid);
 
-            $date_modify = json_decode($time_info['parent_modify_time'],true);
-            $day_date = [];
-            foreach($date_modify as $item){
-                $day_date[] = date('Y-m-d',$item);
-            }
-            $b = array_flip(array_flip($day_date));
-            $time_info['teacher_lesson_time'] = [];
-            foreach($b as $val){
-                $begin_time = strtotime($val);
-                $end_time   = $begin_time+86400;
-                $teacher_time = $this->t_lesson_info_b2->get_teacher_time_by_lessonid($lessonid, $begin_time, $end_time);
-                array_merge($time_info['teacher_lesson_time'],$teacher_time);
-            }
+            // $date_modify = json_decode($time_info['parent_modify_time'],true);
+            // $day_date = [];
+            // foreach($date_modify as $item){
+            //     $day_date[] = date('Y-m-d',$item);
+            // }
+            // $b = array_flip(array_flip($day_date));
+            // $time_info['teacher_lesson_time'] = [];
+            // foreach($b as $val){
+            //     $begin_time = strtotime($val);
+            //     $end_time   = $begin_time+86400;
+            //     $teacher_time = $this->t_lesson_info_b2->get_teacher_time_by_lessonid($lessonid, $begin_time, $end_time);
+            //     array_merge($time_info['teacher_lesson_time'],$teacher_time);
+            // }
 
-            $time_info['has_do'] = 0;  // 未处理
-            return $this->output_succ(['data'=>$time_info]);
+            // $time_info['has_do'] = 0;  // 未处理
+            // return $this->output_succ(['data'=>$time_info]);
         }
     }
 
