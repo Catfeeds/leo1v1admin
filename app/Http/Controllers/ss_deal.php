d446e322fe529b4b4103679a3d6ef37ba1880531
jim <jim@leoedu.com>
2017-09-29 18:19:42 +0800

    f

--- app/Http/Controllers/ss_deal.php
+++ app/Http/Controllers/ss_deal.php
@@ -2841,12 +2841,7 @@ class ss_deal extends Controller
         $origin_assistant_nick = $this->cache_get_account_nick($origin_assistantid);
 
         $origin_nick=$this->cache_get_student_nick($origin_userid);
-        $this->t_book_revisit->add_book_revisit(
-            $phone,
-            "操作者: $account , 负责人: [$origin_assistant_nick] 转介绍  来自:[$origin_nick] ",
-            "system"
-        );
-
+       
 
         $account_role = $this->t_manager_info->get_account_role($origin_assistantid);
         if($account_role==1){
@@ -2894,9 +2889,22 @@ class ss_deal extends Controller
             $this->t_manager_info->send_wx_todo_msg_by_adminid($sub_assign_adminid_1,"转介绍","学生[$nick][$phone]","","/seller_student_new/seller_student_list_all?userid=$userid");
             $this->t_manager_info->send_wx_todo_msg_by_adminid(349,"转介绍","学生[$nick][$phone]","总监:".$sub_assign_adminid_1,"/seller_student_new/seller_student_list_all?userid=$userid");
 
+            $name = $this->t_manager_info->get_account($sub_assign_adminid_1);
+            $this->t_book_revisit->add_book_revisit(
+                $phone,
+                "操作者: $account , 负责人: [$origin_assistant_nick] 转介绍  来自:[$origin_nick] ,分配给销售经理".$name,
+                "system"
+            );
+
 
 
         }else{
+            $this->t_book_revisit->add_book_revisit(
+                $phone,
+                "操作者: $account , 负责人: [$origin_assistant_nick] 转介绍  来自:[$origin_nick] ",
+                "system"
+            );
+
             //分配给原来的销售
             $admin_revisiterid= $this->t_order_info-> get_last_seller_by_userid($origin_userid);
             //$admin_revisiterid= $origin_assistantid;
--- app/Http/Controllers/wx_teacher_api.php
+++ app/Http/Controllers/wx_teacher_api.php
@@ -587,29 +587,6 @@ class wx_teacher_api extends Controller
             $ret_info['has_do'] = 1;// 已处理
             return $this->output_succ(['data'=>$ret_info]);
         }else{
-            // $time_info = $this->t_lesson_info_b3->get_lesson_info($lessonid);
-            // $time_info['subject'] = E\Esubject::get_desc($time_info['subject']);
-            // $time_info['grade'] = E\Egrade::get_desc($time_info['grade']);
-            // $time_info['parent_modify_time']  = $this->t_lesson_time_modify->get_parent_modify_time($lessonid);
-
-            // $date_modify = json_decode($time_info['parent_modify_time'],true);
-            // $day_date = [];
-            // foreach($date_modify as $item){
-            //     $day_date[] = date('Y-m-d',$item);
-            // }
-            // $b = array_flip(array_flip($day_date));
-            // $time_info['teacher_lesson_time'] = [];
-            // foreach($b as $val){
-            //     $begin_time = strtotime($val);
-            //     $end_time   = $begin_time+86400;
-            //     $teacher_time = $this->t_lesson_info_b2->get_teacher_time_by_lessonid($lessonid, $begin_time, $end_time);
-            //     array_merge($time_info['teacher_lesson_time'],$teacher_time);
-            // }
-
-            // $time_info['has_do'] = 0;  // 未处理
-            // return $this->output_succ(['data'=>$time_info]);
-
-
             $time_info = $this->t_lesson_info_b3->get_lesson_info($lessonid);
             $time_info['subject'] = E\Esubject::get_desc($time_info['subject']);
             $time_info['grade'] = E\Egrade::get_desc($time_info['grade']);
