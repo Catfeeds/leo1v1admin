230bd5d3b4cc6f302d0c672142c3f4cfadd3d91c
boby <boby@leoedu.com>
2017-09-30 14:17:15 +0800

    fix

--- app/Http/Controllers/warning_overtime.php
+++ app/Http/Controllers/warning_overtime.php
@@ -19,8 +19,6 @@ class warning_overtime extends Controller
         $end_time = $now - 7*24*3600;
         $start_time   = $end_time - 60;
         $ret_list   = $this->t_revisit_info->get_overtime_by_now($start_time, $end_time);
-        dd($ret_list);
-
         foreach ($ret_list as $item) {
             if(is_array($item)) {
                 $this->t_revisit_warning_overtime_info->row_insert([
