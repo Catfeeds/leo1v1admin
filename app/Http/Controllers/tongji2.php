4e762777b28e14baee116976c2d67a49e55e9e20
sam <sam@leoedu.com>
2017-09-30 19:53:22 +0800

    fix

--- app/Http/Controllers/tongji2.php
+++ app/Http/Controllers/tongji2.php
@@ -1183,7 +1183,7 @@ class tongji2 extends Controller
                     $end_time = $end_time + 86400;
                 }else{//跨月报
                     $type = 3;
-                    $start_time = strtotime($end_month);
+                    $start_time = $start_time + 86400;
                     $end_time = $end_time + 86400;
                 }
             }elseif($opt_date_type == 3){
@@ -1198,7 +1198,11 @@ class tongji2 extends Controller
             //节点
             //概况
             $ret_total   = $this->t_order_info->get_total_price($start_time,$end_time);
-            $month_ret_total   = $this->t_order_info->get_total_price(strtotime($end_month),$end_time);
+            if($type == 3){
+                $month_ret_total   = $this->t_order_info->get_total_price(strtotime($end_month),$end_time);
+            }elseif($type == 1 || $type == 2){
+                $month_ret_total   = $this->t_order_info->get_total_price($start_time,$end_time);
+            }
             $ret_total_thirty = $this->t_order_info->get_total_price_thirty($start_time,$end_time);
             $ret_cr = $this->t_manager_info->get_cr_num($start_time,$end_time);
             $ret_refund = $this->t_order_refund->get_assistant_num($start_time,$end_time);  //退费总人数
