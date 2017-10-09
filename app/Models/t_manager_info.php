77b36f8ad34cdc2b4b29f54149d2383a3b0dcd37
tom <tom@leoedu.com>
2017-10-09 16:54:59 +0800

    fix

--- app/Models/t_manager_info.php
+++ app/Models/t_manager_info.php
@@ -196,7 +196,7 @@ class t_manager_info extends \App\Models\Zgen\z_t_manager_info
         $this->where_arr_add_int_or_idlist($where_arr,"t1.del_flag", $del_flag);
         $this->where_arr_add_int_or_idlist($where_arr,"t1.uid", $adminid);
 
-        $sql =$this->gen_sql_new("select  t1.create_time,leave_member_time,become_member_time,call_phone_type, call_phone_passwd, fingerprint1 ,ytx_phone,wx_id,up_adminid,day_new_user_flag, account_role,creater_adminid,t1.uid,t1.del_flag,t1.account,t1.seller_level, name,nickname, email, phone,password, permission,tquin,wx_openid ,cardid,become_full_member_flag,main_department,fulltime_teacher_type from %s t1  left join %s t2 on t1.uid=t2.id    left join %s t_wx on t1.wx_openid =t_wx.openid  where  %s  order by t1.uid desc",
+        $sql =$this->gen_sql_new("select t1.create_time,leave_member_time,become_member_time,call_phone_type, call_phone_passwd, fingerprint1 ,ytx_phone,wx_id,up_adminid,day_new_user_flag, account_role,creater_adminid,t1.uid,t1.del_flag,t1.account,t1.seller_level, name,nickname, email, phone,password, permission,tquin,wx_openid ,cardid,become_full_member_flag,main_department,fulltime_teacher_type from %s t1  left join %s t2 on t1.uid=t2.id    left join %s t_wx on t1.wx_openid =t_wx.openid  where  %s  order by t1.uid desc",
                                  self::DB_TABLE_NAME,
                                  t_admin_users::DB_TABLE_NAME,
                                  t_wx_user_info::DB_TABLE_NAME,
