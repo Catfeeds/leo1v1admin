60ffeaef17a029d8e4e16d1d67c8198108c93cb2
tom <tom@leoedu.com>
2017-10-09 17:33:11 +0800

    fix

---
+++ app/Http/Controllers/seller_level_goal.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use \App\Enums as E;
+
+use Illuminate\Support\Facades\Mail ;
+
+
+class seller_level_goal extends Controller
+{
+    public function seller_level_goal_list(){
+
+        dd('a');
+    }
+}
