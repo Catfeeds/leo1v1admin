c561aea27d2d23e671526e1093f695ae51f2ef47
tom <tom@leoedu.com>
2017-10-10 15:26:40 +0800

    fix

--- public/page_ts/g_args.d.ts/seller_level_goal-seller_level_goal_list.d.ts
+++ public/page_ts/g_args.d.ts/seller_level_goal-seller_level_goal_list.d.ts
@@ -1,4 +1,7 @@
 interface GargsStatic {
+	seller_level:	number;
+	level_goal:	number;
+	level_face:	string;
 	page_num:	number;
 	page_count:	number;
 }
@@ -14,7 +17,7 @@ interface RowData {
 	level_goal	:any;
 	level_face	:any;
 	create_time	:any;
-	pp_level_str	:any;
+	seller_level_str	:any;
 }
 
 /*
@@ -28,11 +31,16 @@ tofile:
 $(function(){
     function load_data(){
         $.reload_self_page ( {
-
+			seller_level:	$('#id_seller_level').val(),
+			level_goal:	$('#id_level_goal').val(),
+			level_face:	$('#id_level_face').val()
         });
     }
 
 
+	$('#id_seller_level').val(g_args.seller_level);
+	$('#id_level_goal').val(g_args.level_goal);
+	$('#id_level_face').val(g_args.level_face);
 
 
 	$('.opt-change').set_input_change_event(load_data);
@@ -42,4 +50,25 @@ $(function(){
 
 */
 /* HTML ...
+
+        <div class="col-xs-6 col-md-2">
+            <div class="input-group ">
+                <span class="input-group-addon">seller_level</span>
+                <input class="opt-change form-control" id="id_seller_level" />
+            </div>
+        </div>
+
+        <div class="col-xs-6 col-md-2">
+            <div class="input-group ">
+                <span class="input-group-addon">level_goal</span>
+                <input class="opt-change form-control" id="id_level_goal" />
+            </div>
+        </div>
+
+        <div class="col-xs-6 col-md-2">
+            <div class="input-group ">
+                <span class="input-group-addon">level_face</span>
+                <input class="opt-change form-control" id="id_level_face" />
+            </div>
+        </div>
 */
