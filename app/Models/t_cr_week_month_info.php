924b3300e3744670f9b80dd45a44d6c7fc0490ba
sam <sam@leoedu.com>
2017-10-10 11:25:59 +0800

    fix

--- app/Models/t_cr_week_month_info.php
+++ app/Models/t_cr_week_month_info.php
@@ -18,9 +18,10 @@ class t_cr_week_month_info extends \App\Models\Zgen\z_t_cr_week_month_info
     public function get_student_list_new($type,$create_time){
         $where_arr = [
             ["create_time=%u",$create_time,-1],
-            ["type=%u",$type,-1]
+            " type=2 or type =3",
         ];
         $sql = $this->gen_sql_new("select student_list  from %s where %s",self::DB_TABLE_NAME,$where_arr);
+        print($sql);
         return $this->main_get_value($sql);
 
     }
