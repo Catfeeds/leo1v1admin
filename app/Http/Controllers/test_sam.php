ebba4eb1b2b344c9bb4eed9af18808ccb9e5b217
sam <sam@leoedu.com>
2017-10-10 16:48:35 +0800

    fix

--- app/Http/Controllers/test_sam.php
+++ app/Http/Controllers/test_sam.php
@@ -389,7 +389,7 @@ class test_sam  extends Controller
                 $month_tranfer_data = $this->t_order_info->get_cr_to_cc_order_num($start_time,$now_time); //签单数量(分配例子当月1号到6个月)
                 $arr['month_tranfer_total_num']   = $month_tranfer_data['total_num'];
                 if($arr['month_tranfer_total_num']){
-                  $arr['tranfer_success_per'] = round($arr['month_tranfer_total_price']/$tranfer,2); //D4-月转介绍至CC签单率
+                  $arr['tranfer_success_per'] = round($arr['month_tranfer_total_num']/$tranfer,2); //D4-月转介绍至CC签单率
                 }else{
                   $arr['tranfer_success_per'] = 0;
                 }
