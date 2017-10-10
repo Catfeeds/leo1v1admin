fb58ea33db0de3a2ab4c5662abfde9f4a286178b
boby <boby@leoedu.com>
2017-10-10 10:56:59 +0800

    fix

--- app/Models/t_revisit_info.php
+++ app/Models/t_revisit_info.php
@@ -184,6 +184,7 @@ class t_revisit_info extends \App\Models\Zgen\z_t_revisit_info
                 ["m.uid= %u",$ass_adminid,-1],
                 "r.revisit_time<$two",
                 "r.revisit_time>=$three",
+                "r.revisit_type=0",
             ];
 
         } else if ($revisit_warning_type == 3){
@@ -515,7 +516,8 @@ class t_revisit_info extends \App\Models\Zgen\z_t_revisit_info
         $where_arr = [
             "revisit_time >= $start_time",
             "revisit_time < $end_time",
-            "is_warning_flag=1"
+            "revisit_type = 0",
+            "is_warning_flag=1",
         ];
         $sql = $this->gen_sql_new("select userid,revisit_time,sys_operator"
                                   ." from %s "
