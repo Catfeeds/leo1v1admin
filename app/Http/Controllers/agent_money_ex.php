cbd020c18e1534f09e198eeaf27a1d94f88d826a
abner <abner@leoedu.com>
2017-10-10 18:13:54 +0800

    f

--- app/Http/Controllers/agent_money_ex.php
+++ app/Http/Controllers/agent_money_ex.php
@@ -3,22 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Enums as E;
 
+use App\Http\Requests;
 
 class agent_money_ex extends Controller
 {
-    
+    //
+
     use CacheNick;
     //
     public function agent_money_ex_list(){
         $page_info=$this->get_in_page_info();
-        list($start_time, $end_time  ) =$this->get_in_date_range_day(0);
+        list($start_time, $end_time  ) =$this->get_in_date_range_month(0);
         $ret_info=$this->t_agent_money_ex->get_list($page_info,$start_time, $end_time);
         //dd($ret_info);
         foreach ($ret_info["list"] as &$item ) {
             \App\Helper\Utils::unixtime2date_for_item($item,"add_time");
-            // E\Egrade::set_item_value_str($item);
-
+            E\Eagent_money_ex_type::set_item_value_str($item);
+            $item["money"] = $item["money"]/100;
 
         }
         return $this->pageView(__METHOD__,$ret_info);
@@ -26,28 +29,20 @@ class agent_money_ex extends Controller
     }
 
     public function agent_add( ) {
-        $ex_type = $this->get_in_str_val("ex_type");
+        $agent_money_ex_type = $this->get_in_int_val("agent_money_ex_type");
         $agent_id = $this->get_in_int_val("agent_id");
-        $adminid_id = $this->get_in_int_val("adminid_id");
-        $money = $this->get_in_int_val("money");
+        $money = $this->get_in_str_val("money");
+        $adminid = $this->get_account_id();
         $this->t_agent_money_ex->row_insert([
             "add_time" =>  time(NULL),
-            "ex_type" =>  $ex_type,
+            "agent_money_ex_type" =>  $agent_money_ex_type,
             "agent_id" =>  $agent_id,
-            "adminid_id" =>  $adminid_id,
-            "money" =>  $money
+            "adminid" =>  $adminid,
+            "money" =>  $money*100
         ]);
         return $this->output_succ();
     }
-    public function agent_money_ex_set() {
-        $id= $this->get_in_int_val("id");
-        $grade= $this->get_in_e_grade();
-        $this->t_agent_money_ex->field_update_list($id,[
-            "grade" => $grade,
-        ]);
-        return $this->output_succ();
-    }
-    public function agent_money_ex_del() {
+   public function agent_money_ex_del() {
         $id= $this->get_in_int_val("id");
         $this->t_agent_money_ex->row_delete($id);
         return $this->output_succ();
--- app/Models/t_agent_money_ex.php
+++ app/Models/t_agent_money_ex.php
@@ -8,6 +8,7 @@ class t_agent_money_ex extends \App\Models\Zgen\z_t_agent_money_ex
         parent::__construct();
     }
 
+
     public function  get_list( $page_info,$start_time, $end_time) {
 
         $where_arr=[
@@ -15,7 +16,7 @@ class t_agent_money_ex extends \App\Models\Zgen\z_t_agent_money_ex
         $this->where_arr_add_time_range($where_arr,"ame.add_time",$start_time,$end_time);
 
         $sql=$this->gen_sql_new(
-            "select ame.agent_money_ex_type,ame.add_time,ame.money,mi.account,mi.name ".
+            "select ame.id,ame.agent_id,ame.agent_money_ex_type,ame.add_time,ame.money,mi.account,mi.name ".
             "from %s ame ".
             "left join %s mi on ame.adminid = mi.uid ".
             "where %s ",
@@ -26,11 +27,11 @@ class t_agent_money_ex extends \App\Models\Zgen\z_t_agent_money_ex
 
         return $this->main_get_list_by_page($sql,$page_info);
     }
-
     public function get_all_money ($agent_id) {
         $sql=$this->gen_sql_new( "select sum(money) from %s where  agent_id =%u "
-                              ,self::DB_TABLE_NAME , $agent_id);
+                                 ,self::DB_TABLE_NAME , $agent_id);
         return $this->main_get_value($sql);
     }
+
 
 }
