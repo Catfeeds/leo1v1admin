71dcf449a49f4a163420f7d16eeb7ea7e6353697
tom <tom@leoedu.com>
2017-10-10 09:44:44 +0800

    fix

--- public/page_ts/seller_level_goal/seller_level_goal_list.ts
+++ public/page_ts/seller_level_goal/seller_level_goal_list.ts
@@ -27,6 +27,38 @@ $(function(){
     $('#id_pp_price').val(g_args.pp_price);
     $('#id_userid').val(g_args.userid);
 
+    $("#id_add").on("click",function(){
+        var $seller_level = $("<select></select>");
+        var $level_goal   = $("<input/>");
+        var $level_face   = $("<input id='id_upload_level_face' /> <button class='btn  btn-primary upload_change_reason_url' title='上传'>上传</button>");
+        Enum_map.append_option_list("seller_level",$seller_level);
+        $level_face.find(".upload_change_reason_url").attr("id","id_upload_level_face_url");
+        var th = setTimeout(function(){
+            $.custom_upload_file('id_upload_level_face_url',true,function (up, info, file) {
+                var res = $.parseJSON(info);
+                console.log(res);
+                $level_face.find("#id_upload_level_face").val(res.key);
+            }, null,["png", "jpg",'jpeg','bmp','gif','rar','zip']);
+            clearTimeout(th);
+        }, 1000);
+
+        var arr=[
+            ["销售等级",  $seller_level],
+            ["等级目标",  $level_goal],
+            ["等级头像",  $level_face],
+        ];
+        $.show_key_value_table("新增转介绍订单", arr ,{
+            label: '确认',
+            cssClass: 'btn-warning',
+            action: function(dialog) {
+                $.do_ajax("/ajax_deal/seller_level_goal_add",{
+                    "seller_level" : $seller_level.val(),
+                    "level_goal"   : $level_goal.val(),
+                    "level_face"   : $level_face.val(),
+                })
+            }
+        })
+    });
 
     $('.opt-change').set_input_change_event(load_data);
 });
--- resources/views/seller_level_goal/seller_level_goal_list.blade.php
+++ resources/views/seller_level_goal/seller_level_goal_list.blade.php
@@ -1,15 +1,20 @@
 @extends('layouts.app')
 @section('content')
-
+    <script type="text/javascript" src="/js/qiniu/plupload/plupload.full.min.js"></script>
+    <script type="text/javascript" src="/js/qiniu/plupload/i18n/zh_CN.js"></script>
+    <script type="text/javascript" src="/js/qiniu/ui.js"></script>
+    <script type="text/javascript" src="/js/qiniu/qiniu.js"></script>
+    <script type="text/javascript" src="/js/qiniu/highlight/highlight.js"></script>
     <section class="content ">
-        
         <div>
             <div class="row  row-query-list" >
                 <div class="col-xs-12 col-md-5"  data-title="时间段">
                     <div  id="id_date_range" >
                     </div>
                 </div>
-
+                <div class="col-xs-6 col-md-2">
+                    <button style="" id="id_add"> 增加</button>
+                </div>
                 <div class="col-xs-6 col-md-2">
                     <div class="input-group " >
                         <span >xx</span>
@@ -22,14 +27,20 @@
         <table     class="common-table"  > 
             <thead>
                 <tr>
-                    <td>字段1 </td>
+                    <td>销售等级 </td>
+                    <td>等级目标 </td>
+                    <td>等级头像 </td>
+                    <td>创建时间 </td>
                     <td> 操作  </td>
                 </tr>
             </thead>
             <tbody>
                 @foreach ( $table_data_list as $var )
                     <tr>
-                        <td>{{@$var[""]}} </td>
+                        <td>{{@$var["seller_level"]}} </td>
+                        <td>{{@$var["level_goal"]}} </td>
+                        <td>{{@$var["level_face"]}} </td>
+                        <td>{{@$var["create_time"]}} </td>
                         <td>
                             <div
                                 {!!  \App\Helper\Utils::gen_jquery_data($var )  !!}
