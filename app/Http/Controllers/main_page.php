5ba07ca0d832efdea59785d0cb3388c19cfe9493
boby <boby@leoedu.com>
2017-10-11 15:18:16 +0800

    fix

--- app/Http/Controllers/main_page.php
+++ app/Http/Controllers/main_page.php
@@ -546,7 +546,7 @@ class main_page extends Controller
         ];
 
         $revisit_assess_list = [
-            ['type' =>'今日'],
+            $month_goal,
             ['type' =>'本月'],
         ];
 
