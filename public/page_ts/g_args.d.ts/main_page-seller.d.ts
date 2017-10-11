c83c09dc4d824baa56cc7d65eb0866a79f0a2209
tom <tom@leoedu.com>
2017-10-11 14:32:33 +0800

    fix

--- public/page_ts/g_args.d.ts/main_page-seller.d.ts
+++ public/page_ts/g_args.d.ts/main_page-seller.d.ts
@@ -18,10 +18,6 @@ declare var g_account: string;
 declare var g_account_role: any;
 declare var g_adminid: any;
 interface RowData {
-	sys_operator	:any;
-	all_price	:any;
-	all_count	:any;
-	index	:any;
 }
 
 /*
