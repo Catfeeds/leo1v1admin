910df69de09f1fee9e965496df4a0917688a85b7
boby <boby@leoedu.com>
2017-10-12 11:10:16 +0800

    fix

--- app/Models/t_revisit_assess_info.php
+++ app/Models/t_revisit_assess_info.php
@@ -10,14 +10,13 @@ class t_revisit_assess_info extends \App\Models\Zgen\z_t_revisit_assess_info
 
     public function get_month_assess_info_by_uid($ass_adminid, $start_time,$end_time,$uid_str = -1){
         $where_arr = [
-            ["ra.uid=%u",$ass_adminid,-1],
             "ra.create_time>=$start_time",
             "ra.create_time<$end_time",
-            // "r.revisit_time>=$start_time",
-            // "r.revisit_time<$end_time",
         ];
         if ($uid_str != -1 && $uid_str !== null) {
             $where_arr[] = "ra.uid in ($uid_str)";
+        } else {
+            $where_arr[] = ["ra.uid=%u",$ass_adminid,-1];
         }
 
 
