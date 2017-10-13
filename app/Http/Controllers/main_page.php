f44cb1915a4704fa4cbe6b28db3da82cdd7417e1
boby <boby@leoedu.com>
2017-10-13 13:59:25 +0800

    fix

--- app/Http/Controllers/main_page.php
+++ app/Http/Controllers/main_page.php
@@ -587,7 +587,7 @@ class main_page extends Controller
         //当天回访信息
         $start_time = strtotime( "today" );
         $end_time   = strtotime("tomorrow");
-        $today_info = $this->t_manager_info->get_today_assess_info_by_uid($ass_adminid, $start_time, $end_time);
+        $today_info = $this->t_revisit_info->get_today_assess_info_by_uid($ass_adminid, $start_time, $end_time);
         $today_info["call_num"]= \App\Helper\Common::get_time_format_minute($today_info["call_num"]);
         $today_info['goal'] = ceil($today_info['stu_num']/10);
 
