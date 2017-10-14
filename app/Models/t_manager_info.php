c2109d05a06a657649f5540ffa15dc704132e1e2
tom <tom@leoedu.com>
2017-10-14 15:47:43 +0800

    fix

--- app/Models/t_manager_info.php
+++ app/Models/t_manager_info.php
@@ -723,7 +723,7 @@ class t_manager_info extends \App\Models\Zgen\z_t_manager_info
             "m.del_flag =0 ",
         ];
         $sql=$this->gen_sql_new(
-            "select uid,account_role,create_time,seller_level,face_pic,level_face  "
+            "select uid,account_role,m.create_time,m.seller_level,face_pic,level_face  "
             ." from %s m "
             ." left join %s g on g.seller_level=m.seller_level "
             ." where %s "
