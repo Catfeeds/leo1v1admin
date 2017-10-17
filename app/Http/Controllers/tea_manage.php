5c1335a029556171afea7cba301b6e47038dffee
boby <boby@leoedu.com>
2017-10-17 12:21:04 +0800

    fix

--- app/Http/Controllers/tea_manage.php
+++ app/Http/Controllers/tea_manage.php
@@ -423,9 +423,9 @@ class tea_manage extends Controller
         // dd($ret_arr);
         //图片信息
         $qr_info = "title=lessonid:{$lessonid}&beginTime={$ret_arr['lesson_start']}&endTime={$ret_arr['lesson_end']}&roomId={$ret_arr['roomid']}&xmpp={$ret_arr['xmpp']}&webrtc={$ret_arr['webrtc']}&ownerId={$ret_arr['teacherid']}&type={$ret_arr['type']}&audioService={$ret_arr['audioService']}";
-        $base64_qr = base64_decode($qr_info);
+        $base64_qr = base64_encode($qr_info);
 
-        $lessonid_qr_name = $lessonid."-qr-new.png";
+        $lessonid_qr_name = $lessonid."-qr.png";
         $qiniu     = \App\Helper\Config::get_config("qiniu");
         $qiniu_url = $qiniu['public']['url'];
         $is_exists = \App\Helper\Utils::qiniu_file_stat($qiniu_url,$lessonid_qr_name);
