f19466bea96578c71bb25e3f1e9036656a0b09a6
tom <tom@leoedu.com>
2017-10-17 17:44:50 +0800

    fix

--- app/Http/Controllers/seller_student_new2.php
+++ app/Http/Controllers/seller_student_new2.php
@@ -837,82 +837,12 @@ class seller_student_new2 extends Controller
 
     //分配例子统计
     public function seller_student_new_distribution(){
-        \App\Helper\Utils::logger("START");
-        $start = strtotime(date('Y-m-01',time()));
-        $day = intval(ceil((time()-$start)/86400)-1);
-        $day = $day-2*$day;
         list($start_time,$end_time)=$this->get_in_date_range(0,0,0,[],3);
         if($end_time >= time()){
             $end_time = time();
         }
-
-        $start_first = date('Y-m-01',$start_time);
         $res = [];
-        $this->t_seller_month_money_target->switch_tongji_database();
-        $ret_info = $this->t_seller_month_money_target->get_seller_month_time_info($start_first);
-        $start_day = date('d',$start_time);
-        $end_day = date('d',($end_time-10));
-        foreach($ret_info as $k=>&$item){
-            $month_time = json_decode($item['month_time'],true);
-            $i = $j = $l=0;
-            $now = time();
-            $day = ceil(($end_time- $start_time)/86400);
-            if(!empty($month_time)){
-                foreach($month_time as $val){
-                    if(substr($val[0],11,1) ==1){
-                        $i++;
-                    }
-                    if(substr($val[0],11,1) ==1 && substr($val[0],8,2) <= $end_day && substr($val[0],8,2) >= $start_day ){
-                        $j++;
-                    }
-                }
-                $leave_and_overtime = json_decode($item['leave_and_overtime'],true);
-                if(!empty($leave_and_overtime)){
-                    foreach($leave_and_overtime as $v){
-                        if(substr($v[0],11,1) ==2 && substr($v[0],8,2) <= $end_day && substr($v[0],8,2) >= $start_day ){
-                            $l--;
-                        }
-                        if(substr($v[0],11,1) ==3 && substr($v[0],8,2) <= $end_day && substr($v[0],8,2) >= $start_day ){
-                            $l++;
-                        }
-
-                    }
-                }
-
-            }
-
-            $res[$k]['month_work_day'] = $i;
-            $res[$k]['month_work_day_now'] = $j;
-            $res[$k]['month_work_day_now_real'] = $j+$l;
-            $res[$k]['target_personal_money'] = $item['personal_money'];
-        }
-        $this->t_admin_group_user->switch_tongji_database();
-        $group_money_info = $this->t_admin_group_user->get_seller_month_money_info($start_first);
-        $num_info = $this->t_admin_group_user->get_group_num($start_time);
-        foreach($group_money_info as &$item){
-            $groupid = $item['groupid'];
-            if($groupid >0 && isset($num_info[$groupid])){
-                $res[$item['adminid']]['target_money'] =  $item['month_money']/$num_info[$groupid]['num'];
-            }
-        }
-        $this->t_tq_call_info->switch_tongji_database();
-
-
-        /*
-        $list=$this->t_tq_call_info->tongji_tq_info_new($start_time,$end_time);
-        foreach ($list as $k=>&$item )  {
-            $res[$k]['is_called_phone_count_for_month'] = $item['all_count'];
-            if(isset($res[$k]['month_work_day_now_real']) && $res[$k]['month_work_day_now_real'] != 0){
-                $res[$k]['is_called_phone_count_for_day'] = round($item['all_count']/$res[$k]['month_work_day_now_real']);
-                $res[$k]['duration_count_for_day'] = round($item['duration_count']/$res[$k]['month_work_day_now_real']);
-            }
-        }
-        */
-
-
         $this->t_test_lesson_subject_require->switch_tongji_database();
-
-        // $tr_info=$this->t_test_lesson_subject_require->tongji_require_test_lesson_group_by_admin_revisiterid($start_time,$end_time);
         $tr_info=$this->t_test_lesson_subject_require->tongji_require_test_lesson_group_by_admin_revisiterid_new($start_time,$end_time);
         foreach($tr_info['list'] as $item){
             $adminid = $item['admin_revisiterid'];
@@ -922,75 +852,32 @@ class seller_student_new2 extends Controller
             }
 
         }
-        // $test_leeson_list=$this->t_test_lesson_subject_require->tongji_test_lesson_group_by_admin_revisiterid($start_time,$end_time );
         $test_leeson_list=$this->t_test_lesson_subject_require->tongji_test_lesson_group_by_admin_revisiterid_new_two($start_time,$end_time );
         foreach($test_leeson_list['list'] as $item){
             $adminid = $item['admin_revisiterid'];
             $res[$adminid]['test_lesson_count_for_month'] = $item['test_lesson_count'];
-            // $res[$adminid]['succ_all_count_for_month']=$item['succ_all_count'];
-            // $res[$adminid]['fail_all_count_for_month'] = $item['fail_all_count'];
-            // if($item['test_lesson_count'] != 0){
-            //     $res[$adminid]['lesson_per'] = round($item['fail_all_count']/$item['test_lesson_count'],2);
-            // }
         }
 
-        // $test_leeson_list=$this->t_test_lesson_subject_require->tongji_test_lesson_group_by_admin_revisiterid($start_time,$end_time );
         $test_leeson_list=$this->t_test_lesson_subject_require->tongji_test_lesson_group_by_admin_revisiterid_new($start_time,$end_time );
         foreach($test_leeson_list['list'] as $item){
             $adminid = $item['admin_revisiterid'];
-            $res[$adminid]['succ_all_count_for_month']=$item['succ_all_count'];
             $res[$adminid]['fail_all_count_for_month'] = $item['fail_all_count'];
             if($item['test_lesson_count'] != 0){
                 $res[$adminid]['lesson_per'] = round($item['fail_all_count']/$item['test_lesson_count'],2);
             }
         }
 
-        $this->t_order_info->switch_tongji_database();
-
-        $order_new = $this->t_order_info->get_1v1_order_list_by_adminid($start_time,$end_time,-1);
-        foreach($order_new as $k=>$v){
-            $res[$k]['all_new_contract_for_month'] = $v['all_new_contract'];
-            if(isset($res[$k]['succ_all_count_for_month']) && $res[$k]['succ_all_count_for_month'] != 0){
-                $res[$k]['order_per'] =round($v['all_new_contract']/$res[$k]['succ_all_count_for_month'],2);
-            }
-            $res[$k]['all_price_for_month'] = $v['all_price']/100;
-            /* $res[$k]['max_price_for_month'] = $v['max_price']/100;*/
-            if(isset($res[$k]['target_money']) && $res[$k]['target_money'] != 0){
-                $res[$k]['finish_per'] =  round($v['all_price']/100/$res[$k]['target_money'],2);
-                $res[$k]['los_money'] = $res[$k]['target_money']-$v['all_price']/100;
-            }
-            if(isset($res[$k]['target_personal_money']) && $res[$k]['target_personal_money'] != 0){
-                $res[$k]['finish_personal_per'] =  round($v['all_price']/100/$res[$k]['target_personal_money'],2);
-                $res[$k]['los_personal_money'] = $res[$k]['target_personal_money']-$v['all_price']/100;
-            }
-        }
         foreach ($res as $ret_k=> &$res_item) {
             $res_item["adminid"] = $ret_k ;
         }
         list($member_new,$member_num_new,$member,$member_num,$become_member_num_l1,$leave_member_num_l1,$become_member_num_l2,$leave_member_num_l2,$become_member_num_l3,$leave_member_num_l3) = [[],[],[],[],0,0,0,0,0,0];
-        //$ret_info=\App\Helper\Common::gen_admin_member_data($res);
-        $ret_info=\App\Helper\Common::gen_admin_member_data($res,[],0,strtotime(date("Y-m-01",$start_time )));
-        // dd($ret_info);
-        foreach( $ret_info as $key=>&$item ){
+        $ret_info = \App\Helper\Common::gen_admin_member_data($res,[],0,strtotime(date("Y-m-01",$start_time )));
+        foreach($ret_info as $key=>&$item){
             $item["become_member_time"] = isset($item["create_time"])?$item["create_time"]:0;
             $item["leave_member_time"] = isset($item["leave_member_time"])?$item["leave_member_time"]:0;
             $item["del_flag"] = isset($item["del_flag"])?$item["del_flag"]:0;
             E\Emain_type::set_item_value_str($item);
-            $item['lesson_per'] = @$item['test_lesson_count_for_month']!=0?(round(@$item['fail_all_count_for_month']/$item['test_lesson_count_for_month'],2)*100)."%":0;
-            $item['order_per'] = @$item['succ_all_count_for_month']!=0?(round(@$item['all_new_contract_for_month']/$item['succ_all_count_for_month'],2)*100)."%":0;
-            $item['finish_per'] =@$item['target_money']!=0?(round(@$item['all_price_for_month']/$item['target_money'],2)*100)."%":0;
-            $item['finish_personal_per'] =@$item['target_personal_money']!=0?(round(@$item['all_price_for_month']/$item['target_personal_money'],2)*100)."%":0;
-
-
-            $item['duration_count_for_day'] = \App\Helper\Common::get_time_format(@$item['duration_count_for_day']);
-            $item['ave_price_for_month'] =@$item['all_new_contract_for_month']!=0?round(@$item['all_price_for_month']/@$item['all_new_contract_for_month']):0;
-            $item['los_money'] = @$item['target_money']-@$item['all_price_for_month'];
-            $item['los_personal_money'] = @$item['target_personal_money']-@$item['all_price_for_month'];
-
             if($item['level'] == "l-4" ){
-                $item['target_money']="";
-                $item['finish_per'] = "";
-                $item['los_money'] = "";
                 \App\Helper\Utils::unixtime2date_for_item($item,"become_member_time",'','Y-m-d');
                 \App\Helper\Utils::unixtime2date_for_item($item,"leave_member_time",'','Y-m-d');
                 $item["del_flag_str"] = \App\Helper\Common::get_boolean_color_str($item["del_flag"]);
@@ -1035,21 +922,6 @@ class seller_student_new2 extends Controller
             }
             if($item['main_type_str'] == '助教'){
                 unset($ret_info[$key]);
-            }
-            if(isset($item['target_money'])){
-                $item['target_money'] = round($item['target_money']);
-            }
-            if(isset($item['los_money'])){
-                $item['los_money'] = round($item['los_money']);
-            }
-            if(isset($item['all_price_for_month'])){
-                $item['all_price_for_month'] = round($item['all_price_for_month']);
-            }
-            if(isset($item['ave_price_for_month'])){
-                $item['ave_price_for_month'] = round($item['ave_price_for_month']);
-            }
-            if(isset($item['los_personal_money'])){
-                $item['los_personal_money'] = round($item['los_personal_money']);
             }
         }
         foreach($member as $key=>&$item){
@@ -1098,7 +970,6 @@ class seller_student_new2 extends Controller
                 }
             }
         }
-        \App\Helper\Utils::logger("OUTPUT");
 
         return $this->pageView(__METHOD__,\App\Helper\Utils::list_to_page_info($ret_info));
     }
