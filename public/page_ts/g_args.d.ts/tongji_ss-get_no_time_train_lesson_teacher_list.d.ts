160e9362b6a998d9ebbce0014513d600cd7e1fc1
jim <jim@leoedu.com>
2017-10-19 19:15:47 +0800

    fix

---
+++ public/page_ts/g_args.d.ts/tongji_ss-get_no_time_train_lesson_teacher_list.d.ts
@@ -0,0 +1,44 @@
+interface GargsStatic {
+}
+declare module "g_args" {
+    export = g_args;
+}
+declare var g_args: GargsStatic;
+declare var g_account: string;
+declare var g_account_role: any;
+declare var g_adminid: any;
+interface RowData {
+	teacherid	:any;
+	realname	:any;
+	phone	:any;
+	wx_openid	:any;
+	wx_flag	:any;
+}
+
+/*
+
+tofile: 
+	 mkdir -p ../tongji_ss; vi  ../tongji_ss/get_no_time_train_lesson_teacher_list.ts
+
+/// <reference path="../common.d.ts" />
+/// <reference path="../g_args.d.ts/tongji_ss-get_no_time_train_lesson_teacher_list.d.ts" />
+
+function load_data(){
+    if ( window["g_load_data_flag"]) {return;}
+    $.reload_self_page ( {
+
+    });
+}
+$(function(){
+
+
+
+
+	$('.opt-change').set_input_change_event(load_data);
+});
+
+
+
+*/
+/* HTML ...
+*/
