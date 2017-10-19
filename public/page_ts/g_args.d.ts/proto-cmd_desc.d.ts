563d6a2be76648e58ff621d88dda5e4e240211ef
jim <xcwenn@qq.comm>
2017-10-19 18:11:40 +0800

    f

---
+++ public/page_ts/g_args.d.ts/proto-cmd_desc.d.ts
@@ -0,0 +1,61 @@
+interface GargsStatic {
+	project:	string;
+	cmdid:	string;
+}
+declare module "g_args" {
+    export = g_args;
+}
+declare var g_args: GargsStatic;
+declare var g_account: string;
+declare var g_account_role: any;
+declare var g_adminid: any;
+interface RowData {
+	tags	:any;
+	cmdid	:any;
+	name	:any;
+	desc	:any;
+}
+
+/*
+
+tofile: 
+	 mkdir -p ../proto; vi  ../proto/cmd_desc.ts
+
+/// <reference path="../common.d.ts" />
+/// <reference path="../g_args.d.ts/proto-cmd_desc.d.ts" />
+
+$(function(){
+    function load_data(){
+        $.reload_self_page ( {
+			project:	$('#id_project').val(),
+			cmdid:	$('#id_cmdid').val()
+        });
+    }
+
+
+	$('#id_project').val(g_args.project);
+	$('#id_cmdid').val(g_args.cmdid);
+
+
+	$('.opt-change').set_input_change_event(load_data);
+});
+
+
+
+*/
+/* HTML ...
+
+        <div class="col-xs-6 col-md-2">
+            <div class="input-group ">
+                <span class="input-group-addon">project</span>
+                <input class="opt-change form-control" id="id_project" />
+            </div>
+        </div>
+
+        <div class="col-xs-6 col-md-2">
+            <div class="input-group ">
+                <span class="input-group-addon">cmdid</span>
+                <input class="opt-change form-control" id="id_cmdid" />
+            </div>
+        </div>
+*/
---
+++ resources/views/proto/cmd_desc.blade.php
@@ -0,0 +1,143 @@
+@extends('layouts.base')
+@section('content')
+
+
+    <section class="content "  style="max-width:1000px" >
+        <h1 style="text-align:center" > 协议平台 </h1>
+        @if( count( $table_data_list )  >0 )
+            <table     class="table table-bordered table-striped "   style="max-width:1000px " >
+                <thead>
+                    <tr>
+                        <td width="50px">命令号</td>
+                        <td width="200px">名称</td>
+                        <td >说明</td>
+                        <td >标签</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ( $table_data_list as $var )
+                        <tr>
+                            <td>{{@$var["cmdid"]}} </td>
+                            <td  > <a
+                                       id="{{@$var["name"]}}_item"
+                                       href="#{{@$var["name"]}}_desc"
+                                   > {{@$var["name"]}} </td>
+                            <td>{{@$var["desc"]}} </td>
+                            <td>{{@$var["tags"]}} </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        @endif
+
+
+        @foreach (  $cmd_desc_list as $cmd_item )
+
+            <h4>
+
+                <a
+                                       id="{{@$cmd_item["name"]}}_desc"
+                                       href="#{{@$cmd_item["name"]}}_item" style="font-size:20px;color:blue;" >
+                    {{$cmd_item["cmdid"]}} | {{$cmd_item["name"]}}  | {{$cmd_item["desc"]}} </a> </h4>
+            <table      class="table table-bordered table-striped " style="margin-left:50px;max-width: 800px; "  >
+                <thead>
+                    <tr>
+                        <td width="40%"> {{$cmd_item["name"]}} <font color="blue">请求包 </font>  字段名</td>
+                        <td width="10%">类型</td>
+                        <td width="40%">说明</td>
+                        <td width="10%">字段id</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ( $cmd_item["in"] as $var )
+                        <tr>
+                            <td>{{@$var[2]}} </td>
+                            <td>
+                                {{@$var["1"]}}
+                                {!!  $var[0]=="repeated"?"<font color=red>数组 </font>":"" !!}
+                            </td>
+                            <td>{{@$var["4"]}} </td>
+                            <td>{{@$var["3"]}} </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+
+            <table        class="table table-bordered table-striped " style="margin-left:50px ;max-width: 800px; "  >
+                <thead>
+                    <tr>
+                        <td width="40%"> {{$cmd_item["name"]}} 返回包 字段名</td>
+                        <td width="10%">类型</td>
+                        <td width="40%">说明</td>
+                        <td width="10%">字段id</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ( $cmd_item["out"] as $var )
+                        <tr>
+                            <td>{{@$var[2]}} </td>
+                            <td>
+                                {{@$var["1"]}}
+                                {!!  $var[0]=="repeated"?"<font color=red>数组 </font>":"" !!}
+                            </td>
+                            <td>{{@$var["4"]}} </td>
+                            <td>{{@$var["3"]}} </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+
+            <table        class="table table-bordered table-striped " style="margin-left:50px ;max-width: 800px; "  >
+                <thead>
+                    <tr>
+                        <td width="10%"> 错误码值 </td>
+                        <td width="10%"> 错误码 </td>
+                        <td width="80%">说明</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ( $cmd_item["cmd_error_list"] as $var )
+                        <tr>
+                            <td>{{@$var["value"]}} </td>
+                            <td>{{@$var["name"]}} </td>
+                            <td>{{@$var["desc"]}} </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        @endforeach
+
+
+
+        @if (count($error_list) >0 )
+            <table
+                class="table table-bordered table-striped  " style="max-width:1000px "
+
+            >
+
+                <thead>
+                    <tr>
+                        <td width="10%">  错误码值 </td>
+                        <td width="10%"> 错误码 </td>
+                        <td width="80%">说明
+                            <font color= "red">全体错误码列表 </font>
+                        </td>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ( $error_list as $var )
+                        <tr>
+                            <td>{{@$var["value"]}} </td>
+                            <td>{{@$var["name"]}} </td>
+                            <td>{{@$var["desc"]}} </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        @endif
+
+
+    </section>
+
+
+@endsection
