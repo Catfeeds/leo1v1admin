d64aee3d950d487d849eda71a17bafaf7a6d336e
boby <boby@leoedu.com>
2017-10-19 19:21:51 +0800

    fix

--- app/Http/Controllers/test_boby.php
+++ app/Http/Controllers/test_boby.php
@@ -324,48 +324,51 @@ class test_boby extends Controller
 
     //添加给老师添加公开课学生
     public function add_stu_to_tea_open_lesson(){
-        return 'bey';
-        // $start_time = strtotime('2017-08-05');
-        // $end_time = strtotime('2017-09-01');
-        // $userid_list = $this->t_order_info->get_userid_by_pay_time($start_time, $end_time);
+        $start_time = strtotime('2017-09-01');
+        $end_time = strtotime('2017-10-01');
+        $userid_list = $this->t_order_info->get_userid_by_pay_time($start_time, $end_time);
 
         // $teacherid = "(180795)";
         // $start_time = strtotime('2017-09-01');
         // $end_time = strtotime('2017-10-01');
-        $lessonid_list = ['318458','318459','318460','318461','318462'];
+        $lessonid_list = ['371543','371544','371545','318460','318461'];
         // $lessonid_list = $this->t_lesson_info_b2->get_lessonid_by_teacherid($start_time, $end_time, $teacherid);
         // foreach ($lessonid_list as $v) {
         //     $this->t_open_lesson_user->delete_open_lesson_by_lessonid( $v );
         // }
-        echo 'ok';
-        exit;
-        $g100 = [];
-        $g200 = [];
-        $g300 = [];
-        foreach ($lessonid_list as $v){
-            if ($v['grade'] < 200) {
-                $g100[] = $v['lessonid'];
-            } else if ($v['grade'] < 300) {
-                $g200[] = $v['lessonid'];
-            }else {
-                $g300[] = $v['lessonid'];
-            }
-        }
+        // exit;
+
+        // $g100 = [];
+        // $g200 = [];
+        // $g300 = [];
+        // foreach ($lessonid_list as $v){
+        //     if ($v['grade'] < 200) {
+        //         $g100[] = $v['lessonid'];
+        //     } else if ($v['grade'] < 300) {
+        //         $g200[] = $v['lessonid'];
+        //     }else {
+        //         $g300[] = $v['lessonid'];
+        //     }
+        // }
         foreach ($userid_list as $item) {
-            if ($item['grade'] > 0) {
-                if ($item['grade'] < 200 ) {
-                    foreach ($g100 as $lessonid) {
-                        $this->t_open_lesson_user->add_open_class_user($lessonid, $item['userid']);
-                    }
-                } else if ($item['grade'] < 300 ) {
-                    foreach ($g200 as $lessonid) {
-                        $this->t_open_lesson_user->add_open_class_user($lessonid, $item['userid']);
-                    }
-                } else {
-                    foreach ($g300 as $lessonid) {
-                        $this->t_open_lesson_user->add_open_class_user($lessonid, $item['userid']);
-                    }
-                }
+            // if ($item['grade'] > 0) {
+            //     if ($item['grade'] < 200 ) {
+            //         foreach ($g100 as $lessonid) {
+            //             $this->t_open_lesson_user->add_open_class_user($lessonid, $item['userid']);
+            //         }
+            //     } else if ($item['grade'] < 300 ) {
+            //         foreach ($g200 as $lessonid) {
+            //             $this->t_open_lesson_user->add_open_class_user($lessonid, $item['userid']);
+            //         }
+            //     } else {
+            //         foreach ($g300 as $lessonid) {
+                        // $this->t_open_lesson_user->add_open_class_user($lessonid, $item['userid']);
+            //         }
+            //     }
+            // }
+
+            foreach($lessonid_list as $lessonid){
+                $this->t_open_lesson_user->add_open_class_user($lessonid, $item['userid']);
             }
         }
 
