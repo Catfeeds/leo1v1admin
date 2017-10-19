c57f9f621b2cf5c7543a11c9881b5a962d6d2cee
boby <boby@leoedu.com>
2017-10-19 16:47:40 +0800

    fix

--- resources/views/tongji/match_lesson_textbook.blade.php
+++ resources/views/tongji/match_lesson_textbook.blade.php
@@ -27,6 +27,12 @@
             </div>
             <div class="col-xs-6 col-md-2">
                 <div class="input-group ">
+                    <span>匹配转化率</span>
+                    <input value="{{$match_succ_rate}}">
+                </div>
+            </div>
+            <div class="col-xs-6 col-md-2">
+                <div class="input-group ">
                     <span>非匹配转化率</span>
                     <input value="{{$succ_rate}}">
                 </div>
