8b273c81522d71932a1b39108ab9ba2ef2ea15ae
jim <jim@leoedu.com>
2017-10-20 19:10:20 +0800

    fix

--- app/Models/t_order_refund.php
+++ app/Models/t_order_refund.php
@@ -25,12 +25,12 @@ class t_order_refund extends \App\Models\Zgen\z_t_order_refund
             ." f.flow_status_time,f.flowid,r.should_refund,r.price,o.invoice,o.order_time,o.sys_operator,r.pay_account, "
             ." r.real_refund,r.refund_status,r.apply_time,r.refund_userid,o.contractid,r.save_info,r.refund_info,file_url, "
             ." o.grade,o.need_receipt  "
-            ." ,if(co.child_order_type=2,1,0) is_staged_flag"
+            // ." ,if(co.child_order_type=2,1,0) is_staged_flag"
             ." from %s r"
             ." left join %s s on s.userid=r.userid"
             ." left join %s o on o.orderid=r.orderid"
             ." left join %s f on (f.flow_type=%u and r.orderid=f.from_key_int and r.apply_time = f.from_key2_int) "
-            ." left join %s co on (co.parent_orderid = r.orderid and co.child_order_type = 2)"
+            // ." left join %s co on (co.parent_orderid = r.orderid and co.child_order_type = 2)"
             ." where %s"
             ." order by $opt_date_str desc"
             ,self::DB_TABLE_NAME
@@ -38,7 +38,7 @@ class t_order_refund extends \App\Models\Zgen\z_t_order_refund
             ,t_order_info::DB_TABLE_NAME
             ,t_flow::DB_TABLE_NAME
             ,E\Eflow_type::V_ASS_ORDER_REFUND
-            ,t_child_order_info::DB_TABLE_NAME
+            // ,t_child_order_info::DB_TABLE_NAME
             ,$where_arr
         );
         return $this->main_get_list_by_page($sql,$page_num,10);
