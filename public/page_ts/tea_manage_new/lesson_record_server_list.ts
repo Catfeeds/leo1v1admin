e3c2124f4b40a7109779a76cd6293b44080cf239
jim <jim@leoedu.com>
2017-10-21 10:52:57 +0800

    fix

--- public/page_ts/tea_manage_new/lesson_record_server_list.ts
+++ public/page_ts/tea_manage_new/lesson_record_server_list.ts
@@ -1,17 +1,34 @@
 /// <reference path="../common.d.ts" />
 /// <reference path="../g_args.d.ts/tea_manage_new-lesson_record_server_list.d.ts" />
 
+function load_data(){
+    if ( window["g_load_data_flag"]) {return;}
+    $.reload_self_page ( {
+		date_type_config:	$('#id_date_type_config').val(),
+		date_type:	$('#id_date_type').val(),
+		opt_date_type:	$('#id_opt_date_type').val(),
+		start_time:	$('#id_start_time').val(),
+		end_time:	$('#id_end_time').val(),
+		record_audio_server1:	$('#id_record_audio_server1').val(),
+		xmpp_server_name:	$('#id_xmpp_server_name').val()
+    });
+}
+
 $(function(){
-    function load_data(){
-        $.reload_self_page ( {
-			      xmpp_server_name:	$('#id_xmpp_server_name').val(),
-			      record_audio_server1:	$('#id_record_audio_server1').val()
-        });
-    }
 
 
+
+    $('#id_date_range').select_date_range({
+        'date_type' : g_args.date_type,
+        'opt_date_type' : g_args.opt_date_type,
+        'start_time'    : g_args.start_time,
+        'end_time'      : g_args.end_time,
+        date_type_config : JSON.parse( g_args.date_type_config),
+        onQuery :function() {
+            load_data();
+        }
+    });
 	$('#id_record_audio_server1').val(g_args.record_audio_server1);
-
 	$('#id_xmpp_server_name').val(g_args.xmpp_server_name);
 
 
