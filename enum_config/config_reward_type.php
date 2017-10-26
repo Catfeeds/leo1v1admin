1f569072325668a2601c3ae8317db3a8e8a12c72
jim <jim@leoedu.com>
2017-10-26 15:36:31 +0800

    fix

--- enum_config/config_reward_type.php
+++ enum_config/config_reward_type.php
@@ -4,6 +4,6 @@ return array(
     array(2,"trial_reward","试听课奖金"),
     array(3,"compensate_90","90分钟课程补偿"),
     array(4,"compensate","工资补偿"),
-    array(5,"","模拟试听奖金"),
-    array(6,"","伯乐奖"),
+    array(5,"trial_train","模拟试听奖金"),
+    array(6,"reference","伯乐奖"),
 );
--- public/page_ts/user_manage_new/notify_phone.ts
+++ public/page_ts/user_manage_new/notify_phone.ts
@@ -9,8 +9,17 @@ $(function(){
     }
 
     $("#id_notify_phone") .on("click",function(){
+        var phone=  $.trim( $("#id_phone").val());
 
+        try{
+            window.navigate(
+                "app:1234567@"+phone+"");
+        } catch(e){
 
+        };
+        $.do_ajax_t("/ss_deal/call_ytx_phone", {
+            "phone": phone
+        } );
     });
 
 
