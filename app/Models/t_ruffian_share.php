7a5c9b14c70cb6103b5d4447feb94d2a4166d551
jim <jim@leoedu.com>
2017-10-27 17:34:30 +0800

    fix

---
+++ app/Models/t_ruffian_share.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Models;
+use \App\Enums as E;
+class t_ruffian_share extends \App\Models\Zgen\z_t_ruffian_share
+{
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
