63c35427dbc03d66915a96ea01c94ae3930d4225
tom <tom@leoedu.com>
2017-10-27 20:53:09 +0800

    fix

--- app/Http/Controllers/agent.php
+++ app/Http/Controllers/agent.php
@@ -624,7 +624,7 @@ class agent extends Controller
                     }
                 }
             }
-            
+
             foreach($stu_wheat as $item){
                 $login_s = $item['login'];
                 $logout_s = $item['logout'];
@@ -640,7 +640,7 @@ class agent extends Controller
                     $login_time_seller = $login_c['opt_time'];
                     $logout_time_seller = $logout_c['opt_time'];
                     $server_ip_seller = $login_c['server_ip'];
-                    
+
                     // if($server_ip_stu != $server_ip_seller){
                         $time_differ = 0;
                         if($logout_time_stu == ''){//学生无下麦信息
@@ -652,10 +652,10 @@ class agent extends Controller
                                 $time_differ = min($logout_time_stu,$logout_time_seller)-$login_time_seller;
                             }
                         }
-                        
+
 
                         if($login_s['lessonid'] == 377680){
-                            dd($time_differ,date('Y-m-d H:i:s',$login_time_stu),date('Y-m-d H:i:s',$logout_time_stu),date('Y-m-d H:i:s',$login_time_seller),date('Y-m-d H:i:s',$logout_time_seller));
+                            dd($time_differ,$login_time_stu,$logout_time_stu,$login_time_seller,$logout_time_seller);
                         }
                         if($time_differ>300){//不同ip,同时上麦>5分钟
                             $task->t_lesson_info->field_update_list($info,[
