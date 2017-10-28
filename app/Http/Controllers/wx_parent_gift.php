e9befb7574f9f20d74ac5258e8f1a495c9d21b25
jim <jim@leoedu.com>
2017-10-28 14:55:04 +0800

    f

--- app/Http/Controllers/wx_parent_gift.php
+++ app/Http/Controllers/wx_parent_gift.php
@@ -293,7 +293,10 @@ class wx_parent_gift extends Controller
     }
 
 
-    // 双11活动 理优在线
+    /***
+        双11活动 理优在线
+
+    ***/
 
     public function get_draw_num($parentid){
         // 检查是否分享朋友圈
