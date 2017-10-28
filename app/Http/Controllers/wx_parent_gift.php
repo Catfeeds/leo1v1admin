eb8f8f42724e08499979088fb77fdb078d5d19bb
jim <jim@leoedu.com>
2017-10-28 10:10:51 +0800

    f

--- app/Http/Controllers/wx_parent_gift.php
+++ app/Http/Controllers/wx_parent_gift.php
@@ -300,17 +300,21 @@ class wx_parent_gift extends Controller
 
         $this->t_ruffian_share->delete_row_by_pid($parentid);
 
+        $this->t_ruffian_share->row_insert([
+            "is_share_flag" => 1,
+            "share_time" => time(),
+            "parentid" => $parentid
+        ]);
 
+        // $check_falg = $this->t_ruffian_activity->check_share($parentid);
 
-        $check_falg = $this->t_ruffian_activity->check_share($parentid);
+        // $this->t_ruffian_activity->update_share_flag($parentid);
 
-        $this->t_ruffian_activity->update_share_flag($parentid);
+        // $ret = $this->t_ruffian_activity->get_is_share_flag($parentid);
 
-        $ret = $this->t_ruffian_activity->get_is_share_flag($parentid);
-
-        if($ret){
-            // return $this->ge
-        }
+        // if($ret){
+        //     // return $this->ge
+        // }
     }
 
     public function ruffian_activity(){ // 双11活动
