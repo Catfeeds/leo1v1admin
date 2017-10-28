8f4b99b39965e97b995ae96816f7a0ce6aad4773
jim <jim@leoedu.com>
2017-10-28 10:16:40 +0800

    f

--- app/Models/t_ruffian_share.php
+++ app/Models/t_ruffian_share.php
@@ -8,6 +8,15 @@ class t_ruffian_share extends \App\Models\Zgen\z_t_ruffian_share
 		parent::__construct();
 	}
 
+
+    public function delete_row_by_pid($parentid){
+        $sql = $this->gen_sql_new("  delete from %s t where parentid=%d"
+                                  ,self::DB_TABLE_NAME
+                                  ,$parentid
+        );
+
+        return $this->main_update($sql);
+    }
 }
 
 
