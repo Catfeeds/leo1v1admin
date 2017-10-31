5d9bc21f50f52e8398a2683c603f676e26337267
tom <tom@leoedu.com>
2017-10-31 13:12:39 +0800

    fix

--- app/Http/Controllers/tongji2.php
+++ app/Http/Controllers/tongji2.php
@@ -363,12 +363,8 @@ class tongji2 extends Controller
 		//unset($item);
         foreach($order_user_list as $item ) {
             if(!@$map[$item["adminid"] ] ) {
-                //if(isset($map[$item["adminid"]])) {
                 if ($adminid == -1  && $adminid==  $item["adminid"]   ) {
-                    //if ($adminid == $item["adminid"]){
                     $ret_info["list"][]=["adminid" => $item["adminid"] ];
-                    // $ret_info["list"][$item["adminid"]] = ["adminid" => $item["adminid"]];
-                    // dd($order_user_list,$ret_info["list"],$item["adminid"]);
                 }
             }
         }
