16616a719704e8e305a8c68bf0cd6e0d50dbc9a3
jim <jim@leoedu.com>
2017-10-31 15:24:14 +0800

    f

--- app/Http/Controllers/wx_yxyx_common.php
+++ app/Http/Controllers/wx_yxyx_common.php
@@ -649,10 +649,8 @@ class wx_yxyx_common extends Controller
         $token_info = $wx->get_token_from_code($code);
         $openid     = @$token_info["openid"];
 
-        $agent_id = session('agent_id');
-
         if($openid){ // ==> 活动页面
-            header("Location: http://wx-yxyx-web.leo1v1.com/m11/m11.html?p_phone=".$p_phone);
+            header("Location: http://wx-yxyx-web.leo1v1.com/m11/m11.html?openid=".$openid);
         }elseif(!$openid){ // 绑定会员的页面
             header("Location: http://www.leo1v1.com/market-invite/index.html?p_phone=$p_phone&type=2");
         }
