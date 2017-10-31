aa288586723791228edfb7302b43401bc1875e6a
jim <jim@leoedu.com>
2017-10-31 10:12:11 +0800

    fix

---
+++ resources/views/common_new/test_web_page.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.base')
+@section('content')
+
+    <section class="content ">
+        <div>
+            <div class="row  row-query-list" >
+                <div class="col-xs-12 col-md-5"  data-title="时间段">
+                    <div  id="id_date_range" >
+                    </div>
+                </div>
+
+                <div class="col-xs-6 col-md-2">
+                    <div class="input-group " >
+                        <span >xx</span>
+                        <input type="text" value=""  class="opt-change"  id="id_"  placeholder=""  />
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr/>
+    </section>
+
+@endsection
