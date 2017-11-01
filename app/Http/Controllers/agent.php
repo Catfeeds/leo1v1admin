55ca6b5a161086086bc181e0a370615411ade6e2
jim <jim@leoedu.com>
2017-11-01 18:58:31 +0800

    fix

--- app/Http/Controllers/agent.php
+++ app/Http/Controllers/agent.php
@@ -2013,7 +2013,7 @@ class agent extends Controller
             }
             $all_user = $all_user+$item['user_count'];
             $order_user = $order_user+$item['order_user_count'];
-            $price = $pirce+$item['pirce'];
+            $price = $price+$item['pirce'];
 
         }
         return $this->pageView(__METHOD__,$ret_info,[
