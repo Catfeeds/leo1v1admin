7d73cba85a5e45e7bccb4c21050916f4731b54b4
jim <jim@leoedu.com>
2017-11-01 15:47:27 +0800

    fix

--- app/Models/t_ruffian_activity.php
+++ app/Models/t_ruffian_activity.php
@@ -39,4 +39,8 @@ class t_ruffian_activity extends \App\Models\Zgen\z_t_ruffian_activity
 
         return $this->main_get_value($sql);
     }
+
+    public function get_prize_list($parentid){
+        
+    }
 }
