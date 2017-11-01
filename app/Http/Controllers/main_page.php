c354d824a41fa71af36f17fd8e0674602120d27c
tom <tom@leoedu.com>
2017-11-01 14:25:03 +0800

    fix

--- app/Http/Controllers/main_page.php
+++ app/Http/Controllers/main_page.php
@@ -307,13 +307,16 @@ class main_page extends Controller
     {
         $order_by_str = $this->get_in_str_val('order_by_str','');
         list($start_time,$end_time)= $this->get_in_date_range_month(date("Y-m-01"));
-        $group_start_time=   $start_time;
-        if($start_time == 1504195200){//9月,9.1-10.2
-            $end_time = 1506960000;
+        $time = time(null);
+        $ret_time = $this->t_month_def_type->get_all_list();
+        foreach($ret_time as $item){//本月
+            if($time>=$item['start_time'] && $time<$item['end_time']){
+                $start_time = $item['start_time'];
+                $end_time = $item['end_time'];
+                break;
+            }
         }
-        if($start_time == 1506787200){//10月,10.3-10.31
-            $start_time = 1506960000;
-        }
+        $group_start_time = $start_time;
         $start_first = date('Y-m-01',$start_time);
         $adminid=$this->get_account_id();
 
@@ -386,7 +389,6 @@ class main_page extends Controller
         $ret_info_first = [];
         $ret_info_two = [];
         foreach ($ret_info["list"] as $key=> &$item) {
-            $item["index"] = isset($item["index"])?$item["index"]:0;
             $item["index"]=$key+1;
             $item["all_price"] =sprintf("%.2f", $item["all_price"]  );
             if($key == 0){
