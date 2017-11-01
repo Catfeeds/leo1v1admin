e8842fbb20cc9b766b6f2135d995b90b8d3a281f
boby <boby@leoedu.com>
2017-11-01 16:41:38 +0800

    fix

--- resources/views/agent/get_yxyx_member.blade.php
+++ resources/views/agent/get_yxyx_member.blade.php
@@ -41,9 +41,9 @@
             <tbody>
                 @foreach ( $table_data_list as $var )
                     <tr>
-                        <td>{{@$var["phone1"]}} <br/>/ {{@$var["nickname1"]}} </td>
-                        <td>{{@$var["phone2"]}} <br/>/ {{@$var["nickname2"]}} </td>
-                        <td>{{@$var["phone3"]}} <br/>/ {{@$var["nickname3"]}} </td>
+                        <td>{{@$var["phone1"]}} <br/>/ {{@$var["nick1"]}} </td>
+                        <td>{{@$var["phone2"]}} <br/>/ {{@$var["nick2"]}} </td>
+                        <td>{{@$var["phone3"]}} <br/>/ {{@$var["nick3"]}} </td>
                         <td>{{@$var["user_count"]}} </td>
                         <td>{{@$var["no_revisit_count"]}} </td>
                         <td>{{@$var["no_phone_count"]}} </td>
