2a6ca9ada10d5ea1ea72dc37fe625c312945545f
boby <boby@leoedu.com>
2017-11-02 11:03:25 +0800

    fix

--- app/Http/Controllers/agent.php
+++ app/Http/Controllers/agent.php
@@ -2035,7 +2035,7 @@ class agent extends Controller
                 $item['order_rate'] = '0%';
             }
             $item['no_phone_count'] = $item['user_count'] -$item['no_revisit_count']-$item['ok_phone_count'];
-            $item['ok_phone_no_lesson'] = $item['ok_phone_count'] - $item['ok_lesson_count'];
+            $item['ok_phone_no_lesson'] = $item['ok_phone_count'] - $item['rank_count'];
             $item['ok_lesson_no_order'] = $item['ok_lesson_count'] - $item['order_user_count'];
             $all_user = $all_user+$item['user_count'];
             $order_user = $order_user+$item['order_user_count'];
