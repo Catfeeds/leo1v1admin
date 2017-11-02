017f4d500148794d4c1e36dcc193f6cab1998ba9
boby <boby@leoedu.com>
2017-11-02 13:58:30 +0800

    fix

--- public/page_js/agent/get_yxyx_member.js
+++ public/page_js/agent/get_yxyx_member.js
@@ -30,7 +30,7 @@ $(function(){
     $('.opt-a td').css('cursor','pointer');
     $('.opt-a td').on('click',function(){
         var id = $(this).parent().attr('data-id');
-        if ( localtion.search ) {
+        if ( location.search ) {
             window.open("http://admin.yb1v1.com/agent/get_yxyx_member_detail"+location.search+"&id="+id);
         } else {
             window.open("http://admin.yb1v1.com/agent/get_yxyx_member_detail?id="+id);
