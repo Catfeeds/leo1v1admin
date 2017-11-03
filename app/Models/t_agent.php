63c8034db49c1c7c4e443a649d5549ccee6f5f5f
boby <boby@leoedu.com>
2017-11-03 18:12:29 +0800

    fix

--- app/Models/t_agent.php
+++ app/Models/t_agent.php
@@ -2547,7 +2547,10 @@ class t_agent extends \App\Models\Zgen\z_t_agent
         if( $opt_type === 'no_revisit_count') {//没有拨打过电话
             $where_arr[] = " tq.id is null ";
         } else if ( $opt_type === 'no_phone_count' ) {//未拨通
-            $where_arr[] = " tq.id>0 and tq.is_called_phone=0";
+            $where_arr[] = sprintf(' tq.id>0 and tq.is_called_phone=0 and not exists (select 1 from t_tq_call_info where phone=%u and start_time >= %u and start_time<%u)',
+                                   $this->ensql($phone),
+                                   $this->ensql($start_time),
+                                   $this->ensql($end_time));
         } else if ( $opt_type === 'ok_phone_count' ) {//拨通
             $where_arr[] = " tq.id>0 and tq.is_called_phone=1";
         } else if ( $opt_type === 'ok_phone_no_lesson' ) {//拨通没排课
