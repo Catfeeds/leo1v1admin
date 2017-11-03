9ce8a726e7e420cb08312bc5e3d14382db5610d5
jim <jim@leoedu.com>
2017-11-03 14:57:38 +0800

    fix

--- app/Http/Controllers/wx_parent_gift.php
+++ app/Http/Controllers/wx_parent_gift.php
@@ -383,10 +383,7 @@ class wx_parent_gift extends Controller
 
         //检查是否可以抽奖
         $left_num = $this->get_draw_num($parentid);
-
-        if($left_num <= 0){
-            return $this->output_err("您的抽奖次数已用完!");
-        }
+        if($left_num <= 0){ return $this->output_err("您的抽奖次数已用完!"); }
 
         $stu_type = 1;
         if($check_time>$reg_time && $has_buy>0){
--- public/page_ts/tongji_ex/get_lesson_user_ip_same_info.ts
+++ public/page_ts/tongji_ex/get_lesson_user_ip_same_info.ts
@@ -1,31 +1,33 @@
 /// <reference path="../common.d.ts" />
 /// <reference path="../g_args.d.ts/tongji_ex-get_lesson_user_ip_same_info.d.ts" />
 
+function load_data(){
+    if ( window["g_load_data_flag"]) {return;}
+    $.reload_self_page ( {
+		date_type_config:	$('#id_date_type_config').val(),
+		date_type:	$('#id_date_type').val(),
+		opt_date_type:	$('#id_opt_date_type').val(),
+		start_time:	$('#id_start_time').val(),
+		end_time:	$('#id_end_time').val(),
+		match_type:	$('#id_match_type').val()
+    });
+}
 $(function(){
-    function load_data(){
-        $.reload_self_page ( {
-			date_type:	$('#id_date_type').val(),
-			opt_date_type:	$('#id_opt_date_type').val(),
-			start_time:	$('#id_start_time').val(),
-			end_time:	$('#id_end_time').val(),
-			tongji_type:	$('#id_tongji_type').val()
-        });
-    }
 
-	Enum_map.append_option_list("tongji_type",$("#id_tongji_type")); 
 
     $('#id_date_range').select_date_range({
         'date_type' : g_args.date_type,
         'opt_date_type' : g_args.opt_date_type,
         'start_time'    : g_args.start_time,
         'end_time'      : g_args.end_time,
-        date_type_config : JSON.parse( g_args.date_type_config), 
+        date_type_config : JSON.parse( g_args.date_type_config),
         onQuery :function() {
             load_data();
         }
     });
-	$('#id_tongji_type').val(g_args.tongji_type);
+	$('#id_match_type').val(g_args.match_type);
 
 
 	$('.opt-change').set_input_change_event(load_data);
 });
+
