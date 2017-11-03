2d351873148b58fb0f7541c30d0b8dd34bcb3c57
tom <tom@leoedu.com>
2017-11-03 14:43:32 +0800

    fix

--- app/Http/Controllers/agent.php
+++ app/Http/Controllers/agent.php
@@ -502,68 +502,61 @@ class agent extends Controller
             }
         }
         $start_time_arr = array_unique(array_column($ret_time,'start_time'));
-        dd($start_time_this,$end_time_this,$start_time_last,$end_time_last,$start_time_very_last,$end_time_very_last,$reduce_flag);
         $account_role = E\Eaccount_role::V_2;
-        $seller_list = $this->task->t_manager_info->get_seller_list_new_two($account_role);
-        $ret_level_goal = $this->task->t_seller_level_goal->get_all_list_new();
+        $seller_list = $this->t_manager_info->get_seller_list_new_two($account_role);
+        $ret_level_goal = $this->t_seller_level_goal->get_all_list_new();
         foreach($seller_list as $item){
             $adminid = $item['uid'];
-            $face_pic = $item['face_pic'];
-            if($face_pic == ''){
-                $face_pic = 'http://7u2f5q.com2.z0.glb.qiniucdn.com/fdc4c3830ce59d611028f24fced65f321504755368876.png';
-            }
-            $account = $this->task->t_manager_info->get_account_by_uid($adminid);
+            $account = $item['account'];
+            $face_pic = $face_pic!=''?$item['face_pic']:'http://7u2f5q.com2.z0.glb.qiniucdn.com/fdc4c3830ce59d611028f24fced65f321504755368876.png';
             $this_level = $item['seller_level'];
+            $num = isset($item['num'])?$item['num']:0;
+            $level_goal = isset($item['level_goal'])?$item['level_goal']:0;
             $become_member_time = $item['create_time'];
-            $ret_this = $this->task->t_seller_level_goal->field_get_list($item['seller_level'],'*');
-            $num = isset($ret_this['num'])?$ret_this['num']:0;
-            $level_goal = isset($ret_this['level_goal'])?$ret_this['level_goal']:0;
-            $next_goal = $level_goal;
-            $next_num = $num + 1;
-            $ret_next = $this->task->t_seller_level_goal->get_next_level_by_num($next_num);
-            if($ret_next){
-                $next_goal = $ret_next['level_goal'];
-            }
-            //统计本月
-            $price = $this->task->t_order_info->get_seller_price($start_time_this,$end_time_this,$adminid);
-            $price = $price/100;
-            // if($price>$next_goal && !in_array($adminid,[380,457])){
-            if($price>$next_goal){//升级
-                foreach($ret_level_goal as $item){
-                    if($price >= $item['level_goal']){
-                        $next_level = $item['seller_level'];
-                        $level_face = $item['level_face'];
+            $ret_next = $this->t_seller_level_goal->get_next_level_by_num($num+1);
+            $next_goal = isset($ret_next['level_goal'])?$ret_next['level_goal']:$level_goal;
+            if($reduce_flag == 1){//月头
+                //统计上个月
+                $price = $this->task->t_order_info->get_seller_price($start_time_last,$end_time_last,$adminid);
+                $price = $price/100;
+                if($price<$level_goal){//降级
+                    foreach($ret_level_goal as $item){
+                        if($price >= $item['level_goal']){
+                            $next_level = $item['seller_level'];
+                            $level_face = $item['level_face'];
+                        }
                     }
                 }
-                $face_pic_str = substr($face_pic,-12,5);
-                $ex_str = $next_level.$face_pic_str;
-                $level_face_pic = $this->get_top_img($adminid,$face_pic,$level_face,$ex_str);
-                $this->task->t_manager_info->field_update_list($adminid,[
-                    'seller_level'=>$next_level,
-                    'level_face_pic'=>$level_face_pic,
-                ]);
-
-                $this->task->t_seller_edit_log->row_insert([
-                    "uid"         => $adminid,
-                    "type"        => 2,
-                    "old"         => $this_level,
-                    "new"         => $next_level,
-                    "create_time" => time(NULL),
-                ],false,false,true );
-                $this->task->t_manager_info->send_wx_todo_msg_by_adminid($adminid,"咨询师等级升级","咨询师等级升级",$account."从".E\Eseller_level::get_desc($this_level)."级升级为".E\Eseller_level::get_desc($next_level)."级","");
-                $this->task->t_manager_info->send_wx_todo_msg_by_adminid(898,"咨询师等级升级","咨询师等级升级",$account."从".E\Eseller_level::get_desc($this_level)."级升级为".E\Eseller_level::get_desc($next_level)."级","");
-                $this->task->t_manager_info->send_wx_todo_msg_by_adminid(412,"咨询师等级升级","咨询师等级升级",$account."从".E\Eseller_level::get_desc($this_level)."级升级为".E\Eseller_level::get_desc($next_level)."级","");
-            }else{//月头降级
-                if($reduce_flag == 1){
-
+            }else{//月中
+                //统计本月
+                $price = $this->t_order_info->get_seller_price($start_time_this,$end_time_this,$adminid);
+                $price = $price/100;
+                if($price>$next_goal){//升级
+                    foreach($ret_level_goal as $item){
+                        if($price >= $item['level_goal']){
+                            $next_level = $item['seller_level'];
+                            $level_face = $item['level_face'];
+                        }
+                    }
                 }
             }
-            //统计上个月
-            $price_last = $this->task->t_order_info->get_seller_price($start_time_last,$end_time_last,$adminid);
-            $price_last = $price/100;
-            //统计上上个月
-            $price_very_last = $this->task->t_order_info->get_seller_price($start_time_very_last,$end_time_very_last,$adminid);
-            $price_very_last = $price_very_last/100;
+            $face_pic_str = substr($face_pic,-12,5);
+            $ex_str = $next_level.$face_pic_str;
+            $level_face_pic = $this->get_top_img($adminid,$face_pic,$level_face,$ex_str);
+            $this->t_manager_info->field_update_list($adminid,[
+                'seller_level'=>$next_level,
+                'level_face_pic'=>$level_face_pic,
+            ]);
+            $this->t_seller_edit_log->row_insert([
+                "uid"         => $adminid,
+                "type"        => 2,
+                "old"         => $this_level,
+                "new"         => $next_level,
+                "create_time" => time(NULL),
+            ],false,false,true );
+            $this->t_manager_info->send_wx_todo_msg_by_adminid($adminid,"咨询师等级升级","咨询师等级升级",$account."从".E\Eseller_level::get_desc($this_level)."级升级为".E\Eseller_level::get_desc($next_level)."级","");
+            $this->t_manager_info->send_wx_todo_msg_by_adminid(898,"咨询师等级升级","咨询师等级升级",$account."从".E\Eseller_level::get_desc($this_level)."级升级为".E\Eseller_level::get_desc($next_level)."级","");
+            $this->t_manager_info->send_wx_todo_msg_by_adminid(412,"咨询师等级升级","咨询师等级升级",$account."从".E\Eseller_level::get_desc($this_level)."级升级为".E\Eseller_level::get_desc($next_level)."级","");
         }
 
     }
