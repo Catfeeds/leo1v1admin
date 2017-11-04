ee356459574fc347b8588f2ba1914e61ef89b0dd
tom <tom@leoedu.com>
2017-11-04 18:08:58 +0800

    fix

--- public/page_ts/g_args.d.ts/tongji_ss-require_count_seller.d.ts
+++ public/page_ts/g_args.d.ts/tongji_ss-require_count_seller.d.ts
@@ -36,17 +36,18 @@ tofile:
 /// <reference path="../common.d.ts" />
 /// <reference path="../g_args.d.ts/tongji_ss-require_count_seller.d.ts" />
 
+function load_data(){
+    if ( window["g_load_data_flag"]) {return;}
+    $.reload_self_page ( {
+		date_type_config:	$('#id_date_type_config').val(),
+		date_type:	$('#id_date_type').val(),
+		opt_date_type:	$('#id_opt_date_type').val(),
+		start_time:	$('#id_start_time').val(),
+		end_time:	$('#id_end_time').val(),
+		seller_groupid_ex:	$('#id_seller_groupid_ex').val()
+    });
+}
 $(function(){
-    function load_data(){
-        $.reload_self_page ( {
-			date_type_config:	$('#id_date_type_config').val(),
-			date_type:	$('#id_date_type').val(),
-			opt_date_type:	$('#id_opt_date_type').val(),
-			start_time:	$('#id_start_time').val(),
-			end_time:	$('#id_end_time').val(),
-			seller_groupid_ex:	$('#id_seller_groupid_ex').val()
-        });
-    }
 
 
     $('#id_date_range').select_date_range({
