db789f2b0357c0276500074f1e0d0f93f3ecb923
tom <tom@leoedu.com>
2017-11-04 18:23:06 +0800

    fix

--- app/Http/Controllers/common.php
+++ app/Http/Controllers/common.php
@@ -700,9 +700,9 @@ class common extends Controller
         }
         $qiniu         = \App\Helper\Config::get_config("qiniu");
         if ( \App\Helper\Utils::check_env_is_test() ) {
-            $phone_qr_name = $phone."_qr_agent_test_new.png";
+            $phone_qr_name = $phone."_qr_agent_ppx_new.png";
         }else{
-            $phone_qr_name = $phone."_qr_agent_gkk_new.png";
+            $phone_qr_name = $phone."_qr_agent_ppx_new.png";
         }
         $qiniu_url     = $qiniu['public']['url'];
         \App\Helper\Utils::logger("CHECK is_exists start");
@@ -768,7 +768,7 @@ class common extends Controller
             }
             imagepng($image_3,$agent_qr_url);
 
-            $file_name = \App\Helper\Utils::qiniu_upload($agent_qr_url);
+            $file_name = \App\Helper\Utils::qiniu_upload($phone_qr_url);
 
             if($file_name!=''){
                 $cmd_rm = "rm /tmp/".$phone."*.png";
