5ac3b28e0548fe373cac4650e4e188e30d772820
jim <jim@leoedu.com>
2017-11-04 17:37:53 +0800

    fix

--- app/Jobs/SendParentWx.php
+++ app/Jobs/SendParentWx.php
@@ -15,11 +15,11 @@ class SendParentWx extends Job implements ShouldQueue
      *
      * @return void
      */
-    public function __construct($user_list,$template_id,$data,$url)
+    public function __construct($parent_list,$template_id,$data,$url)
     {
         //
         $this->wx_info=[
-            "parent_list"  => $user_list,
+            "parent_list"  => $parent_list,
             "template_id" => $template_id,
             "data"        => $data,
             "url"         => $url,
@@ -36,13 +36,15 @@ class SendParentWx extends Job implements ShouldQueue
         $wx_info     = $this->wx_info;
         $template_id = $wx_info['template_id'];
         $data        = $wx_info['data'];
-        $user_list   = $wx_info['parent_list'];
+        $parent_list = $wx_info['parent_list'];
         $url         = $wx_info['url'];
 
-        foreach($user_list as $user_val){
+        foreach($parent_list as $user_val){
             if(isset($user_val['wx_openid'])){
                 \App\Helper\Utils::send_wx_to_parent($user_val['wx_openid'] ,$template_id,$data,$url);
             }
         }
+        \App\Helper\Utils::logger("succ");
+
     }
 }
