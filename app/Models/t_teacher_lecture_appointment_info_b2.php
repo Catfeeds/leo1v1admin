22aa5c42dae6b12d47b1d1ba1ff4e0b53c568c52
ricky <ricky@leoedu.com>
2017-11-04 11:07:55 +0800

    f

--- app/Models/t_teacher_lecture_appointment_info_b2.php
+++ app/Models/t_teacher_lecture_appointment_info_b2.php
@@ -35,12 +35,12 @@ class t_teacher_lecture_appointment_info_b2 extends \App\Models\Zgen\z_t_teacher
     //
     public function get_teacher_list(){
         // 拉取所有数据
-        $sql = $this->gen_sql_new("select teacherid,realname,user_agent from %s where user_agent!='' ",t_teacher_info::DB_TABLE_NAME);
+        $sql = $this->gen_sql_new("select teacherid,realname,user_agent from %s where user_agent!='' and is_test_user=0 ",t_teacher_info::DB_TABLE_NAME);
         return $this->main_get_list($sql);
     }
 
     public function get_student_list() {
-        $sql = $this->gen_sql_new("select userid,realname,user_agent from %s where user_agent!='' ",t_student_info::DB_TABLE_NAME);
+        $sql = $this->gen_sql_new("select userid,realname,user_agent from %s where user_agent!='' and is_test_user=0 ",t_student_info::DB_TABLE_NAME);
         return $this->main_get_list($sql);
     }
 }
