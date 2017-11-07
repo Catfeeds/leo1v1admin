3834f91f71cb40908601924dabafbde1c560498b
jim <jim@leoedu.com>
2017-11-07 17:46:25 +0800

    fix

--- app/Config/publish_version.php
+++ app/Config/publish_version.php
@@ -2,5 +2,5 @@
 namespace App\Config;
 class publish_version {
     //不要手动修改
-    static public $version="20171107-174006";
+    static public $version="20171107-174452";
 };
--- app/Http/Controllers/assistant_performance.php
+++ app/Http/Controllers/assistant_performance.php
@@ -18,6 +18,16 @@ class assistant_performance extends Controller
         }
         $assistantid = $this->t_assistant_info->get_assistantid( $account);
         $ret_info = $this->t_student_info->get_assistant_read_stu_info($assistantid);
+        $month_start = strtotime(date("Y-m-01",time()));
+        $month_end = strtotime(date($month_end_str));
+        $cur_start = $month_start+15*86400;
+        $cur_end =  $month_end;
+        $last_start = $month_start;
+        $last_end =  $month_start+15*86400;
+        $cur_time_str  = date("m.d",$cur_start)."-".date("m.d",$cur_end-300);
+        $last_time_str = date("m.d",$last_start)."-".date("m.d",$last_end-300);
+        dd($cur_time_str);
+
         return $this->pageView(__METHOD__,$ret_info);
     }
 
