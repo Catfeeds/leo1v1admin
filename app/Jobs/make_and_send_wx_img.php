d4881f5c497f170edd0c9a323280c4debf9855d7
boby <boby@leoedu.com>
2017-11-07 16:32:01 +0800

    fix

--- app/Jobs/make_and_send_wx_img.php
+++ app/Jobs/make_and_send_wx_img.php
@@ -7,13 +7,14 @@ use App\Enums as E;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Yxyx\Core\Media;
 
-    use Yxyx\Core\AccessToken;
-    use LaneWeChat\Core\ResponsePassive;
 class make_and_send_wx_img extends Job implements ShouldQueue
 {
     use InteractsWithQueue, SerializesModels;
+    use Yxyx\Core\Media;
+    use Yxyx\Core\AccessToken;
+    use LaneWeChat\Core\ResponsePassive;
+
 
     var $wx_openid;
     var $id;
