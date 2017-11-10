2fdd75778af368a40d763d9e1a6e8dfea4b60dd6
jim <jim@leoedu.com>
2017-11-10 14:28:57 +0800

    f

--- public/page_ts/human_resource/teacher_lecture_list.ts
+++ public/page_ts/human_resource/teacher_lecture_list.ts
@@ -339,10 +339,10 @@ $(function(){
         },function(result){
             var num = result.num;
             console.log(num);
-            // if(num>1 && account=="" && opt_data.phone != 13079618620){
-            //     BootstrapDialog.alert("该老师重审次数超过1次,不能再审核,请联系技术人员处理!!");
-            //     return;
-            // }
+            if(num>4 && account=="" && opt_data.phone != 13079618620){
+                BootstrapDialog.alert("该老师重审次数超过1次,不能再审核,请联系技术人员处理!!");
+                return;
+            }
         });
 
         $.do_ajax("/human_resource/get_week_confirm_num",{
@@ -711,7 +711,7 @@ $(function(){
         },function(result){
             var num = result.num;
             console.log(num);
-            if(num>2  && data.phone != 13079618620){
+            if(num>4  && data.phone != 13079618620){
                 BootstrapDialog.alert("该老师没有审核机会了!!");
                 return;
             }else{
