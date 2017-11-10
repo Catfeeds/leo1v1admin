84a7e022abf948d6ab0be59a235c8f41dc0f1579
jim <jim@leoedu.com>
2017-11-10 17:24:44 +0800

    fix

--- app/OrderPrice/Activity/activity_2017111003.php
+++ app/OrderPrice/Activity/activity_2017111003.php
@@ -3,30 +3,19 @@ namespace App\OrderPrice\Activity;
 use \App\Enums as E;
 class activity_2017111003  extends  activity_config_base {
 
-    public static $order_activity_type= E\Eorder_activity_type::V_2017102701;
+    public static $order_activity_type= E\Eorder_activity_type::V_2017111003;
 
     public function __construct(  $args   ) {
         parent::__construct($args);
 
-        //2017-102701 27-31   送课:  60->3(全款) ,90->6,120->9.. :  ->((n/30)-1)*3
-        $this->date_range=[ "2017-10-27"  , "2017-11-31"];
+        //2017-1110-12 ,CR,90次, 全款, 送9次  
+        $this->date_range=[ "2017-11-10"  , "2017-11-11"];
         $this->period_flag_list= [ E\Eperiod_flag::V_0   ];
-        $this->contract_type_list = [E\Econtract_type::V_0 ];
+        $this->contract_type_list = [ E\Econtract_type::V_3 ];
+        $this->lesson_times_range=[90,90];
+
         $this->lesson_times_present_lesson_count = [
-            60  => 3,
-            90  => 6,
-            120 => 8,
-            150 => 10,
-            180 => 12,
-            210 => 14,
-            240 => 16,
-            270 => 18,
-            300 => 20,
-            330 => 22,
-            360 => 24,
-            390 => 26,
-            420 => 28,
-            450 => 30,
+            90  => 9,
         ];
     }
 }
