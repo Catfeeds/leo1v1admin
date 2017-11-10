789070785d5d84238779924c2fa52386bad7e053
jim <jim@leoedu.com>
2017-11-10 10:27:08 +0800

    fix

--- app/Models/t_lesson_info_b3.php
+++ app/Models/t_lesson_info_b3.php
@@ -1803,7 +1803,7 @@ class t_lesson_info_b3 extends \App\Models\Zgen\z_t_lesson_info{
                                   $where_arr
         );
         return $this->main_get_row($sql);
-  
+
     }
 
     public function get_late_lesson_info($late_time){
@@ -1812,8 +1812,8 @@ class t_lesson_info_b3 extends \App\Models\Zgen\z_t_lesson_info{
             "l.lesson_del_flag = 0",
             "l.tea_rate_time=0",
             "l.lesson_type in (0,1,3)",
-            "l.lesson_end < $late_time",
-            "l.lesson_end >= $late_time_begin",
+            "l.lesson_end > $late_time",
+            "l.lesson_end <= $late_time_begin",
         ];
 
         $sql = $this->gen_sql_new("  select t.wx_openid, l.lesson_start, l.lesson_end, l.subject, s.nick as stu_nick, t.nick as tea_nick from %s l"
