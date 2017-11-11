5c32d60d88ea99c4c9ea0bb587ea054d32a81136
ricky <ricky@leoedu.com>
2017-11-11 16:50:23 +0800

    f

--- resources/views/teacher_money/teacher_salary_list.blade.php
+++ resources/views/teacher_money/teacher_salary_list.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <script type="text/javascript" src="/page_js/lib/select_dlg_ajax.js"></script>
     <script type="text/javascript">
-        var g_data = <?php echo json_encode(['start_time'=>date('Y-m-e',$start_time),'end_time'=>('Y-m-d',$end_time)]);?>
+        var g_data = <?php echo json_encode(['start_time'=>date('Y-m-e',$start_time),'end_time'=>date('Y-m-d',$end_time)]);?>
     </script>
     <section class="content ">
         <div>
