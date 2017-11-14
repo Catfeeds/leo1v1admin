850392a26cf5f2f8bcbccf2b8ede360bfea58190
jim <jim@leoedu.com>
2017-11-14 14:12:54 +0800

    fix

--- app/Http/Controllers/main_page.php
+++ app/Http/Controllers/main_page.php
@@ -97,10 +97,10 @@ class main_page extends Controller
                 }
 
 
-                if($ret_info['formal_num']>0){ //平均人效
-                    $ret_info['aver_money'] = $ret_info['all_order_price']/$ret_info['formal_num'];
+                if($ret_info['formal_num']>0){ //平均人效 all cc签单额/all cc签单人数
+                    $ret_info['aver_money_cc'] = $ret_info['all_order_price']/$ret_info['formal_num'];
                 }else{
-                    $ret_info['aver_money'] = 0;
+                    $ret_info['aver_money_cc'] = 0;
                 }
 
                 // aver_money_cc
