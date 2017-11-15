b58a1ac6dd97956bba90be0a30dce51a6925be57
jim <jim@leoedu.com>
2017-11-15 16:13:18 +0800

    fix

--- database/migrations/2017_11_15_160956_t_cr_week_month_info_add_grade_stu_list.php
+++ database/migrations/2017_11_15_160956_t_cr_week_month_info_add_grade_stu_list.php
@@ -13,6 +13,11 @@ class TCrWeekMonthInfoAddGradeStuList extends Migration
     public function up()
     {
         //
+        Schema::table('db_weiyi.t_cr_week_month_info', function( Blueprint $table)
+        {
+            t_field($table->string("grade_stu_list",512),"月初各年级在读学生数");
+        });
+
     }
 
     /**
