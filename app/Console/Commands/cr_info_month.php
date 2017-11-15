56fba8878fbc74f73a1504f61cd87850b82f8525
jim <jim@leoedu.com>
2017-11-15 15:12:00 +0800

    fix

--- app/Console/Commands/cr_info_month.php
+++ app/Console/Commands/cr_info_month.php
@@ -331,6 +331,9 @@ class cr_info_month extends Command
         $arr["all_registered_student"] = $arr['finish_num']+$arr["read_num"]+$arr["stop_student"]+$arr["drop_student"]+$arr["summer_winter_stop_student"];
         $arr["student_end_per"] = round($arr["finish_num"]/$arr["all_registered_student"]*100,2)*100;
 
+        //课时消耗目标数量
+        
+
 
         $insert_data = [
           "create_time"             => $create_time,            //存档时间
