ebdc642921c6e04a97aa0a5089883d1322405180
jim <jim@leoedu.com>
2017-11-15 15:16:55 +0800

    fix

--- resources/views/human_resource/index.blade.php
+++ resources/views/human_resource/index.blade.php
@@ -351,6 +351,9 @@
                 <!-- <div class="col-xs-6 col-md-2">
                      <button class="btn btn-primary" id="id_add_teacher"> 新增老师 </button>
                 </div> -->
+                <div class="col-xs-6 col-md-2">
+                    <button class="btn btn-primary" id="id_add_teacher_"> 新增教研老师 </button>
+                </div>
                 <div class="col-xs-6 col-md-1">
                     <button class="btn" id="id_limit_week_lesson_num_person" data-value="{{$week_num_person}}" >{{$week_num_person}}</button>
                 </div>
