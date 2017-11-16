a83319b65cee298038f222a12e815f2a4ff7e0a8
boby <boby@leoedu.com>
2017-11-16 17:33:18 +0800

    fix

--- resources/views/seller_student_new/assign_sub_adminid_list.blade.php
+++ resources/views/seller_student_new/assign_sub_adminid_list.blade.php
@@ -385,9 +385,7 @@
                 @foreach ( $table_data_list as $var )
                     <tr>
                         <td>
-                            @if($var['auto_allot_adminid'] == 0)
-                                <input type="checkbox" class="opt-select-item" data-userid="{{$var["userid"]}}"/>   {{$var["index"]}}
-                            @endif
+                            <input type="checkbox" class="opt-select-item" data-userid="{{$var["userid"]}}"/>   {{$var["index"]}}
                         </td>
                         <td>{{$var["opt_time"]}} </td>
                         <td>{{$var["phone"]}} </td>
