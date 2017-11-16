541702367ebca389ff6b30b61257e44200787d55
james <james@leoedu.com>
2017-11-16 10:25:18 +0800

    fix

--- app/Http/Controllers/user_manage.php
+++ app/Http/Controllers/user_manage.php
@@ -284,7 +284,7 @@ class user_manage extends Controller
         }
 
         $now=time(NULL);
-        foreach($ret_info['list'] as &$item) {
+        foreach($ret_info['list'] as $i=> &$item) {
             $item['originid']          = E\Estu_origin::get_desc($item['originid']);
             $item['type_str']          = $item['type']  ;
             $item['type']              = E\Estudent_type::get_desc($item['type']);
@@ -374,6 +374,11 @@ class user_manage extends Controller
             // 检查交接单是否有驳回
             // $row['confirm_flag'] = $this->t_lesson_info_b3->check_is_consume($orderid);
 
+            $reject_status = $this->t_student_info->check_is_reject($item['userid']);
+
+            if($reject_status == 3){
+                unset($ret_info['list'][$i]);
+            }
 
 
         }
