8547e675fea3b8379f893293278127b630d061f7
jack <jack@leodeu.com>
2017-11-17 19:29:27 +0800

    fix

--- database/migrations/2017_11_17_185726_t_teacher_record_list_add_free_time.php
+++ database/migrations/2017_11_17_185726_t_teacher_record_list_add_free_time.php
@@ -16,7 +16,6 @@ class TTeacherRecordListAddFreeTime extends Migration
         Schema::table('db_weiyi.t_teacher_record_list', function( Blueprint $table)
         {
             t_field($table->tinyInteger("gender"),"性别");
-            t_field($table->tinyInteger("work_year"),"教龄");
             t_field($table->string("teacher_textbook"),"教材版本");
             t_field($table->string("region",64),"地区");
             t_field($table->text("free_time"),"空闲时间");
