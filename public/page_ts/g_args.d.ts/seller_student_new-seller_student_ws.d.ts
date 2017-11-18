83a9dded033a2d6b4686b42fc081c0a8199f9045
sam <sam@leoedu.com>
2017-11-18 10:18:16 +0800

    fix

--- public/page_ts/g_args.d.ts/seller_student_new-seller_student_ws.d.ts
+++ public/page_ts/g_args.d.ts/seller_student_new-seller_student_ws.d.ts
@@ -18,12 +18,13 @@ tofile:
 /// <reference path="../common.d.ts" />
 /// <reference path="../g_args.d.ts/seller_student_new-seller_student_ws.d.ts" />
 
-$(function(){
-    function load_data(){
-        $.reload_self_page ( {
+function load_data(){
+    if ( window["g_load_data_flag"]) {return;}
+    $.reload_self_page ( {
 
-        });
-    }
+    });
+}
+$(function(){
 
 
 
