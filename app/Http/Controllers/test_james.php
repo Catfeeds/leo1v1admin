95cb61c8c48aafbaea9e4184207d5d6bc25aaf0a
james <james@leoedu.com>
2017-11-23 10:23:59 +0800

    s

--- app/Http/Controllers/test_james.php
+++ app/Http/Controllers/test_james.php
@@ -1269,8 +1269,8 @@ $test=	3;
     public function translate_pdf(){
         $path = $this->get_in_str_val('path');
         $cmd  = "curl -F doc=@'$path' 'http://ts.whytouch.com/mass_up.php?token=bbcffc83539bd9069b755e1d359bc70a&mode=-1&aut=James&fn=新文件.pptx'";
-        dd($cmd);
-        // $out = shell_exec($cmd);
+        // dd($cmd);
+        $out = shell_exec($cmd);
         dd($out);
     }
 
