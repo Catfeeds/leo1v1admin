1e566d3d5a29bee40bca40c47c94a1594b823923
jack <jack@leodeu.com>
2017-11-23 14:19:50 +0800

    fix

--- enum_config/config_level.php
+++ enum_config/config_level.php
@@ -1,9 +1,9 @@
 <?php
 
 return array(
-    array (0 , "" ,"C","初级"),
-    array (1 , "" ,"B","中级"),
-    array (2 , "" ,"A","高级"),
-    array (3 , "" ,"A+","金牌"),
+    array (0 , "Junior" ,"C","初级"),
+    array (1 , "Intermediate" ,"B","中级"),
+    array (2 , "Senior" ,"A","高级"),
+    array (3 , "gold" ,"A+","金牌"),
     array (4 , "" ,"S" ),
 );
