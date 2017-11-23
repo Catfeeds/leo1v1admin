3b52094219d335f13e96e9f6b984b189a6211289
adrian <adrian@leoedu.com>
2017-11-23 17:37:03 +0800

    fix'

--- app/Helper/Email.php
+++ app/Helper/Email.php
@@ -10,7 +10,6 @@ class Email{
      * 理优教研组
      */
     static public function SendMailLeoCom($Address,$Title,$Message,$IsHtml=true,$AddAddressKey=0){
-        $MailHost = "smtp.leoedu.com";
         $Username = "jim@leoedu.com";
         $Password = "xcwen142857";
         $From     = "jim@leoedu.com";
@@ -24,11 +23,23 @@ class Email{
      * 教学管理事业部教学部
      */
     static public function SendMailJiaoXue($Address,$Title,$Message,$IsHtml=true,$AddAddressKey=0){
-        $MailHost = "smtp.leoedu.com";
         $Username = "leojiaoxuebu@leoedu.com";
         $Password = "leojiaoxuebu123";
         $From     = "leojiaoxuebu@leoedu.com";
         $FromName = "教学管理事业部教学部";
+
+        $ret = self::SendMail($MailHost,$Username,$Password,$From,$FromName,$Address,$Title,$Message,$IsHtml,$AddAddressKey);
+        return $ret;
+    }
+
+    /**
+     * 理优教学管理部
+     */
+    static public function SendMailEmd($Address,$Title,$Message,$IsHtml=true,$AddAddressKey=0){
+        $Username = "emd@leoedu.com";
+        $Password = "emd123456";
+        $From     = "emd@leoedu.com";
+        $FromName = "理优教学管理部";
 
         $ret = self::SendMail($MailHost,$Username,$Password,$From,$FromName,$Address,$Title,$Message,$IsHtml,$AddAddressKey);
         return $ret;
@@ -59,7 +70,8 @@ class Email{
         $mail->IsSMTP();
         //设置编码，否则发送中文乱码
         $mail->CharSet ="UTF-8";
-        $mail->Host = $MailHost;
+        $mail->Host = "smtp.leoedu.com";
+
         // 启用SMTP验证功能
         $mail->SMTPAuth = true;
         $mail->SMTPSecure="tls";
