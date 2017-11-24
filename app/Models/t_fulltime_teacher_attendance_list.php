f5d9f9924f608c8f80e8311fda80ed923acc7a50
jack <jack@leodeu.com>
2017-11-24 14:07:57 +0800

    fix

--- app/Models/t_fulltime_teacher_attendance_list.php
+++ app/Models/t_fulltime_teacher_attendance_list.php
@@ -41,6 +41,19 @@ class t_fulltime_teacher_attendance_list extends \App\Models\Zgen\z_t_fulltime_t
 
     }
 
+    public function check_is_in_holiday($teacherid,$day_time){
+        $where_arr=[
+            ["attendance_time>=%u",$day_time,-1],
+            ["holiday_end_time<=%u",$day_time,-1],
+            ["teacherid=%u",$teacherid,-1],
+            "attendance_type=3"
+        ];
+        $sql = $this->gen_sql_new("select 1 from %s where %s",self::DB_TABLE_NAME,$where_arr);
+        return $this->main_get_value($sql);
+
+    }
+
+
     public function get_holiday_info(){
         $sql = $this->gen_sql_new("select * from %s where attendance_type=3",self::DB_TABLE_NAME);
         return $this->main_get_list($sql);
