88b4180f3f1badac33f1b3a252fd3f81bc1feb06
jack <jack@leodeu.com>
2017-11-24 18:33:17 +0800

    fix

--- app/Http/Controllers/test_jack.php
+++ app/Http/Controllers/test_jack.php
@@ -342,7 +342,12 @@ class test_jack  extends Controller
     }
 
     public function test_period(){
-
+        $start_time = strtotime("2017-09-01");
+        $end_time = strtotime("2017-10-01");
+        $contract_type = $this->get_in_int_val("contract_type",0);
+        $order_info = $this->t_order_info_finance->get_order_info($start_time,$end_time,$contract_type);
+        $order_info_t = $this->t_order_info_finance->get_order_tongji_info($start_time,$end_time,$contract_type);
+        dd([$order_info,$order_info_t]);
         // $time = time()-7*86400;
         dd(date("w",time()));
         $day_time = strtotime("2017-10-09");
--- app/Models/t_order_info_finance.php
+++ app/Models/t_order_info_finance.php
@@ -8,6 +8,48 @@ class t_order_info_finance extends \App\Models\Zgen\z_t_order_info_finance
 		parent::__construct();
 	}
 
+    public function get_order_info($start_time,$end_time,$contract_type){
+        $where_arr=[
+            ["o.order_time>=%u",$start_time,0],  
+            ["o.order_time<%u",$end_time,0],
+            ["o.contract_type=%u",$contract_type,-1],
+            "s.is_test_user=0",
+            "o.contract_status>0",
+            " o.check_money_flag =1"
+        ];
+        $sql = $this->gen_sql_new("select o.*  "
+                                  ." from %s o left join %s s on o.userid = s.userid"
+                                  ." where %s",
+                                  self::DB_TABLE_NAME,
+                                  t_student_info::DB_TABLE_NAME,
+                                  $where_arr
+        );
+        return $this->main_get_list($sql);
+        
+
+    }
+    public function get_order_tongji_info($start_time,$end_time,$contract_type){
+        $where_arr=[
+            ["o.order_time>=%u",$start_time,0],  
+            ["o.order_time<%u",$end_time,0],
+            ["o.contract_type=%u",$contract_type,-1],
+            "s.is_test_user=0",
+            "o.contract_status>0",
+            " o.check_money_flag =1"
+        ];
+        $sql = $this->gen_sql_new("select count(o.userid) num,sum(o.price) money  "
+                                  ." from %s o left join %s s on o.userid = s.userid"
+                                  ." where %s",
+                                  self::DB_TABLE_NAME,
+                                  t_student_info::DB_TABLE_NAME,
+                                  $where_arr
+        );
+        return $this->main_get_row($sql);
+        
+
+    }
+
+
 }
 
 
