1d04db26a6b83244c22008e880d7e9e11af7f4a3
ricky <ricky@leoedu.com>
2017-11-24 14:54:26 +0800

    f

---
+++ database/migrations/2017_11_23_132147_create_company_wx_department.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCompanyWxDepartment extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('db_weiyi_admin.t_company_wx_department', function(Blueprint $table) {
+            t_field($table->increments("id"),"企业微信员工组织表");
+            t_field($table->string("name",50),"部门名");
+            t_field($table->integer("parentid"),"父部门id");
+            t_field($table->integer("order"),"排序");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
