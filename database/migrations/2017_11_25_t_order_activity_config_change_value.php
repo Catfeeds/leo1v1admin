ef535f6e34b8f6c55113c58906a34222b5bcdba3
xcwen <xcwenn@qq.com>
2017-11-25 18:01:17 +0800

    f

--- database/migrations/2017_11_25_t_order_activity_config_change_value.php
+++ database/migrations/2017_11_25_t_order_activity_config_change_value.php
@@ -12,9 +12,14 @@ class OrderActivityConfigChangeValue extends Migration
      */
     public function up()
     {
-        Schema::table('db_weiyi_admin.t_web_page_trace_log', function (Blueprint $table){
-            t_field($table->integer('share_wx_flag')->default(0),"是否分享朋友圈");
+
+
+        Schema::table('db_weiyi.t_order_activity_config', function (Blueprint $table){
+
+            t_field($table->integer('need_spec_require_flag')->default(0),"是否需要特殊申请 0:不需要 1:需要");
+
         });
+
     }
 
     /**
