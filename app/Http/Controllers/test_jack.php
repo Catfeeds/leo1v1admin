346c3f54a0fa867ed7a7378b45ecd7cb0047064b
jack <jack@leodeu.com>
2017-11-25 17:58:21 +0800

    fix

--- app/Http/Controllers/test_jack.php
+++ app/Http/Controllers/test_jack.php
@@ -342,8 +342,8 @@ class test_jack  extends Controller
     }
 
     public function test_period(){
-        $start_time = strtotime("2017-07-01");
-        $end_time = strtotime("2017-08-01");
+        $start_time = strtotime("2017-06-01");
+        $end_time = strtotime("2017-07-01");
         // $list = $this->t_order_info_finance->get_add_info();
         // foreach($list as $val){
         //     $val["contract_starttime"] = strtotime("+1 months",$val["contract_starttime"]);
@@ -376,21 +376,21 @@ class test_jack  extends Controller
                 // $val["contract_starttime"] = strtotime("+2 months",$val["contract_starttime"]);
                 // $val["contract_endtime"] = strtotime("+2 months",$val["contract_endtime"]);
 
-                $val["order_time"] = strtotime("+1 months",$val["order_time"]);
-                $val["pay_time"] = strtotime("+1 months",$val["pay_time"]);
-                if($val["app_time"]>0){
-                    $val["app_time"] = strtotime("+1 months",$val["app_time"]);
-                }
-                $val["check_money_time"] = strtotime("+1 months",$val["check_money_time"]);
-                $val["contract_starttime"] = strtotime("+1 months",$val["contract_starttime"]);
-                $val["contract_endtime"] = strtotime("+1 months",$val["contract_endtime"]);
+                // $val["order_time"] = strtotime("+1 months",$val["order_time"]);
+                // $val["pay_time"] = strtotime("+1 months",$val["pay_time"]);
+                // if($val["app_time"]>0){
+                //     $val["app_time"] = strtotime("+1 months",$val["app_time"]);
+                // }
+                // $val["check_money_time"] = strtotime("+1 months",$val["check_money_time"]);
+                // $val["contract_starttime"] = strtotime("+1 months",$val["contract_starttime"]);
+                // $val["contract_endtime"] = strtotime("+1 months",$val["contract_endtime"]);
 
                 
-                $val["parent_order_id"] = 3000;
-                unset($val["orderid"]);
-                $this->t_order_info_finance->row_insert($val);
+                // $val["parent_order_id"] = 3000;
+                // unset($val["orderid"]);
+                // $this->t_order_info_finance->row_insert($val);
 
-                if(count($arr) >= 120){
+                if(count($arr) >= 113){
                     break;
                 }
  
