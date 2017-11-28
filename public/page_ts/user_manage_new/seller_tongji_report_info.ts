7007f202be5d30aed6aae75eae7823eb3b63157b
tom <tom@leoedu.com>
2017-11-28 17:27:07 +0800

    f

--- public/page_ts/user_manage_new/seller_tongji_report_info.ts
+++ public/page_ts/user_manage_new/seller_tongji_report_info.ts
@@ -33,7 +33,6 @@ $(function(){
             if (do_index < row_list.length ) {
                 var $tr      = $(row_list[do_index]);
                 var opt_data = $tr.find(".opt-show").get_opt_data();
-                var order_per = 0;
                 $.do_ajax("/seller_student_new2/seller_test_lesson_info",{
                     "adminid"    : opt_data.adminid,
                     "start_time" : g_args.start_time,
@@ -51,8 +50,8 @@ $(function(){
                     // if(data["succ_all_count_for_month"] > 0){
                     //     order_per = Math.round(opt_data.all_new_contract_for_month/data["succ_all_count_for_month"])*100;
                     // }
-                    $tr.find(".order_per").text(Math.round(opt_data.all_new_contract_for_month/data["succ_all_count_for_month"])*100+'%');
-                    // $tr.find(".order_per").text(order_per+'%');
+                    var order_per = Math.round(opt_data.all_new_contract_for_month/data["succ_all_count_for_month"]);
+                    $tr.find(".order_per").text(order_per);
                     do_index++;
                     do_one();
                 });
