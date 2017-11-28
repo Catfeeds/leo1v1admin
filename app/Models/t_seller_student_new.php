fa6c7c0318c1718943fe00f55a7b481c754bafd9
jack <jack@leodeu.com>
2017-11-28 14:17:35 +0800

    fix

--- app/Models/t_seller_student_new.php
+++ app/Models/t_seller_student_new.php
@@ -3138,7 +3138,7 @@ class t_seller_student_new extends \App\Models\Zgen\z_t_seller_student_new
         $where_arr=[
             "s.is_test_user=0",
             "s.origin_assistantid>0",
-            // "s.origin_userid>0"
+            "s.origin_userid>0"
         ];
         $this->where_arr_add_time_range($where_arr,'n.add_time',$start_time,$end_time);
         $sql = $this->gen_sql_new("select count(distinct n.userid) stu_num,sum(if(l.lessonid>0,1,0)) lesson_num,"
