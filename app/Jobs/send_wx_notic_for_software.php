68ffb7eb252717aeb9ed29c8fa88ebbb1b72ee45
james <james@leoedu.com>
2017-11-28 14:50:13 +0800

    s

--- app/Jobs/send_wx_notic_for_software.php
+++ app/Jobs/send_wx_notic_for_software.php
@@ -34,21 +34,21 @@ class send_wx_notic_for_software extends Job implements ShouldQueue
         $t_parent_send_mgs_log = new  \App\Models\t_parent_send_mgs_log();
         // $parent_list = $t_parent_info->get_openid_list();
 
-        $wx = new \App\Helper\Wx();
+        // $wx = new \App\Helper\Wx();
 
         $parent_list = [
             [
                 'wx_openid' => 'orwGAs_IqKFcTuZcU1xwuEtV3Kek',
                 'parentid' => '271968'
             ],
-            [
-                'wx_openid' => 'orwGAs6J8tzBAO3mSKez8SX-DWq4',
-                'parentid' => '271968'
-            ]
 
         ];
 
-        $parent_template_id  = '9MXYC2KhG9bsIVl16cJgXFVsI35hIqffpSlSJFYckRU';
+        // $parent_template_id  = '9MXYC2KhG9bsIVl16cJgXFVsI35hIqffpSlSJFYckRU';
+
+        if(count($parent_list) >2){
+            return '';
+        }
 
         foreach($parent_list as $item){
 
@@ -58,18 +58,21 @@ class send_wx_notic_for_software extends Job implements ShouldQueue
                 'keyword1' => "功能上线通知",
                 'keyword2' => "点击个人中心，成绩记录功能，录入孩子成绩让班主任准确掌握孩子情况，制定实际有效解决孩子学习问题的课程规划",
                 'keyword3' => date('Y-m-d H:i:s'),
-                'remark'   => "点击推文前往下载页面"
+                'remark'   => ""
             ];
-            $url_leo = "http://a.app.qq.com/o/simple.jsp?pkgname=com.yibai.android.parent&fromcase=40002";
+            $url_leo = '';
+            // $wx->send_template_msg($item['wx_openid'], $parent_template_id, $data_leo, $url_leo);
 
-            // $wx->send_template_msg('orwGAs6J8tzBAO3mSKez8SX-DWq4', $parent_template_id, $data_leo, $url_leo);//james
-            $wx->send_template_msg($item['wx_openid'], $parent_template_id, $data_leo, $url_leo);
+            // 测试 调用新的方法
+            $WxSend = new \App\Helper\WxSendMsg();
+            $WxSend::send_wx_notic_for_software($item['wx_openid'], $data_leo, $url_leo);
+            // 测试 结束
 
-            // $t_parent_send_mgs_log->row_insert([
-            //     "parentid" => $item['parentid'],
-            //     "create_time" => time(),
-            //     "is_send_flag" => 3
-            // ]);
+            $t_parent_send_mgs_log->row_insert([
+                "parentid" => $item['parentid'],
+                "create_time" => time(),
+                "is_send_flag" => 3
+            ]);
 
         }
 
