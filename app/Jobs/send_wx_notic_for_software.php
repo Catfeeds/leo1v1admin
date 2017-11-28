0020f24e77a50827433836d746530f6a816c2019
james <james@leoedu.com>
2017-11-28 17:15:25 +0800

    s

--- app/Jobs/send_wx_notic_for_software.php
+++ app/Jobs/send_wx_notic_for_software.php
@@ -39,6 +39,15 @@ class send_wx_notic_for_software extends Job implements ShouldQueue
                 'wx_openid' => 'orwGAs_IqKFcTuZcU1xwuEtV3Kek',
                 'parentid'  => '271968'
             ],
+            [
+                'wx_openid' => '11',
+                'parentid'  => '271968'
+            ],
+            [
+                'wx_openid' => ' ',
+                'parentid'  => '271968'
+            ],
+
         ];
         /**
          * @ ios软件升级
@@ -46,7 +55,7 @@ class send_wx_notic_for_software extends Job implements ShouldQueue
          */
         foreach($parent_list as $item){
             $data_leo = [
-                'first'    => "家长，您好！ios端理优升学帮新版本V4.5.0正式上线了",
+                'first'    => "家长,您好！ios端理优升学帮新版本V4.5.0正式上线了",
                 'keyword1' => "功能上线通知",
                 'keyword2' => "该版本增加了建行分期付款功能，请及时更新哦~",
                 'keyword3' => date('Y-m-d H:i:s'),
@@ -55,11 +64,13 @@ class send_wx_notic_for_software extends Job implements ShouldQueue
             $url_leo = 'http://a.app.qq.com/o/simple.jsp?pkgname=com.yibai.android.parent&fromcase=40002';
 
             $WxSend = new \App\Helper\WxSendMsg();
-            $WxSend::send_wx_notic_for_software($item['wx_openid'], $data_leo, $url_leo);
+            if($item['wx_openid']){
+                $WxSend::send_wx_notic_for_software($item['wx_openid'], $data_leo, $url_leo);
+            }
 
             $t_parent_send_mgs_log->row_insert([
-                "parentid" => $item['parentid'],
-                "create_time" => time(),
+                "parentid"     => $item['parentid'],
+                "create_time"  => time(),
                 "is_send_flag" => 4 // ios升级微信通知
             ]);
         }
