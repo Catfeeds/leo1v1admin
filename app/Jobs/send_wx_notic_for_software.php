2878125acc1a6280cad76b4acdc63058a53b4ab3
james <james@leoedu.com>
2017-11-28 11:51:42 +0800

    s

--- app/Jobs/send_wx_notic_for_software.php
+++ app/Jobs/send_wx_notic_for_software.php
@@ -40,8 +40,12 @@ class send_wx_notic_for_software extends Job implements ShouldQueue
             [
                 'wx_openid' => 'orwGAs_IqKFcTuZcU1xwuEtV3Kek',
                 'parentid' => '271968'
-
+            ],
+            [
+                'wx_openid' => 'orwGAs6J8tzBAO3mSKez8SX-DWq4',
+                'parentid' => '271968'
             ]
+
         ];
 
         $parent_template_id  = '9MXYC2KhG9bsIVl16cJgXFVsI35hIqffpSlSJFYckRU';
@@ -54,12 +58,12 @@ class send_wx_notic_for_software extends Job implements ShouldQueue
                 'keyword1' => "功能上线通知",
                 'keyword2' => "点击个人中心，成绩记录功能，录入孩子成绩让班主任准确掌握孩子情况，制定实际有效解决孩子学习问题的课程规划",
                 'keyword3' => date('Y-m-d H:i:s'),
-                'remark'   => ""
+                'remark'   => "点击推文前往下载页面"
             ];
-            $url_leo = '';
+            $url_leo = 'http://a.app.qq.com/o/image/microQr.png?pkgName=com.yibai.android.parent';
 
-            $wx->send_template_msg('orwGAs6J8tzBAO3mSKez8SX-DWq4', $parent_template_id, $data_leo, $url_leo);//james
-            // $wx->send_template_msg($item['wx_openid'], $parent_template_id, $data_leo, $url_leo);
+            // $wx->send_template_msg('orwGAs6J8tzBAO3mSKez8SX-DWq4', $parent_template_id, $data_leo, $url_leo);//james
+            $wx->send_template_msg($item['wx_openid'], $parent_template_id, $data_leo, $url_leo);
 
             // $t_parent_send_mgs_log->row_insert([
             //     "parentid" => $item['parentid'],
