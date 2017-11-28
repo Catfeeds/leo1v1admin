792f0c9ac6c4ee6301252602a227923fcb6b9c4b
jack <jack@leodeu.com>
2017-11-28 15:16:19 +0800

    fix

--- app/Console/Commands/teacher_test_lesson_tra_day_wx.php
+++ app/Console/Commands/teacher_test_lesson_tra_day_wx.php
@@ -19,7 +19,7 @@ class teacher_test_lesson_tra_day_wx extends Command
      *
      * @var string
      */
-    protected $description = '各学科试听转化率推送Erick';
+    protected $description = '各学科试听转化率推送';
 
     /**
      * Create a new command instance.
@@ -62,6 +62,8 @@ class teacher_test_lesson_tra_day_wx extends Command
         $start_time = strtotime(date("Y-m-01",time()));
         $end_time = strtotime(date("Y-m-d",time()-$day_num*86400));
         //$ret_info = $task->t_lesson_info->tongji_teacher_test_lesson_info_list($start_time,$end_time);
+
+        //整体转化率
         $all = $zh= [];
         $test_person_num= $task->t_lesson_info->get_teacher_test_person_num_list_subject( $start_time,$end_time);
         foreach($test_person_num as $item){
@@ -118,26 +120,160 @@ class teacher_test_lesson_tra_day_wx extends Command
 
         //$success_test_lesson_list = $task->t_lesson_info->get_success_test_lesson_list_new_subject($start_time,$end_time);
 
-        $admin_arr2=[349,72,448];
+        // $admin_arr2=[349,72,448];
+        $admin_arr2=[349];
         foreach($admin_arr2 as $vv){
             $task->t_manager_info->send_wx_todo_msg_by_adminid ($vv,"理优监课组","每日各学科试听转化率","本月签单率:".$all["order_per"].",扩课签单率:".$all["kk_per"].",换老师签单率:".$all["change_per"].";其中语文签单率:".$yw["order_per"].",扩课签单率:".$yw["kk_per"].",换老师签单率:".$yw["change_per"].";数学签单率:".$sx["order_per"].",扩课签单率:".$sx["kk_per"].",换老师签单率:".$sx["change_per"].";英语签单率:".$yy["order_per"].",扩课签单率:".$yy["kk_per"].",换老师签单率:".$yy["change_per"].";物理签单率:".$wl["order_per"].",扩课签单率:".$wl["kk_per"].",换老师签单率:".$wl["change_per"].";小学科签单率:".$zh["order_per"].",扩课签单率:".$zh["kk_per"].",换老师签单率:".$zh["change_per"],"");   
         }
-        $list = $task->t_admin_main_group_name->get_group_list (4);
-        $task->t_manager_info->send_wx_todo_msg_by_adminid (793,"理优监课组","物理每日试听转化率","签单率:".$wl["order_per"].",扩课签单率:".$wl["kk_per"].",换老师签单率:".$wl["change_per"],"");
-        foreach($list as $item){
-            if($item["group_name"]=="文综组"){
-                $task->t_manager_info->send_wx_todo_msg_by_adminid ($item["master_adminid"],"理优监课组","语文每日试听转化率","签单率:".$yw["order_per"].",扩课签单率:".$yw["kk_per"].",换老师签单率:".$yw["change_per"],"");
-                $task->t_manager_info->send_wx_todo_msg_by_adminid ($item["master_adminid"],"理优监课组","英语每日试听转化率","签单率:".$yy["order_per"].",扩课签单率:".$yy["kk_per"].",换老师签单率:".$yy["change_per"],"");
+        // $list = $task->t_admin_main_group_name->get_group_list (4);
+        // $task->t_manager_info->send_wx_todo_msg_by_adminid (793,"理优监课组","物理每日试听转化率","签单率:".$wl["order_per"].",扩课签单率:".$wl["kk_per"].",换老师签单率:".$wl["change_per"],"");
+        // foreach($list as $item){
+        //     if($item["group_name"]=="文综组"){
+        //         $task->t_manager_info->send_wx_todo_msg_by_adminid ($item["master_adminid"],"理优监课组","语文每日试听转化率","签单率:".$yw["order_per"].",扩课签单率:".$yw["kk_per"].",换老师签单率:".$yw["change_per"],"");
+        //         $task->t_manager_info->send_wx_todo_msg_by_adminid ($item["master_adminid"],"理优监课组","英语每日试听转化率","签单率:".$yy["order_per"].",扩课签单率:".$yy["kk_per"].",换老师签单率:".$yy["change_per"],"");
                 
-            }elseif($item["group_name"]=="数学组"){
-                $task->t_manager_info->send_wx_todo_msg_by_adminid ($item["master_adminid"],"理优监课组","数学每日试听转化率","签单率:".$sx["order_per"].",扩课签单率:".$sx["kk_per"].",换老师签单率:".$sx["change_per"],"");
-                $task->t_manager_info->send_wx_todo_msg_by_adminid (754,"理优监课组","数学每日试听转化率","签单率:".$sx["order_per"].",扩课签单率:".$sx["kk_per"].",换老师签单率:".$sx["change_per"],"");
+        //     }elseif($item["group_name"]=="数学组"){
+        //         $task->t_manager_info->send_wx_todo_msg_by_adminid ($item["master_adminid"],"理优监课组","数学每日试听转化率","签单率:".$sx["order_per"].",扩课签单率:".$sx["kk_per"].",换老师签单率:".$sx["change_per"],"");
+        //         $task->t_manager_info->send_wx_todo_msg_by_adminid (754,"理优监课组","数学每日试听转化率","签单率:".$sx["order_per"].",扩课签单率:".$sx["kk_per"].",换老师签单率:".$sx["change_per"],"");
 
-            }elseif($item["group_name"]=="小学科"){
-                $task->t_manager_info->send_wx_todo_msg_by_adminid ($item["master_adminid"],"理优监课组","小学科每日试听转化率","签单率:".$zh["order_per"].",扩课签单率:".$zh["kk_per"].",换老师签单率:".$zh["change_per"],""); 
+        //     }elseif($item["group_name"]=="小学科"){
+        //         $task->t_manager_info->send_wx_todo_msg_by_adminid ($item["master_adminid"],"理优监课组","小学科每日试听转化率","签单率:".$zh["order_per"].",扩课签单率:".$zh["kk_per"].",换老师签单率:".$zh["change_per"],""); 
+        //     }
+
+        // }
+
+
+
+        //整体转化率(兼职老师)
+        $all = $zh= [];
+        $test_person_num= $task->t_lesson_info->get_teacher_test_person_num_list_subject( $start_time,$end_time,0);
+        foreach($test_person_num as $item){
+            @$all["person_num"] +=$item["person_num"];
+            @$all["have_order"] +=$item["have_order"];
+            if($item["subject"]==4 && $item["subject"] >5){
+                @$zh["person_num"] +=$item["person_num"];
+                @$zh["have_order"] +=$item["have_order"];
             }
-
         }
+        $all["order_per"] = (!empty($all["person_num"])?round($all["have_order"]/$all["person_num"],3)*100:0)."%";
+        $zh["order_per"] = (!empty($zh["person_num"])?round($zh["have_order"]/$zh["person_num"],3)*100:0)."%";
+        $yw["order_per"] = (!empty($test_person_num[1]["person_num"])?round($test_person_num[1]["have_order"]/$test_person_num[1]["person_num"],3)*100:0)."%";
+        $sx["order_per"] = (!empty($test_person_num[2]["person_num"])?round($test_person_num[2]["have_order"]/$test_person_num[2]["person_num"],3)*100:0)."%";
+        $yy["order_per"] = (!empty($test_person_num[3]["person_num"])?round($test_person_num[3]["have_order"]/$test_person_num[3]["person_num"],3)*100:0)."%";
+        $wl["order_per"] = (!empty($test_person_num[5]["person_num"])?round($test_person_num[5]["have_order"]/$test_person_num[5]["person_num"],3)*100:0)."%";
+
+        $kk_test_person_num= $task->t_lesson_info->get_kk_teacher_test_person_num_list_subject( $start_time,$end_time,0);
+        //dd($kk_test_person_num);
+        foreach($kk_test_person_num as $item){
+            @$all["kk_num"] +=$item["kk_num"];
+            @$all["kk_order"] +=$item["kk_order"];
+            if($item["subject"]==4 && $item["subject"] >5){
+                @$zh["kk_num"] +=$item["kk_num"];
+                @$zh["kk_order"] +=$item["kk_order"];
+            }
+        }
+        $all["kk_per"] = (!empty($all["kk_num"])?round($all["kk_order"]/$all["kk_num"],3)*100:0)."%";
+        $zh["kk_per"] = (!empty($zh["kk_num"])?round($zh["kk_order"]/$zh["kk_num"],3)*100:0)."%";
+        $yw["kk_per"] = (!empty($kk_test_person_num[1]["kk_num"])?round($kk_test_person_num[1]["kk_order"]/$kk_test_person_num[1]["kk_num"],3)*100:0)."%";
+        $sx["kk_per"] = (!empty($kk_test_person_num[2]["kk_num"])?round($kk_test_person_num[2]["kk_order"]/$kk_test_person_num[2]["kk_num"],3)*100:0)."%";
+        $yy["kk_per"] = (!empty($kk_test_person_num[3]["kk_num"])?round($kk_test_person_num[3]["kk_order"]/$kk_test_person_num[3]["kk_num"],3)*100:0)."%";
+        $wl["kk_per"] = (!empty($kk_test_person_num[5]["kk_num"])?round($kk_test_person_num[5]["kk_order"]/$kk_test_person_num[5]["kk_num"],3)*100:0)."%";
+
+
+        $change_test_person_num= $task->t_lesson_info->get_change_teacher_test_person_num_list_subject( $start_time,$end_time,0);
+        //dd($change_test_person_num);
+        foreach($change_test_person_num as $item){
+            @$all["change_num"] +=$item["change_num"];
+            @$all["change_order"] +=$item["change_order"];
+            if($item["subject"]==4 && $item["subject"] >5){
+                @$zh["change_num"] +=$item["change_num"];
+                @$zh["change_order"] +=$item["change_order"];
+            }
+        }
+        $all["change_per"] = (!empty($all["change_num"])?round($all["change_order"]/$all["change_num"],3)*100:0)."%";
+        $zh["change_per"] = (!empty($zh["change_num"])?round($zh["change_order"]/$zh["change_num"],3)*100:0)."%";
+        $yw["change_per"] = (!empty($change_test_person_num[1]["change_num"])?round($change_test_person_num[1]["change_order"]/$change_test_person_num[1]["change_num"],3)*100:0)."%";
+        $sx["change_per"] = (!empty($change_test_person_num[2]["change_num"])?round($change_test_person_num[2]["change_order"]/$change_test_person_num[2]["change_num"],3)*100:0)."%";
+        $yy["change_per"] = (!empty($change_test_person_num[3]["change_num"])?round($change_test_person_num[3]["change_order"]/$change_test_person_num[3]["change_num"],3)*100:0)."%";
+        $wl["change_per"] = (!empty($change_test_person_num[5]["change_num"])?round($change_test_person_num[5]["change_order"]/$change_test_person_num[5]["change_num"],3)*100:0)."%";
+
+
+
+        //$success_test_lesson_list = $task->t_lesson_info->get_success_test_lesson_list_new_subject($start_time,$end_time);
+
+        //$admin_arr2=[349,72,448];
+        $admin_arr2=[349];
+        foreach($admin_arr2 as $vv){
+            $task->t_manager_info->send_wx_todo_msg_by_adminid ($vv,"理优监课组","每日兼职试听转化率","本月签单率:".$all["order_per"].",扩课签单率:".$all["kk_per"].",换老师签单率:".$all["change_per"].";其中语文签单率:".$yw["order_per"].",扩课签单率:".$yw["kk_per"].",换老师签单率:".$yw["change_per"].";数学签单率:".$sx["order_per"].",扩课签单率:".$sx["kk_per"].",换老师签单率:".$sx["change_per"].";英语签单率:".$yy["order_per"].",扩课签单率:".$yy["kk_per"].",换老师签单率:".$yy["change_per"].";物理签单率:".$wl["order_per"].",扩课签单率:".$wl["kk_per"].",换老师签单率:".$wl["change_per"].";小学科签单率:".$zh["order_per"].",扩课签单率:".$zh["kk_per"].",换老师签单率:".$zh["change_per"],"");   
+        }
+
+
+
+        //整体转化率(全职老师)
+        $all = $zh= [];
+        $test_person_num= $task->t_lesson_info->get_teacher_test_person_num_list_subject( $start_time,$end_time,1);
+        foreach($test_person_num as $item){
+            @$all["person_num"] +=$item["person_num"];
+            @$all["have_order"] +=$item["have_order"];
+            if($item["subject"]==4 && $item["subject"] >5){
+                @$zh["person_num"] +=$item["person_num"];
+                @$zh["have_order"] +=$item["have_order"];
+            }
+        }
+        $all["order_per"] = (!empty($all["person_num"])?round($all["have_order"]/$all["person_num"],3)*100:0)."%";
+        $zh["order_per"] = (!empty($zh["person_num"])?round($zh["have_order"]/$zh["person_num"],3)*100:0)."%";
+        $yw["order_per"] = (!empty($test_person_num[1]["person_num"])?round($test_person_num[1]["have_order"]/$test_person_num[1]["person_num"],3)*100:0)."%";
+        $sx["order_per"] = (!empty($test_person_num[2]["person_num"])?round($test_person_num[2]["have_order"]/$test_person_num[2]["person_num"],3)*100:0)."%";
+        $yy["order_per"] = (!empty($test_person_num[3]["person_num"])?round($test_person_num[3]["have_order"]/$test_person_num[3]["person_num"],3)*100:0)."%";
+        $wl["order_per"] = (!empty($test_person_num[5]["person_num"])?round($test_person_num[5]["have_order"]/$test_person_num[5]["person_num"],3)*100:0)."%";
+
+        $kk_test_person_num= $task->t_lesson_info->get_kk_teacher_test_person_num_list_subject( $start_time,$end_time,1);
+        //dd($kk_test_person_num);
+        foreach($kk_test_person_num as $item){
+            @$all["kk_num"] +=$item["kk_num"];
+            @$all["kk_order"] +=$item["kk_order"];
+            if($item["subject"]==4 && $item["subject"] >5){
+                @$zh["kk_num"] +=$item["kk_num"];
+                @$zh["kk_order"] +=$item["kk_order"];
+            }
+        }
+        $all["kk_per"] = (!empty($all["kk_num"])?round($all["kk_order"]/$all["kk_num"],3)*100:0)."%";
+        $zh["kk_per"] = (!empty($zh["kk_num"])?round($zh["kk_order"]/$zh["kk_num"],3)*100:0)."%";
+        $yw["kk_per"] = (!empty($kk_test_person_num[1]["kk_num"])?round($kk_test_person_num[1]["kk_order"]/$kk_test_person_num[1]["kk_num"],3)*100:0)."%";
+        $sx["kk_per"] = (!empty($kk_test_person_num[2]["kk_num"])?round($kk_test_person_num[2]["kk_order"]/$kk_test_person_num[2]["kk_num"],3)*100:0)."%";
+        $yy["kk_per"] = (!empty($kk_test_person_num[3]["kk_num"])?round($kk_test_person_num[3]["kk_order"]/$kk_test_person_num[3]["kk_num"],3)*100:0)."%";
+        $wl["kk_per"] = (!empty($kk_test_person_num[5]["kk_num"])?round($kk_test_person_num[5]["kk_order"]/$kk_test_person_num[5]["kk_num"],3)*100:0)."%";
+
+
+        $change_test_person_num= $task->t_lesson_info->get_change_teacher_test_person_num_list_subject( $start_time,$end_time,1);
+        //dd($change_test_person_num);
+        foreach($change_test_person_num as $item){
+            @$all["change_num"] +=$item["change_num"];
+            @$all["change_order"] +=$item["change_order"];
+            if($item["subject"]==4 && $item["subject"] >5){
+                @$zh["change_num"] +=$item["change_num"];
+                @$zh["change_order"] +=$item["change_order"];
+            }
+        }
+        $all["change_per"] = (!empty($all["change_num"])?round($all["change_order"]/$all["change_num"],3)*100:0)."%";
+        $zh["change_per"] = (!empty($zh["change_num"])?round($zh["change_order"]/$zh["change_num"],3)*100:0)."%";
+        $yw["change_per"] = (!empty($change_test_person_num[1]["change_num"])?round($change_test_person_num[1]["change_order"]/$change_test_person_num[1]["change_num"],3)*100:0)."%";
+        $sx["change_per"] = (!empty($change_test_person_num[2]["change_num"])?round($change_test_person_num[2]["change_order"]/$change_test_person_num[2]["change_num"],3)*100:0)."%";
+        $yy["change_per"] = (!empty($change_test_person_num[3]["change_num"])?round($change_test_person_num[3]["change_order"]/$change_test_person_num[3]["change_num"],3)*100:0)."%";
+        $wl["change_per"] = (!empty($change_test_person_num[5]["change_num"])?round($change_test_person_num[5]["change_order"]/$change_test_person_num[5]["change_num"],3)*100:0)."%";
+
+
+
+        //$success_test_lesson_list = $task->t_lesson_info->get_success_test_lesson_list_new_subject($start_time,$end_time);
+
+        // $admin_arr2=[349,72,448];
+        $admin_arr2=[349];
+        foreach($admin_arr2 as $vv){
+            $task->t_manager_info->send_wx_todo_msg_by_adminid ($vv,"理优监课组","每日全职试听转化率","本月签单率:".$all["order_per"].",扩课签单率:".$all["kk_per"].",换老师签单率:".$all["change_per"].";其中语文签单率:".$yw["order_per"].",扩课签单率:".$yw["kk_per"].",换老师签单率:".$yw["change_per"].";数学签单率:".$sx["order_per"].",扩课签单率:".$sx["kk_per"].",换老师签单率:".$sx["change_per"].";英语签单率:".$yy["order_per"].",扩课签单率:".$yy["kk_per"].",换老师签单率:".$yy["change_per"].";物理签单率:".$wl["order_per"].",扩课签单率:".$wl["kk_per"].",换老师签单率:".$wl["change_per"].";小学科签单率:".$zh["order_per"].",扩课签单率:".$zh["kk_per"].",换老师签单率:".$zh["change_per"],"");   
+        }
+
+       
+
 
     
            
