2154d927c94f47c782e15b6b04cfab3a3eaef3dc
jack <jack@leodeu.com>
2017-11-29 19:25:31 +0800

    fix

--- enum_config/config_order_channel.php
+++ enum_config/config_order_channel.php
@@ -3,4 +3,6 @@ return array (
     array (1 ,  "wx" ,"微信支付", "" ),
     array (2 ,  "jdpay_wap" ,"京东支付",  ),
     array (3 ,  "alipay" ,"支付宝 APP 支付",  ),
+    array (4 ,  "alipay_pc_direct" ,"支付宝 网站 支付",  ),
+    array (5 ,  "wx_pub_qr" ,"微信扫码 支付",  ),
 );
