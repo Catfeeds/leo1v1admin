d0f3183371bfded50ebbeea16b2764cf3e66e3a8
jim <jim@leoedu.com>
2017-11-29 11:03:11 +0800

    f

--- table_config/_z_config.php
+++ table_config/_z_config.php
@@ -1,6 +1,7 @@
 <?php
 return array(
     ["db_weiyi","t_tag_library","tag_id"],
+    ['db_weiyi_admin','t_company_wx_tag','id'],
     ["db_weiyi","t_resource","resource_id"],
     ['db_weiyi','t_sum_activity_quota','id'],
     ['db_weiyi','t_activity_quota_detail','id'],
