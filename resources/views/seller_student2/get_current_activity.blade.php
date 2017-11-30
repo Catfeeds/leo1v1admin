0dde8e8104f7d70eb8d23a4d6228d0f825db3035
gupeigen <gupeigen@leoedu.com>
2017-11-30 18:16:26 +0800

    bacon

--- resources/views/seller_student2/get_current_activity.blade.php
+++ resources/views/seller_student2/get_current_activity.blade.php
@@ -27,7 +27,7 @@
                 <td >活动ID</td>
                 <td >活动标题</td>
                 <td >优惠力度</td>
-                <td style="display:none">活动时间</td>
+                <td >活动时间</td>
                 <td >开启类型</td>
                 <td >操作</td>
             </tr>
@@ -44,6 +44,7 @@
                         <div {!!  \App\Helper\Utils::gen_jquery_data($var )  !!}>
                             <a class="fa-hand-o-up opt-stu-origin btn fa" title="编辑优惠力度"></a>
                             <a href="javascript:;" class="fa-edit btn act-edit" title="编辑活动"></a>
+                            <a href="javascript:;" class="fa-comment opt-return-back btn fa act-look" title="查看活动"></a>
                         </div>
                     </td>
                 </tr>
