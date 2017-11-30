cc842547938285d956b6aab15fc68cba9d310cd4
sam <sam@leoedu.com>
2017-11-30 15:09:24 +0800

    fix

--- app/Http/Controllers/aliyun_oss.php
+++ app/Http/Controllers/aliyun_oss.php
@@ -14,12 +14,7 @@ class aliyun_oss  extends Controller
 {
     use CacheNick;
     use TeaPower;
-
-    var $oss;
     public function __construct(){
-        include_once(app_path("Libs/aliyun/OSS.php"));
-        $this->oss = new \OSS();
-        // dd($oss);
     }
     public function upload_list(){
         list($start_time,$end_time) = $this->get_in_date_range( 0,0,0,[],3);
@@ -32,62 +27,6 @@ class aliyun_oss  extends Controller
         return $this->pageView(__METHOD__,$ret_info);
     }
 
-    public function upload_page(){
-        return $this->pageView(__METHOD__,null,null);
-    }
-    public function upload_add(Request $request){
-        $file = $_FILES['file'];
-        echo "<pre>";
-        var_dump($file);
-        echo "</per>";
-        if($file['error'] != 0){
-            dd("文件上传有误,错误代码".$file['error']);
-        }
-        $file_name = $_POST['file_name'];
-        $file_type = $_POST['file_type'];
-        $file_origin = $file['name'];
-
-        $info =  pathinfo($file_origin);
-        $file_extension = $info['extension'];
-        if($file_type == 1){
-            $new_name = "student/".$file_name.".".$file_extension;
-        }else{
-            $new_name = "teacher/".$file_name.".".$file_extension;
-        }
-        $tmp_url = $file['tmp_name'];
-        $tmp_type = $file['type'];
-        /*
-        $ret_info = $this->oss::publicUpload('sam-test', $new_name, $tmp_url, [
-            'ContentType' => $tmp_type,
-        ]);
-        */
-        $ret_info = $this->oss::multi_publicUpload('sam-test', $new_name, $tmp_url, [
-            'ContentType' => $tmp_type,
-        ]);
-        
-        if($ret_info){
-            echo "<pre>";
-            var_dump($ret_info);
-            echo "</pre>";
-            $data = [
-                "publish_time" => time(),
-                "file_path"  => $new_name,
-                "file_type"    => $file_type,
-                "file_url"     => $file_type,
-            ];
-            $this->t_version_control->row_insert($data);
-            print("<a href=upload_list>点击跳转到列表页</a>");
-        }else{
-            dd("上传出错");
-        }
-    }
-    public function file_manage(){
-        $ret_info = $this->oss::publicUpload('sam-test', 'student/test_sam01.jpg', '/home/sam/coder01.jpg', [
-            'ContentType' => 'application/jpg',
-        ]);
-        
-        dd($ret_info);
-    }
     public function add_file(){
         $file_type = $this->get_in_int_val("file_type",-1);
         $file_name = $this->get_in_str_val("file_name","");
