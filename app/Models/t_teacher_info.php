751403f0f3e89a73f7492ed5023ff3686b753bfb
jack <jack@leodeu.com>
2017-12-04 11:18:40 +0800

    fix

--- app/Models/t_teacher_info.php
+++ app/Models/t_teacher_info.php
@@ -4235,7 +4235,7 @@ class t_teacher_info extends \App\Models\Zgen\z_t_teacher_info
         $where_arr=[
             "is_test_user=1",
             // "train_through_new=1",
-            "teacher_tags <> ''"
+            "(teacher_tags <> '' and teacher_tags <> 0 and teacher_tags <> ',')"
         ];
         $sql = $this->gen_sql_new("select teacherid,teacher_tags from %s where %s ",self::DB_TABLE_NAME,$where_arr);
         return $this->main_get_list($sql);
