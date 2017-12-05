866a80256a38c5b89594237479e9288a280dc961
ricky <ricky@leoedu.com>
2017-12-05 18:19:06 +0800

    f

--- app/Console/Commands/add_new_tea_entry.php
+++ app/Console/Commands/add_new_tea_entry.php
@@ -64,6 +64,7 @@ class add_new_tea_entry extends Command
             }
         }
         foreach($users as $key => $item) {
+            if (isset($user[$key])) continue;
             echo $user[$key]['name'].' '.$item['tag'].' '.$item['order'].',';
         }
         dd($users);
