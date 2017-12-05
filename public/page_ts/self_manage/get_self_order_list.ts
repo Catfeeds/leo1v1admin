7f5f3edcd7a3ec1edc0ccfcf6e428ba2ea11989e
jack <jack@leodeu.com>
2017-12-05 14:11:32 +0800

    fix

--- public/page_ts/self_manage/get_self_order_list.ts
+++ public/page_ts/self_manage/get_self_order_list.ts
@@ -9,19 +9,19 @@ function load_data(){
 }
 $(function(){
 
-    $.each($(".opt-hand-over_info"),function(){
-        var hand_over_view = $(this).get_opt_data("hand_over_view");
-        if(hand_over_view == 0){
-            $(this).hide();
-        }
-    });
+    // $.each($(".opt-hand-over_info"),function(){
+    //     var hand_over_view = $(this).get_opt_data("hand_over_view");
+    //     if(hand_over_view == 0){
+    //         $(this).hide();
+    //     }
+    // });
 
-    $.each($(".opt-hand-over"),function(){
-        var hand_over_flag = $(this).get_opt_data("contract_type");
-        if(hand_over_flag != 0){
-            $(this).hide();
-        }
-    });
+    // $.each($(".opt-hand-over"),function(){
+    //     var hand_over_flag = $(this).get_opt_data("contract_type");
+    //     if(hand_over_flag != 0){
+    //         $(this).hide();
+    //     }
+    // });
 
 
     $('.opt-hand-over').on("click",function(){
@@ -31,11 +31,11 @@ $(function(){
     });
 
 
-    $('.opt-hand-over_info').on("click",function(){
-        var orderid = $(this).get_opt_data('orderid');
-        var sid = $(this).get_opt_data('userid');
-        window.location.href="/stu_manage/init_info_by_contract_cr?orderid="+orderid+"&sid="+sid;
-    });
+    // $('.opt-hand-over_info').on("click",function(){
+    //     var orderid = $(this).get_opt_data('orderid');
+    //     var sid = $(this).get_opt_data('userid');
+    //     window.location.href="/stu_manage/init_info_by_contract_cr?orderid="+orderid+"&sid="+sid;
+    // });
 
 
 
--- public/page_ts/stu_manage/order_info_list.ts
+++ public/page_ts/stu_manage/order_info_list.ts
@@ -10,19 +10,19 @@ $(function(){
     $('#id_competition_flag').val(g_args.competition_flag);
     $('.opt-change').set_input_change_event(load_data);
 
-    // $.each($(".opt-hand-over_info"),function(){
-    //     var hand_over_view = $(this).get_opt_data("hand_over_view");
-    //     if(hand_over_view == 0){
-    //         $(this).hide();
-    //     }
-    // });
+    $.each($(".opt-hand-over_info"),function(){
+        var hand_over_view = $(this).get_opt_data("hand_over_view");
+        if(hand_over_view == 0){
+            $(this).hide();
+        }
+    });
 
-    // $.each($(".opt-hand-over"),function(){
-    //     var hand_over_flag = $(this).get_opt_data("contract_type");
-    //     if(hand_over_flag != 0){
-    //         $(this).hide();
-    //     }
-    // });
+    $.each($(".opt-hand-over"),function(){
+        var hand_over_flag = $(this).get_opt_data("contract_type");
+        if(hand_over_flag != 0){
+            $(this).hide();
+        }
+    });
 
 
 
@@ -35,11 +35,11 @@ $(function(){
     });
 
 
-    // $('.opt-hand-over_info').on("click",function(){
-    //     var orderid = $(this).get_opt_data('orderid');
-    //     var sid     = g_sid;
-    //     window.location.href="/stu_manage/init_info_by_contract_cr?orderid="+orderid+"&sid="+sid;
-    // });
+    $('.opt-hand-over_info').on("click",function(){
+        var orderid = $(this).get_opt_data('orderid');
+        var sid     = g_sid;
+        window.location.href="/stu_manage/init_info_by_contract_cr?orderid="+orderid+"&sid="+sid;
+    });
 
 
 });
