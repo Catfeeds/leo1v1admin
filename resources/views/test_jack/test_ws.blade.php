32e6cbde6faa354f51e27a4e65ecbd2ad7dea96c
jack <jack@leodeu.com>
2017-12-07 12:02:23 +0800

    fix

--- resources/views/test_jack/test_ws.blade.php
+++ resources/views/test_jack/test_ws.blade.php
@@ -1,8 +1,10 @@
 @extends('layouts.app')
 @section('content')
+        <script src="/js/jquery-2.1.4.js" type="text/javascript"></script>
 
         <script type="text/javascript" src="/js/jquery.md5.js"></script>
         <script type="text/javascript" src="/js/jquery.query.js"></script>
+        <script type="text/javascript" src="/js/jquery.admin.js"></script>
         <script type="text/javascript" src="/js/jquery.websocket.js"></script>
 
 
