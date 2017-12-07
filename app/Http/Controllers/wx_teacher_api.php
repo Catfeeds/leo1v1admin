edc40326e214c016d2d95da59b22de239ce3efde
james <james@leoedu.com>
2017-12-07 10:48:03 +0800

    s

--- app/Http/Controllers/wx_teacher_api.php
+++ app/Http/Controllers/wx_teacher_api.php
@@ -1025,7 +1025,7 @@ class wx_teacher_api extends Controller
         $lessonid  = $this->get_in_int_val('lessonid',-1);
         $ret_info  = $this->t_test_lesson_subject->get_test_require_info($lessonid);
 
-        if($lesson_info['lesson_del_flag']==1){
+        if($ret_info['lesson_del_flag']==1){
             $lesson_info['status'] = 2;
         }
 
