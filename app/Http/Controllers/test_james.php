56bc725674752a30771c4bcbdca5d342e3b9b78d
james <james@leoedu.com>
2017-12-07 19:35:07 +0800

    s

--- app/Http/Controllers/test_james.php
+++ app/Http/Controllers/test_james.php
@@ -1316,16 +1316,21 @@ class test_james extends Controller
         fclose($fp);
         $content = base64_encode($content); // 将二进制信息编码成字符串
 
+        $content = str_replace("\n"," ",$content);
 
         $post_data = [
             "format"=>"wav",
             "rate"=>16000,
             "channel"=>1,
             "token"=>"24.17dde2e70ba5d12d9217220c62a95853.2592000.1515234816.282335-10485794",
-            "cuid"=>"98:e7:f4:bd:7d:4c",
-            "len"=>127,
+            "cuid"=>"baidu_workshop122xuejijams",
+            // "len"=>127,
             "lan" => "zh",
-            "speech"=>"$content", // xxx为 base64（FILE_CONTENT）
+            // "speech"=>"$content",
+
+            "url" => "http://speech-doc.gz.bcebos.com/rest-api-asr/public_audio/16k.wav",
+            "callback" => "http://admin.leo1v1.com/test_james/get_post"
+            
         ];
 
         $post_data = json_encode($post_data);
@@ -1347,6 +1352,11 @@ class test_james extends Controller
         $ret_arr = json_decode($output,true);
 
         dd($ret_arr);
+    }
+
+    public function get_post(){
+        $a = $_POST;
+        dd($a);
     }
 
 
