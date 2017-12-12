a5a7126f75ec5ec24377e9a83f234b0cec0ede48
jack <jack@leodeu.com>
2017-12-12 15:24:51 +0800

    fix

--- app/Models/t_teacher_lecture_appointment_info.php
+++ app/Models/t_teacher_lecture_appointment_info.php
@@ -856,7 +856,11 @@ class t_teacher_lecture_appointment_info extends \App\Models\Zgen\z_t_teacher_le
     }
 
     public function get_id_list_by_adminid($accept_adminid,$lecture_revisit_type=-1){
-        $sql = $this->gen_sql_new("select id,answer_begin_time,accept_adminid from %s where accept_adminid=%u",
+        $where_arr=[];
+        if($lecture_revisit_type==1){
+            $where_arr[]="lecture_revisit_type not in (4,8)"; 
+        }
+        $sql = $this->gen_sql_new("select id,answer_begin_time,accept_adminid,lecture_revisit_type from %s where accept_adminid=%u",
                                   self::DB_TABLE_NAME,
                                   $accept_adminid
         );
