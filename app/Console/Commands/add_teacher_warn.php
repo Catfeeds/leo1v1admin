b6d4dfbb8b81cef5767dece6200f89eda02179e7
ricky <ricky>
2017-12-13 18:51:12 +0800

    f

--- app/Console/Commands/add_teacher_warn.php
+++ app/Console/Commands/add_teacher_warn.php
@@ -45,7 +45,7 @@ class add_teacher_warn extends Command
         // 删除测试用户
         $data = $task->t_teacher_warn->get_info_for_test_user();
         foreach($data as $val) {
-            $task->t_teacher_warn->row_delete($val['teacheri']);
+            $task->t_teacher_warn->row_delete($val['teacherid']);
         }
         exit;
 
