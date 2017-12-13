046b7e465de2aacac632fe6ffd12ae50f33ab41c
gupeigen <gupeigen@leoedu.com>
2017-12-13 15:40:39 +0800

    bacon

--- database/migrations/2017_12_13_1645_t_change_knowledge.php
+++ database/migrations/2017_12_13_1645_t_change_knowledge.php
@@ -13,10 +13,11 @@ class TChangeKnowledge extends Migration
     {
         
         Schema::table('db_question_new.t_knowledge_point', function (Blueprint $table){
-            t_field($table->integer('level')->default(0),"知识点层级");
-            t_field($table->integer('father_id')->default(0),"父级id");
-            t_field($table->string('father_other')->default(null),"可能对应的其他父级id");
-            t_field($table->integer('grade'),"知识点对应的年级id");
+            $table->dropColumn('grade');
+            // t_field($table->integer('level')->default(0),"知识点层级");
+            // t_field($table->integer('father_id')->default(0),"父级id");
+            t_field($table->integer('open_flag')->default(1),"1:开启 0:关闭");
+            // t_field($table->string('father_other')->default(null),"可能对应的其他父级id");
         });
 	
 
