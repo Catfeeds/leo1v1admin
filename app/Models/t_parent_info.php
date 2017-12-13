981840a8cc5b8be1ba2df90445a45a9ffc63257c
james <james@leoedu.com>
2017-12-13 10:55:05 +0800

    s

--- app/Models/t_parent_info.php
+++ app/Models/t_parent_info.php
@@ -263,7 +263,8 @@ class t_parent_info extends \App\Models\Zgen\z_t_parent_info
 
     public function get_parent_opend_list(){
         $where_arr = [
-            " p.wx_openid is not null"
+            " p.wx_openid is not null",
+            " p.wx_openid != ''"
         ];
         $sql = $this->gen_sql_new("  select wx_openid from %s p"
                                   ." where %s"
