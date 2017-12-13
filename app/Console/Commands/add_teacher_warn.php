849276f1274dee27648f565147a4de7171a0d559
ricky <ricky>
2017-12-13 18:44:33 +0800

    f

--- app/Console/Commands/add_teacher_warn.php
+++ app/Console/Commands/add_teacher_warn.php
@@ -42,6 +42,14 @@ class add_teacher_warn extends Command
         $date = strtotime('-1day');
         $start_time = strtotime(date('Y-m-d 00:00:00', $date));
         $end_time = strtotime(date('Y-m-d 23:59:59', $date));
+        // 删除测试用户
+        $data = $task->t_teacher_warn->get_info_for_test_user();
+        foreach($data as $val) {
+            echo $val;
+            $task->t_teacher_warn->row_delete($val);
+        }
+        exit;
+
 
         // 第一次加载所有数据
         $start_time = strtotime('2017-1-1');
