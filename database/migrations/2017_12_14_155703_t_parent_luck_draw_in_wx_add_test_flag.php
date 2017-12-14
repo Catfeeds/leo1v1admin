e37df486a4ed5c5f75155cbc07293cc4c1f214bb
james <james@leoedu.com>
2017-12-14 16:17:35 +0800

    s

--- database/migrations/2017_12_14_155703_t_parent_luck_draw_in_wx_add_test_flag.php
+++ database/migrations/2017_12_14_155703_t_parent_luck_draw_in_wx_add_test_flag.php
@@ -14,7 +14,7 @@ class TParentLuckDrawInWxAddTestFlag extends Migration
     {
         //
         Schema::table("db_weiyi.t_parent_luck_draw_in_wx", function(Blueprint $table) {
-            t_field($table->tinInteger("is_test_flag"), "测试账号标示 0:正常用户 1:测试用户");
+            t_field($table->tinyInteger("is_test_flag"), "测试账号标示 0:正常用户 1:测试用户");
         });
 
     }
