7f944e7c045752400b2a9e731d1bb77aef2befc0
ricky <ricky>
2017-12-14 11:10:12 +0800

    f

--- app/Models/t_teacher_warn.php
+++ app/Models/t_teacher_warn.php
@@ -24,11 +24,11 @@ class t_teacher_warn extends \App\Models\Zgen\z_t_teacher_warn
 
     public function get_all_info($start_time, $end_time, $teacherid, $type = 0) {
         $where_arr = [
-            //['t.lesson_start>=%u', $start_time, 0],
+            ['t.lesson_start>=%u', $start_time, 0],
             ['t.lesson_start<%u', $end_time, 0]
         ];
         if ($teacherid) {
-            array_push($where_arr, ["t.teacherid=%u",$teacherid,0]);
+            // array_push($where_arr, ["t.teacherid=%u",$teacherid,0]);
             // $sql = $this->gen_sql_new("select id,teacherid,sum(five_num) five_num,sum(fift_num) fift_num,sum(leave_num) leave_num,sum(absent_num) absent_num,sum(adjust_num) adjust_num,sum(ask_leave_num) ask_leave_num,sum(big_order_num) big_order_num from %s t where %s group by teacherid",
             //                           self::DB_TABLE_NAME,
             //                           $where_arr
