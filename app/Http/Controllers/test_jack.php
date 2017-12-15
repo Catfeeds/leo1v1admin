fe27b8819ab651e80ac23c8eb0938ce018469fa7
jack <jack@leodeu.com>
2017-12-15 15:08:12 +0800

    fix

--- app/Http/Controllers/test_jack.php
+++ app/Http/Controllers/test_jack.php
@@ -697,6 +697,13 @@ class test_jack  extends Controller
     }
 
     public function test_wx(){
+        $this->t_flow_node->row_insert([
+            "node_type"=>1,
+            "flowid"   =>4713,
+            "adminid"  =>1004,
+            "add_time" =>time()
+        ]);
+        dd(1111);
         $ret_info   = $this->t_flow_node->get_node_list(4713,"asc");
         dd($ret_info);
 
