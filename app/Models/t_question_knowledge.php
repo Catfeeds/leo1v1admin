5d05bcfd393e357a059fe2fe5b78453d57b7d584
gupeigen <gupeigen@leoedu.com>
2017-12-15 19:17:59 +0800

    bacon

--- app/Models/t_question_knowledge.php
+++ app/Models/t_question_knowledge.php
@@ -9,17 +9,25 @@ class t_question_knowledge extends \App\Models\Zgen\z_t_question_knowledge
 	}
 
     public function question_know_list($question_id){
-        $sql = $this->gen_sql("select kq.id,kq.difficult,kq.knowledge_id,kn.subject,kn.title,kn.detail
-                               from %s kq left join %s kn on kq.knowledge_id = kn.knowledge_id
-                               where kq.question_id = %s
+        $sql = $this->gen_sql("select * from %s where question_id = %s and type = 1
                                order by question_id desc ",
                               self::DB_TABLE_NAME,
-                              t_knowledge_point::DB_TABLE_NAME,
                               $question_id
         );
 
         return $this->main_get_list($sql);
     }
+
+    public function answer_know_list($answer_id){
+        $sql = $this->gen_sql("select * from %s where answer_id = %s and type = 2
+                               order by question_id desc ",
+                              self::DB_TABLE_NAME,
+                              $answer_id
+        );
+
+        return $this->main_get_list($sql);
+    }
+
 
     public function is_question_know_exit($question_id,$knowledge_id,$difficult){
         $sql = $this->gen_sql("select * from %s 
