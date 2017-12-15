1f5a56db0a46fe5f1625fdd9315f2c59c65f8cf8
gupeigen <gupeigen@leoedu.com>
2017-12-15 17:23:32 +0800

    bacon

--- database/migrations/2017_12_15_170320_t_change_knowledge.php
+++ database/migrations/2017_12_15_170320_t_change_knowledge.php
@@ -14,7 +14,7 @@ class TChangeKnowledge extends Migration
         
  
         Schema::table('db_question_new.t_knowledge_point', function (Blueprint $table){
-            $table->dropColumn('grade');
+            //$table->dropColumn('grade');
             // $table->increments('knowledge_id','知识点id');
             //     t_field($table->string('title'),"知识点标题");
             //     t_field($table->integer('subject'),"知识点科目 1:数学 2:语文 3:英语 4:物理 5:化学 6:生物 7:历史 8:政治 9:地理");
@@ -33,6 +33,11 @@ class TChangeKnowledge extends Migration
             //$table->dropColumn('difficult');
                 // t_field($table->integer('answer_id')->default(null),"答案id");
                 // t_field($table->integer('type')->default(1),"1:题目对应的知识点,2:答案对应的知识点");
+        });
+
+        Schema::table('db_question_new.t_knowledge_level', function (Blueprint $table){
+            // $table->dropColumn('level');
+            // t_field($table->integer('answer_type')->default(1),"1:答案,2:解题步骤,3:解析过程");
         });
 
     }
