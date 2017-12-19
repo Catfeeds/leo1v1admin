2031dced5d05c7ad212ad6f5af8693616dc342b8
gupeigen <gupeigen@leoedu.com>
2017-12-19 17:32:41 +0800

    question

--- database/migrations/2017_12_19_163745_t_create_knowledge_level.php
+++ database/migrations/2017_12_19_163745_t_create_knowledge_level.php
@@ -30,22 +30,22 @@ class TCreateKnowledgeLevel extends Migration
         //         t_field($table->integer('level')->default(0),"0:根部节点");
         //     t_field($table->integer('father_id'),"父级id");
         // });
-        Schema::dropIfExists('db_question_new.t_question_type');
-        Schema::create('db_question_new.t_question_type', function (Blueprint $table){
-            $table->increments('id');
-                t_field($table->integer('subject'),"科目");
-                t_field($table->string('name'),"题型名字");
-                t_field($table->integer('open_flag')->default(1),"是否开启");
-        });
+        // Schema::dropIfExists('db_question_new.t_question_type');
+        // Schema::create('db_question_new.t_question_type', function (Blueprint $table){
+        //     $table->increments('id');
+        //         t_field($table->integer('subject'),"科目");
+        //         t_field($table->string('name'),"题型名字");
+        //         t_field($table->integer('open_flag')->default(1),"是否开启");
+        // });
 
-        Schema::table('db_question_new.t_textbook', function (Blueprint $table){
-            t_field($table->integer('open_flag')->default(1),"是否开启");
-        });
+        // Schema::table('db_question_new.t_textbook', function (Blueprint $table){
+        //     t_field($table->integer('open_flag')->default(1),"是否开启");
+        // });
 
-        Schema::table('db_question_new.t_question', function (Blueprint $table){
-            t_field($table->string('question_resource_name')->default(null),"题目来源名字");
-            t_field($table->integer('question_resource_type')->default(0),"题目来源类型");
-        });
+        // Schema::table('db_question_new.t_question', function (Blueprint $table){
+        //     t_field($table->string('question_resource_name')->default(null),"题目来源名字");
+        //     t_field($table->integer('question_resource_type')->default(0),"题目来源类型");
+        // });
 
     }
 
