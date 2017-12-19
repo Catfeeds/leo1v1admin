132f7d201f5f3b7e70d6cff7709d1e1b32a1f0e0
james <james@leoedu.com>
2017-12-19 15:47:42 +0800

    s

---
+++ public/page_ts/tongji2/market_extension.ts
@@ -0,0 +1,32 @@
+/// <reference path="../common.d.ts" />
+/// <reference path="../g_args.d.ts/tongji2-market_extension.d.ts" />
+
+function load_data(){
+	  if ( window["g_load_data_flag"]) {return;}
+		$.reload_self_page ( {
+		    order_by_str : g_args.order_by_str,
+		    type:	$('#id_type').val(),
+		    date_type_config:	$('#id_date_type_config').val(),
+		    date_type:	$('#id_date_type').val(),
+		    opt_date_type:	$('#id_opt_date_type').val(),
+		    start_time:	$('#id_start_time').val(),
+		    end_time:	$('#id_end_time').val()
+		});
+}
+$(function(){
+	  $('#id_date_range').select_date_range({
+		    'date_type' : g_args.date_type,
+		    'opt_date_type' : g_args.opt_date_type,
+		    'start_time'    : g_args.start_time,
+		    'end_time'      : g_args.end_time,
+		    date_type_config : JSON.parse( g_args.date_type_config),
+		    onQuery :function() {
+			      load_data();
+		    }
+    });
+
+	  $('#id_type').val(g_args.type);
+	  $('.opt-change').set_input_change_event(load_data);
+});
+
+
--- resources/views/tongji2/market_extension.blade.php
+++ resources/views/tongji2/market_extension.blade.php
@@ -7,7 +7,7 @@
       <script type="text/javascript" src="/js/qiniu/highlight/highlight.js"></script>
       <script type="text/javascript" src="/js/jquery.md5.js"></script>
     <section class="content ">
-        
+
         <div>
             <div class="row  row-query-list" >
                 <div class="col-xs-12 col-md-5"  data-title="时间段">
@@ -21,58 +21,26 @@
                         </select>
                     </div>
                 </div>
-                <div class="col-xs-6 col-md-2">
-                    <div class="input-group ">
-                        <span class="input-group-addon">科目</span>
-                        <select class="opt-change form-control" id="id_subject" >
-                        </select>
-                    </div>
-                </div>
-                <div class="col-xs-6 col-md-2">
-                    <div class="input-group ">
-                        <span class="input-group-addon">pad</span>
-                        <select class="opt-change form-control " id="id_pad" >
-                        </select>
-                    </div>
-                </div>
             </div>
         </div>
         <hr/>
-        <table     class="common-table"  > 
+        <table     class="common-table"  >
             <thead>
                 <tr>
                     <td>ID</td>
-                    <td>添加时间</td>
-                    <td>第一次试听课时间</td>
-                    <td>年级</td>
-                    <td>科目</td>
-                    <td>pad</td>
-                    <td>地区</td>
-                    <td>电信商</td>
-                    <td>第二级渠道</td>
-                    <td>第三级渠道</td>
-                    <td>渠道进入数</td>
-                    <td>电话接通数</td>
-                    <td>回流公海数</td>
+                    <td>礼品类型</td>
+                    <td>标题</td>
+                    <td>活动描述</td>
+                    <td>活动链接</td>
+                    <td>活动状态</td>
                     <td> 操作  </td>
                 </tr>
             </thead>
             <tbody>
-                @foreach ( $table_data_list as $var )
+                @foreach ( $table_data_list as $i => $var )
                     <tr>
+                        <td>{{@$i}} </td>
                         <td>{{@$var["userid"]}} </td>
-                        <td>{{@$var["add_time"]}} </td>
-                        <td>{{@$var["lesson_time"]}} </td>
-                        <td>{{@$var["grade_str"]}} </td>
-                        <td>{{@$var["subject_str"]}} </td>
-                        <td>{{@$var["pad_str"]}} </td>
-                        <td>{{@$var["location"]}} </td>
-                        <td>{{@$var["cor"]}} </td>
-                        <td>{{@$var["two_origin"]}} </td>
-                        <td>{{@$var["three_origin"]}} </td>
-                        <td>{{@$var["origin_count"]}} </td>
-                        <td>{{@$var["cc_called_count"]}} </td>
-                        <td>{{@$var["return_publish_count"]}} </td>
 
 
                         <td>
@@ -90,6 +58,5 @@
         </table>
         @include("layouts.page")
     </section>
-    
-@endsection
 
+@endsection
