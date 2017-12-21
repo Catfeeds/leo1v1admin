da2ff4a4446fff55bc0d34437a66e2f34f26009f
jim <jim@leoedu.com>
2017-12-21 16:10:10 +0800

    s

--- app/Config/publish_version.php
+++ app/Config/publish_version.php
@@ -2,5 +2,5 @@
 namespace App\Config;
 class publish_version {
     //不要手动修改
-    static public $version="20171221-160314";
+    static public $version="20171221-160529";
 };
---
+++ app/Models/t_teacher_christmas.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Models;
+use \App\Enums as E;
+class t_teacher_christmas extends \App\Models\Zgen\z_t_teacher_christmas
+{
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
