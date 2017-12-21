b4ab85ceb10f96021d1d7dcdce02936d7e49cc0f
james <james@leoedu.com>
2017-12-21 15:51:10 +0800

    s

--- app/Http/Controllers/test_james.php
+++ app/Http/Controllers/test_james.php
@@ -351,23 +351,31 @@ class test_james extends Controller
     }
 
     public function installNew(){ // 新建表单
-        Schema::dropIfExists('db_weiyi.t_activity_usually');
-        Schema::create('db_weiyi.t_activity_usually', function(Blueprint $table) {
-            t_field($table->increments("id"), "市场日常活动表");
-            t_field($table->integer("gift_type"), "礼品类型");
-            t_field($table->string("title",524), "活动标题");
-            t_field($table->string("describe",2048), "活动描述");
-            t_field($table->string("url",512), "活动链接");
-            t_field($table->tinyInteger("activity_status"), "活动状态");
-            t_field($table->integer("add_time"), "添加时间");
-            t_field($table->integer("uid"), "添加人");
+        // Schema::dropIfExists('db_weiyi.t_activity_usually');
+        // Schema::create('db_weiyi.t_activity_usually', function(Blueprint $table) {
+        //     t_field($table->increments("id"), "市场日常活动表");
+        //     t_field($table->integer("gift_type"), "礼品类型");
+        //     t_field($table->string("title",524), "活动标题");
+        //     t_field($table->string("describe",2048), "活动描述");
+        //     t_field($table->string("url",512), "活动链接");
+        //     t_field($table->tinyInteger("activity_status"), "活动状态");
+        //     t_field($table->integer("add_time"), "添加时间");
+        //     t_field($table->integer("uid"), "添加人");
 
-            $table->index('userid');
+        //     $table->index('userid');
+        //     $table->index('add_time');
+        // });
+
+        Schema::create('db_weiyi.t_teacher_christmas', function(Blueprint $table) {
+            t_field($table->increments("id"), "老师圣诞节活动");
+            t_field($table->integer("teacherid"), "分享人");
+            t_field($table->string("next_openid"), "下级openid");
+            t_field($table->integer("add_time"), "添加时间");
+            t_field($table->integer("score"), "积分");
+
+            $table->index('teacherid');
             $table->index('add_time');
         });
-
-
-
 
     }
 
---
+++ database/migrations/2017_12_21_154815_create_t_teacher_christmas.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTTeacherChristmas extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('db_weiyi.t_teacher_christmas', function(Blueprint $table) {
+            t_field($table->increments("id"), "老师圣诞节活动");
+            t_field($table->integer("teacherid"), "分享人");
+            t_field($table->string("next_openid"), "下级openid");
+            t_field($table->integer("add_time"), "添加时间");
+            t_field($table->integer("score"), "积分");
+
+            $table->index('teacherid');
+            $table->index('add_time');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
