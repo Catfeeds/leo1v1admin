d1bedd9bba1d7821077c420f3f9b326770f5a92f
adrian <adrian@leoedu.com>
2017-12-23 10:54:31 +0800

    fix

--- app/Http/Controllers/tea_manage.php
+++ app/Http/Controllers/tea_manage.php
@@ -1731,7 +1731,6 @@ class tea_manage extends Controller
         $lessonid        = $this->get_in_int_val("lessonid",-1);
         $lesson_sub_type = $this->get_in_int_val("lesson_sub_type",-1);
         $train_type      = $this->get_in_int_val("train_type",-1);
-        $acc             = $this->get_account();
         $page_num        = $this->get_in_page_num();
 
         $this->t_lesson_info->switch_tongji_database();
@@ -1767,9 +1766,7 @@ class tea_manage extends Controller
             $val['server_type_str'] = \App\Helper\Utils::get_server_type_str($val);
         }
 
-        return $this->pageView(__METHOD__,$ret_info,[
-            "acc" => $acc
-        ]);
+        return $this->pageView(__METHOD__,$ret_info);
     }
 
     public function add_train_lesson(){
--- table_config/_z_config.php
+++ table_config/_z_config.php
@@ -138,7 +138,7 @@ return array(
 
 
     ["db_weiyi","t_month_student_count","id"] ,
-    ["db_weiyi","t_train_lesson_user","userid","add_time"] ,
+    ["db_weiyi","t_train_lesson_user","lessonid","userid"] ,
     ["db_weiyi","t_psychological_teacher_time_list","day","start"] ,
     ["db_weiyi","t_student_init_info","userid"] ,
     ["db_weiyi","t_gift_consign","exchangeid"] ,
