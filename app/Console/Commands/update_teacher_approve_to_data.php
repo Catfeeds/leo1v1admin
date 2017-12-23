545e210efa74303a167e4fb5e7793668212df181
ricky <ricky>
2017-12-23 15:20:22 +0800

    f

--- app/Console/Commands/update_teacher_approve_to_data.php
+++ app/Console/Commands/update_teacher_approve_to_data.php
@@ -49,7 +49,7 @@ class update_teacher_approve_to_data extends Command
             $cr_order_num  = $task->t_order_info->get_cc_test_lesson_num($start_time, $end_time, $teacherid,'1');
             $cr_lesson_num = $task->t_order_info->get_cc_lesson_num($start_time, $end_time, $teacherid, '1');
             $violation_info = $task->t_lesson_info_b3->get_violation_num($start_time, $end_time, $teacherid);
-            $item['violation_num'] = array_sum($violation_info);
+            $violation_num = array_sum($violation_info);
 
             $id = $task->t_teacher_approve_refer_to_data->get_id_for_teacherid($start_time, $end_time, $teacherid);
             echo $id;
@@ -63,7 +63,7 @@ class update_teacher_approve_to_data extends Command
                     'cc_lesson_num' => $cc_lesson_num,
                     'cr_order_num' => $cr_order_num,
                     'cr_lesson_num' => $cr_lesson_num,
-                    'violation_num' => $violation_info,
+                    'violation_num' => $violation_num,
                 ]);
             } else {
                 echo '调用添加';
@@ -75,7 +75,7 @@ class update_teacher_approve_to_data extends Command
                     'cc_lesson_num' => $cc_lesson_num,
                     'cr_order_num' => $cr_order_num,
                     'cr_lesson_num' => $cr_lesson_num,
-                    'violation_num' => $violation_info,
+                    'violation_num' => $violation_num,
                     'add_time' => $start_time
                 ]);
 
