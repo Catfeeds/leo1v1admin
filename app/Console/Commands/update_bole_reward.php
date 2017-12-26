f8f053bb6bf4b66ea95fb65e58745d842b47e899
ricky <ricky>
2017-12-26 13:19:04 +0800

    f

--- app/Console/Commands/update_bole_reward.php
+++ app/Console/Commands/update_bole_reward.php
@@ -58,10 +58,9 @@ class update_bole_reward extends Command
                 if (count($a_info) != count($m_info)) {
                     foreach($a_info as $val) {
                         if(!isset($m_info[$val['teacherid']])) { // 处理丢失数据
-                            if ($teacherid != 134643) continue;
                             echo $teacherid.' '.$val['teacherid'].PHP_EOL;
                             //if ($teacherid != 134643) continue;
-                            $tea->update_bole_reward($teacherid,$val['teacherid']);
+                            //$tea->update_bole_reward($teacherid,$val['teacherid']);
                         }
                     }
                 }
