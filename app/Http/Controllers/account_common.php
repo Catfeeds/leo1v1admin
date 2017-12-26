f677b07e0f4e975b754f1e174b7bdb274ece1af2
jack <jack@leodeu.com>
2017-12-26 16:20:58 +0800

    fix

--- app/Http/Controllers/account_common.php
+++ app/Http/Controllers/account_common.php
@@ -328,8 +328,8 @@ class account_common extends Controller
             return $this->output_err("密码错误");
         }
         if($role==1){
-            $_SESSION['uid']  = $userid;
-            $_SESSION['role'] = 1;
+            // $_SESSION['uid']  = $userid;
+            // $_SESSION['role'] = 1;
 
             // $old_passwd = $this->t_user_info->get_passwd($userid);
             // $data = file_get_contents("http://api.leo1v1.com/login/stu_login?phone=".$phone."&passwd=".$old_passwd);
