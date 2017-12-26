7dd78902726c02c2f0c4ae009ae9dc03dec469a1
tom <tom@leoedu.com>
2017-12-26 18:46:58 +0800

    f

--- public/page_ts/g_args.d.ts/agent-check.d.ts
+++ public/page_ts/g_args.d.ts/agent-check.d.ts
@@ -1,7 +1,4 @@
 interface GargsStatic {
-	origin_ex:	string;
-	page_num:	number;
-	page_count:	number;
 }
 declare module "g_args" {
     export = g_args;
@@ -11,7 +8,14 @@ declare var g_account: string;
 declare var g_account_role: any;
 declare var g_adminid: any;
 interface RowData {
-	textbook_str	:any;
+	userid	:any;
+	phone	:any;
+	origin	:any;
+	add_time	:any;
+	global_tq_called_flag	:any;
+	last_succ_test_lessonid	:any;
+	last_adminid	:any;
+	is_order	:any;
 }
 
 /*
@@ -26,13 +30,12 @@ function load_data(){
 	if ( window["g_load_data_flag"]) {return;}
 		$.reload_self_page ( {
 		order_by_str : g_args.order_by_str,
-		origin_ex:	$('#id_origin_ex').val()
+
 		});
 }
 $(function(){
 
 
-	$('#id_origin_ex').val(g_args.origin_ex);
 
 
 	$('.opt-change').set_input_change_event(load_data);
@@ -42,14 +45,4 @@ $(function(){
 
 */
 /* HTML ...
-
-        <div class="col-xs-6 col-md-2">
-            <div class="input-group ">
-                <span class="input-group-addon">origin_ex</span>
-                <input class="opt-change form-control" id="id_origin_ex" />
-            </div>
-        </div>
-{!!\App\Helper\Utils::th_order_gen([["origin_ex title", "origin_ex", "th_origin_ex" ]])!!}
-{!!\App\Helper\Utils::th_order_gen([["page_num title", "page_num", "th_page_num" ]])!!}
-{!!\App\Helper\Utils::th_order_gen([["page_count title", "page_count", "th_page_count" ]])!!}
 */
