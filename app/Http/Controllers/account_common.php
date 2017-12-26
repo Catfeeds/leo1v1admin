a4df8b11ba711badfc56761f9492d8a5645c9155
jack <jack@leodeu.com>
2017-12-26 15:47:50 +0800

    fix

--- app/Http/Controllers/account_common.php
+++ app/Http/Controllers/account_common.php
@@ -305,7 +305,7 @@ class account_common extends Controller
         }
         if($role==1){
             $old_passwd = $this->t_user_info->get_passwd($userid);
-            $data = file_get_contents("http://api.leo1v1.com/login/stu_login?phone=".$phone."&passwd=".$old_passw);
+            $data = file_get_contents("http://api.leo1v1.com/login/stu_login?phone=".$phone."&passwd=".$old_passwd);
         }
         dd($data);
 
