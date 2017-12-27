69031fa77862bbb93ffd05649be8a56e4cd0862a
gupeigen <gupeigen@leoedu.com>
2017-12-27 17:10:59 +0800

    que

--- app/Models/t_student_answer.php
+++ app/Models/t_student_answer.php
@@ -35,32 +35,27 @@ class t_student_answer extends \App\Models\Zgen\z_t_student_answer
     }
 
     public function get_answer_scores($room_id){
-        $where_arr = [
-            ["sa.room_id=%d" , $room_id ],
-        ];
-        $where_str = $this->where_str_gen($where_arr);
-        $sql = $this->gen_sql("select sa.score,sa.question_id,a.score as full_score,a.answer_type,group_concat(k.knowledge_id)
-                              as know_str from %s sa
+        $sql = $this->gen_sql("select sa.id,sa.score,sa.question_id,sa.step_id,a.score as full_score,a.answer_type,
+                              group_concat(k1.knowledge_id) as step_know,group_concat(k2.knowledge_id) as qu_know from %s sa
                               left join %s a on sa.step_id = a.step_id
-                              left join %s k on a.step_id = k.answer_id
-                              where %s order by a.answer_no asc"
+                              left join %s k1 on sa.step_id = k1.answer_id
+                              left join %s k2 on sa.question_id = k2.question_id
+                              where sa.room_id = '%s' group by sa.id order by a.answer_no asc"
                               ,self::DB_TABLE_NAME
                               ,t_answer::DB_TABLE_NAME
                               ,t_question_knowledge::DB_TABLE_NAME
-                              ,$where_str );
+                              ,t_question_knowledge::DB_TABLE_NAME
+                              ,$room_id );
         return $this->main_get_list($sql);
     }
 
-    public function get_answer_count($teacher_id,$student_id){
-        $where_arr = [
-            ["sa.room_id=%d" , $room_id ]
-        ];
-        $where_str = $this->where_str_gen($where_arr);
-        $sql = $this->gen_sql("select count(sa.question_id) as count from %s as inner join %s q on
-                               sa.question_id = q.question_id where %s group by sa.question_id"
+
+    public function get_answer_count($room_id){
+        $sql = $this->gen_sql("select count(sa.question_id) as count from %s sa inner join %s q on
+                               sa.question_id = q.question_id where sa.room_id = '%s' group by sa.question_id"
                               ,self::DB_TABLE_NAME
                               ,t_question::DB_TABLE_NAME
-                              ,$where_str );
+                              ,$room_id );
         return $this->main_get_row($sql);
     }
 }
