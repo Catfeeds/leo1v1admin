cf570bf5d43a30d7efefce0a89ca1c5f5225af93
james <james@leoedu.com>
2017-12-29 12:06:34 +0800

    s

--- database/migrations/2017_12_29_120318_t_lesson_info_reset_zip_url.php
+++ database/migrations/2017_12_29_120318_t_lesson_info_reset_zip_url.php
@@ -16,7 +16,7 @@ class TLessonInfoResetZipUrl extends Migration
         Schema::table('db_weiyi.t_lesson_info', function( Blueprint $table)
         {
             $table->dropColumn('zip_url');
-            t_field($table->string("zip_url",100) ,"老师讲义压缩包链接");
+            // t_field($table->string("zip_url",100) ,"老师讲义压缩包链接");
         });
 
     }
