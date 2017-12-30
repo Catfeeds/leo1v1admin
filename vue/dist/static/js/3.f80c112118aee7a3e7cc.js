078877c89bf51062e7ed2ec85ea3377d2db824cb
jim <jim@leoedu.com>
2017-12-30 14:13:57 +0800

    fix

--- vue/dist/static/js/3.f80c112118aee7a3e7cc.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([3],{doec:function(c,n){}});
-//# sourceMappingURL=3.f80c112118aee7a3e7cc.js.map
--- vue/dist/static/js/4.3cc5935388d5985a1016.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([4],{Z3Z2:function(n,c){}});
-//# sourceMappingURL=4.3cc5935388d5985a1016.js.map
