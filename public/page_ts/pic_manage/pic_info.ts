3f888ecc2fbd55e0db4a446ebea710e8f2869ba5
ricky <ricky>
2018-01-04 18:56:31 +0800

    f

--- public/page_ts/pic_manage/pic_info.ts
+++ public/page_ts/pic_manage/pic_info.ts
@@ -105,7 +105,7 @@ $(function(){
             html_node.find(".add_jump_type").val(item.jump_type);
             html_node.find(".add_start_date").val(item.start_time);
             html_node.find(".add_end_date").val(item.end_time);
-            console.log(item.type + " : " + item.usage_type);
+            console.log(item.type + " : " + html_node.find(".add_pic_usage_type").val());
             if (html_node.find(".add_pic_usage_type").val() == 303) {
                 $(".add_jump_type option[value='1']").remove()
             }
