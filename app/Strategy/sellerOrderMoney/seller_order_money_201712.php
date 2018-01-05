0713c08badbcf794ce28d0d6819326c36f18c8d4
tom <tom@leoedu.com>
2018-01-05 15:14:40 +0800

    f

--- app/Strategy/sellerOrderMoney/seller_order_money_201712.php
+++ app/Strategy/sellerOrderMoney/seller_order_money_201712.php
@@ -2,7 +2,7 @@
 namespace App\OrderPrice;
 namespace App\Strategy\sellerOrderMoney  ;
 
-class seller_order_money_201711  extends  seller_order_money_base
+class seller_order_money_201712  extends  seller_order_money_base
 {
 
     static $percent_config = [
