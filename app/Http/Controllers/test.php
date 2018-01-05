9cb11f8496737e8a453c2f2555a1195858c48a55
adrian <adrian@leoedu.com>
2018-01-05 19:04:52 +0800

    fix

--- app/Http/Controllers/test.php
+++ app/Http/Controllers/test.php
@@ -55,7 +55,7 @@ class test extends Controller
         //$this->get_account_role();
         $this->set_in_value("adminid", $this->get_account_id())  ;
 
-        $this->html_hide_list_add([ "grade","opt_grade", "input_grade" ]);
+        $this->html_power_list_add([ "grade","opt_grade", "input_grade" ]);
         return $this->get_user_list();
     }
 
