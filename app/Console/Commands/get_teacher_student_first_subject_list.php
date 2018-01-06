257bf46d4ae462487bf78465aa65d8a7efb4d9a5
jack <jack@leodeu.com>
2018-01-06 17:52:41 +0800

    fix

--- app/Console/Commands/get_teacher_student_first_subject_list.php
+++ app/Console/Commands/get_teacher_student_first_subject_list.php
@@ -41,8 +41,35 @@ class get_teacher_student_first_subject_list extends Command
 
         $task = new \App\Console\Tasks\TaskController ();
         $start_time = strtotime("2018-01-01");
-        $end_time = time();
+        $end_time = strtotime("2018-01-06");
         $list  = $task->t_lesson_info_b3->get_teacher_student_first_subject_info($start_time,$end_time);
+        $i=0;
+        foreach($list as $val){
+            $id = $val["id"];
+            if($id>0){
+                $info =$task->t_teacher_record_list->get_record_info($id);
+                $lessonid =$task->t_teacher_record_list->get_train_lessonid($id);
+                $info .= "lessonid".$lessonid." change to".$val["lessonid"].",时间:".time().";";
+                $task->t_teacher_record_list->field_update_list($id,[
+                    "lesson_time" => $val["lesson_start"],
+                    "record_info" => $info,
+                    "train_lessonid"=>$val["train_lessonid"]
+                ]);
+
+            }else{
+                $add_time = time()+$i;
+                $task->t_teacher_record_list->row_insert([
+                    "teacherid"      => $val["teacherid"],
+                    "userid"         => $val["userid"],
+                    "lesson_subject" => $val["subject"],
+                    "lesson_time"    => $val["lesson_start"],
+                    "train_lessonid" => $val["lessonid"],
+                    "add_time"       => $add_time,
+                    "type"           => 18
+                ]);
+                $i++;
+            }
+        }
         dd($list);
  
     }
