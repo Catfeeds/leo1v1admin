18f45abd962a3f8b5bdfc0f0d6cedf04179d8453
jim <jim@leoedu.com>
2018-01-10 17:36:35 +0800

    f

--- vue/build.sh
+++ vue/build.sh
@@ -2,10 +2,14 @@
 cd $(dirname $0)
 ./gen_route.php
 npm run build
+if [ $? -ne 0 ] ;then
+	echo "build fail.";
+	exit
+fi
 sed -i -e "s/self\./p./g" ./dist/index.html
 
-tar czvf  dist.tar.gz dist
-echo "cp dist.tar.gz to 0.6"
-sshpass -p xcwen142857 scp dist.tar.gz jim@192.168.0.6:~/dist.tar.gz
-echo "tar xvf ~/dist.tar.gz  -C ~/admin.yb1v1.com/vue/  "
-sshpass -p xcwen142857 ssh jim@192.168.0.6 "tar xvf ~/dist.tar.gz  -C ~/admin.yb1v1.com/vue/"
+#tar czvf  dist.tar.gz dist
+#echo "cp dist.tar.gz to 0.6"
+#sshpass -p xcwen142857 scp dist.tar.gz jim@192.168.0.6:~/dist.tar.gz
+#$echo "tar xvf ~/dist.tar.gz  -C ~/admin.yb1v1.com/vue/  "
+#$sshpass -p xcwen142857 ssh jim@192.168.0.6 "tar xvf ~/dist.tar.gz  -C ~/admin.yb1v1.com/vue/"
