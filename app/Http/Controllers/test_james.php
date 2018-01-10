8e44fca99f53ad0f3cbf4661e7044c96642f6f8e
james <james@leoedu.com>
2018-01-10 17:44:50 +0800

    s

--- app/Http/Controllers/test_james.php
+++ app/Http/Controllers/test_james.php
@@ -1726,7 +1726,7 @@ class test_james extends Controller
 
     public function getSellerNum(){
         $s = $this->get_in_str_val('s');
-        $ret = $this->t_admin_group_name->get_group_seller_num_test($s,1);
+        $ret = $this->t_admin_group_name->get_group_seller_num_test(1,$s);
         dd($ret);
     }
 
