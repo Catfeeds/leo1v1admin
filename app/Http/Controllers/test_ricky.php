93ec583a943cb76c4c1d2c721eb39f9e3a0f0469
ricky <ricky>
2018-01-10 18:33:11 +0800

    f

--- app/Http/Controllers/test_ricky.php
+++ app/Http/Controllers/test_ricky.php
@@ -45,7 +45,8 @@ class test_ricky extends Controller
         $end_time = strtotime('2018-1-9');
         $count = $this->t_lesson_info_b3->get_test_lesson_count($start_time, $end_time);
         foreach($count as $item) {
-            $order = $this->t_order_info->get_not_order($item);
+            var_dump($item);
+            $order = $this->t_order_info->get_not_order($item['userid']);
             var_dump($order);
             if ($order) {
                 echo "有值";
