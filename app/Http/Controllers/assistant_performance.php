040dad557ab3eb149272b1ba5621af89603b23ce
jack <jack@leodeu.com>
2018-01-10 10:00:23 +0800

    fix

--- app/Http/Controllers/assistant_performance.php
+++ app/Http/Controllers/assistant_performance.php
@@ -554,6 +554,7 @@ class assistant_performance extends Controller
         $adminid = $this->get_in_int_val("adminid",324);
         list($start_time,$end_time)=$this->get_in_date_range(0,0,0,[],3);
         $ass_month= $this->t_month_ass_student_info->get_ass_month_info_payroll($start_time);
+        dd($ass_month);
         $list = @$ass_month[$adminid];        
         $ret_info=[];
 
