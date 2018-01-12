903e459d75f4fd3c115eb9422dd550ba13013726
ricky <ricky>
2018-01-12 14:29:10 +0800

    f

--- app/Console/Commands/add_new_tea_entry.php
+++ app/Console/Commands/add_new_tea_entry.php
@@ -45,6 +45,7 @@ class add_new_tea_entry extends Command
         $task->t_teacher_info->field_update_list(469110, [
             'bank_type' => '中国工商银行'
         ]);
+        exit;
 
         
         // 明日之星
