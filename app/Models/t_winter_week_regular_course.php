1875e9ead79915f640754500a18cbf42952eaf5b
jack <jack@leodeu.com>
2018-01-12 19:26:12 +0800

    fix

--- app/Models/t_winter_week_regular_course.php
+++ app/Models/t_winter_week_regular_course.php
@@ -49,6 +49,89 @@ class t_winter_week_regular_course extends \App\Models\Zgen\z_t_winter_week_regu
         
     }
 
+    public function check_is_clash_stu_new( $userid, $lessonid,$start_time) {
+        if($userid == "()"){
+            $where_arr[]= "w.userid = 1";
+        }else{
+            $where_arr[] = "w.userid in".$userid;
+        }
+        if($lessonid == "()"){
+            $where_arr_lesson[]= "l.lessonid = 1";
+        }else{
+            $where_arr_lesson[] = "l.lessonid in".$lessonid;
+        }
+
+        $res = [];
+        $sql=$this->gen_sql_new(
+            "select  w.userid,l.lesson_start,l.lesson_count,substring(start_time,1,1)*86400+ substring(start_time,3,2)*3600 + substring(start_time,6,2)*60 as start_time_s , substring(start_time,1,1)*86400+ substring(end_time,1,2)*3600 + substring(end_time,4,2)*60 as end_time_s".
+            " from %s w left join %s l on w.userid = l.userid  where %s "
+            . " and lesson_del_flag=0 and confirm_flag in (0,1)"
+
+            ."and %s  having ( start_time_s = ( select unix_timestamp(substring(from_unixtime(`lesson_start`),1,10))- %s +86400 +substring(from_unixtime(`lesson_start`),12,2)*3600 + substring(from_unixtime(`lesson_start`),15,2)*60 from %s where lessonid= l.lessonid)  and  end_time_s in (select unix_timestamp(substring(from_unixtime(`lesson_end`),1,10))- %s +86400 +substring(from_unixtime(`lesson_end`),12,2)*3600 + substring(from_unixtime(`lesson_end`),15,2)*60 from %s where lessonid= l.lessonid)) ",
+            self::DB_TABLE_NAME,t_lesson_info::DB_TABLE_NAME,
+            $where_arr, $where_arr_lesson,$start_time, t_lesson_info::DB_TABLE_NAME,$start_time,t_lesson_info::DB_TABLE_NAME) ;
+        $res['all'] = $this->main_get_list($sql);
+        $sql=$this->gen_sql_new(
+            "select  w.userid,w.teacherid,substring(start_time,1,1)*86400+ substring(start_time,3,2)*3600 + substring(start_time,6,2)*60 as start_time_s , substring(start_time,1,1)*86400+ substring(end_time,1,2)*3600 + substring(end_time,4,2)*60 as end_time_s  ".
+            " from %s w left join %s l on w.userid = l.userid  where %s and  %s"
+            . " and lesson_del_flag=0 and confirm_flag in (0,1)"
+            ." having (  end_time_s >  (select unix_timestamp(substring(from_unixtime(`lesson_start`),1,10))- %s +86400 +substring(from_unixtime(`lesson_start`),12,2)*3600 + substring(from_unixtime(`lesson_start`),15,2)*60 from %s where lessonid= l.lessonid) and  start_time_s <  (select unix_timestamp(substring(from_unixtime(`lesson_end`),1,10))- %s +86400 +substring(from_unixtime(`lesson_end`),12,2)*3600 + substring(from_unixtime(`lesson_end`),15,2)*60 from %s where lessonid= l.lessonid) and (start_time_s <>  (select unix_timestamp(substring(from_unixtime(`lesson_start`),1,10))- %s +86400 +substring(from_unixtime(`lesson_start`),12,2)*3600 + substring(from_unixtime(`lesson_start`),15,2)*60 from %s where lessonid= l.lessonid) or end_time_s <> (select unix_timestamp(substring(from_unixtime(`lesson_end`),1,10))- %s +86400 +substring(from_unixtime(`lesson_end`),12,2)*3600 + substring(from_unixtime(`lesson_end`),15,2)*60 from %s where lessonid= l.lessonid) or teacherid <> (select teacherid from %s where lessonid = l.lessonid)))  ",
+            self::DB_TABLE_NAME,t_lesson_info::DB_TABLE_NAME,
+            $where_arr,$where_arr_lesson,$start_time,t_lesson_info::DB_TABLE_NAME,$start_time,t_lesson_info::DB_TABLE_NAME,$start_time,t_lesson_info::DB_TABLE_NAME,$start_time,t_lesson_info::DB_TABLE_NAME,t_lesson_info::DB_TABLE_NAME) ;
+        #return $sql;
+        $res['clash'] =  $this->main_get_list($sql);
+        return $res;
+    }
+
+    public function get_stu_count_total_new($userid){
+        if($userid == "()"){
+            $where_arr[]= "userid = 1";
+        }else{
+            $where_arr[] = "userid in".$userid;
+        }
+        $sql = $this->gen_sql_new("select userid,sum(lesson_count) regular_total from %s where %s group by userid",
+                                  self::DB_TABLE_NAME,
+                                  $where_arr
+        );
+        return $this->main_get_list($sql,function($item){
+            return $item['userid'];
+        });
+    }
+    public function get_lesson_info($teacherid,$userid){
+        if($teacherid ==-1 && $userid == -1){
+            $where_arr = "teacherid =".$teacherid;
+        }else{
+            $where_arr=[
+                ['teacherid = %s',$teacherid,-1],
+                ['userid = %s',$userid,-1]
+            ];
+        }
+
+        $sql = $this->gen_sql_new("select * from %s where %s",
+                                  self::DB_TABLE_NAME,
+                                  $where_arr
+        );
+        return $this->main_get_list($sql,function($item){
+            return $item["start_time"];
+        });
+    }
+
+    public function get_lesson_info_new($userid){
+        if($userid == "()"){
+            $where_arr[]= "userid = 1";
+        }else{
+            $where_arr[] = "userid in".$userid;
+        }
+
+        $sql = $this->gen_sql_new("select * from %s where %s",
+                                  self::DB_TABLE_NAME,
+                                  $where_arr
+        );
+        return $this->main_get_list($sql);
+    }
+
+
+
     public function add_regular_course($teacherid,$userid,$start_time,$end_time,$lesson_count,$competition_flag)
     {
         return $this->row_insert([
