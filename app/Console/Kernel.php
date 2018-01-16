149438e581111f2f3bcd8945895df66a2f8275d0
ricky <ricky>
2018-01-16 12:04:45 +0800

    f

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,6 +16,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
+        Commands\test_ricky::class,
         Commands\SendSmsByPhone::class,
         Commands\h5GetPoster::class,
         Commands\uploadPdfChange::class,
