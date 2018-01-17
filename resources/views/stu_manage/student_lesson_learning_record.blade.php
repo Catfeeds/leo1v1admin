31326333e63c8f9162a4ff45384a4b0f5318e432
jack <jack@leodeu.com>
2018-01-17 12:39:17 +0800

    fix

--- resources/views/stu_manage/student_lesson_learning_record.blade.php
+++ resources/views/stu_manage/student_lesson_learning_record.blade.php
@@ -54,32 +54,12 @@
         <table class="common-table">
             <thead>
                 <tr>
-                    <td>orderid</td>
-                    <td>年级</td>
-                    <td>科目</td>
-                    <td>合同类型</td>
-                    <td>合同状态</td>
-                    <td>总课时数</td>
-                    <td>剩余课时</td>
-                    <td>实付价格</td>
-                    <td>原始金额</td>
-                    <td>优惠原因</td>
                     <td>操作</td>
                 </tr>
             </thead>
             <tbody>
                 @foreach ($table_data_list as $var)
                     <tr>
-                        <td> {{$var["orderid"]}} </td>
-                        <td> {{$var["grade_str"]}} </td>
-                        <td> {{$var["subject_str"]}} </td>
-                        <td> {{$var["contract_type_str"]}} </td>
-                        <td> {{$var["contract_status_str"]}} </td>
-                        <td> {{$var["lesson_total"]}} </td>
-                        <td> {{$var["order_left"]}} </td>
-                        <td> {{$var["price"]}} </td>
-                        <td> {{$var["discount_price"]}} </td>
-                        <td> {{$var["discount_reason"]}} </td>
                         <td>
                             <div
 
