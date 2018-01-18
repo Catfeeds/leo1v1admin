1bd232fff4c19b3ff90f9e176914fa75e01b538c
ricky <ricky>
2018-01-18 10:01:50 +0800

    f

--- app/Http/Controllers/company_wx.php
+++ app/Http/Controllers/company_wx.php
@@ -105,7 +105,7 @@ class company_wx extends Controller
     }
 
     public function get_approve() { // 获取审批数据
-        //dd('只用于测试');
+        dd('只用于测试');
 
         $config = Config::get_config("company_wx");
         if (!$config) {
