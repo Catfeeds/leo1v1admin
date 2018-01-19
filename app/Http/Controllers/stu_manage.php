adfdbb570b1c4f1b2274a921edddea433be0f012
jack <jack@leodeu.com>
2018-01-19 11:27:42 +0800

    fix

--- app/Http/Controllers/stu_manage.php
+++ app/Http/Controllers/stu_manage.php
@@ -1643,10 +1643,18 @@ class stu_manage extends Controller
                     $item['stu_point_performance'].=PHP_EOL."总体评价:".$str;
                 }
                 $item['stu_intro']="";
+                if(empty($item["stu_comment"])){
+                    $item["stu_comment"]="—";
+                }
+                if(empty($item["stu_score"])){
+                    $item["stu_score"]="—";
+                }
+
+
 
 
             }
-            $cw_num=$pre_num=0;
+            $tea_comment=$all_num=0;
             foreach($list as $val){
                 if(!isset($subject_arr[$val["subject"]])){
                     $subject_arr[$val["subject"]]=$val["subject"];
@@ -1654,17 +1662,41 @@ class stu_manage extends Controller
                 if(!isset($grade_arr[$val["grade"]])){
                     $grade_arr[$val["grade"]]=$val["grade"];
                 }               
+                if($val["confirm_flag"]<2){
+                    $all_num++;
+                    $val['stu_intro']   = json_decode($val['stu_performance'],true);
+                    $val['stu_point_performance']='';
+                    if(isset($val['stu_intro']['point_note_list']) && is_array($val['stu_intro']['point_note_list'])){
+                        foreach(@$val['stu_intro']['point_note_list'] as $val){
+                            $val['stu_point_performance'].=$val['point_name'].":".$val['point_stu_desc']."。";
+                        }
+                    }
+                    if(isset($val['stu_intro']['stu_comment']) && $val['stu_intro']['stu_comment']!=''){
+                        if(is_array($val['stu_intro']['stu_comment'])){
+                            $str = json_encode($val['stu_intro']['stu_comment']);
+                            $str = $this->get_test_lesson_comment_str($str);
+                        }else{
+                            $str = $val['stu_intro']['stu_comment'];
+                        }
+                        //   $str = $this->get_test_lesson_comment_str($str);
+                        $val['stu_point_performance'].=PHP_EOL."总体评价:".$str;
+                    }
+                    $comment = trim($val['stu_point_performance'],"\"");
+                    if(!empty($comment)){
+                        $tea_comment++;
+                    }
+
+
+                }
 
 
             }
-            $pre_rate = $cw_num==0?0:round($pre_num/$cw_num*100,2);
+            $record_rate = $all_num==0?0:round($tea_comment/$all_num*100,2);
             return $this->pageView(__METHOD__,$ret_info,[
-                "pre_rate"=>$pre_rate,
+                "record_rate"=>$record_rate,
                 "subject_list"=>$subject_arr,
                 "grade_list"=>$grade_arr,
             ]);
-
-            dd($ret_info);
 
 
 
