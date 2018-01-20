cf204f3f1566bcf7e9d44b4a37072f9e6b0ab135
gupeigen <gupeigen@leoedu.com>
2018-01-20 14:14:27 +0800

    tag

--- app/Http/Controllers/resource.php
+++ app/Http/Controllers/resource.php
@@ -130,7 +130,7 @@ class resource extends Controller
         }
 
         return $this->pageView( __METHOD__,$ret_info,[
-            '_publish_version'    => 20180116171249,
+            '_publish_version'    => 20180116171449,
             'tag_info'      => $tag_arr,
             'subject'       => json_encode($sub_grade_info['subject']),
             'grade'         => json_encode($sub_grade_info['grade']),
