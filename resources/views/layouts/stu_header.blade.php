74fe95e44c5a15a2a93eb5e2a199a9d13f969cf7
james <james@leoedu.com>
2018-01-20 14:23:24 +0800

    s

--- resources/views/layouts/stu_header.blade.php
+++ resources/views/layouts/stu_header.blade.php
@@ -126,7 +126,8 @@
                 </div>
             </nav>
         </header>
-        <div class="wrapper row-offcanvas row-offcanvas-left">
+        <div class=" row-offcanvas row-offcanvas-left">
+            <!-- <div class="wrapper row-offcanvas row-offcanvas-left"> -->
             <!-- Left side column. contains the logo and sidebar -->
             <aside class="left-side sidebar-offcanvas">
                 <!-- sidebar: style can be found in sidebar.less -->
