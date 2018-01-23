5b0374f4bfa7c54be72b0632bbba863347c7233a
xcwen <xcwenn@qq.com>
2018-01-23 10:26:48 +0800

    ifx

---
+++ enum_config/config_seller_student_exchange_type.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+    "name" => "例子流转类型" ,
+    array(1,"system_set_adminid_for_call","系统拨打分配"),
+    array(2,"system_free_for_call","系统释放到可拨打"),
+    array(3,"called_to_self","拨通例子入库"),
+    array(101,"admin_set_adminid","管理员分配"),
+    array(102,"admin_free","管理员释放到可分配"),
+);
