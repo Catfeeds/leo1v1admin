ee3cb7a9387a7a4f09628593d82e1f5287caa217
abner <abner@leoedu.com>
2018-01-24 09:25:01 +0800

    f

--- app/Models/t_test_lesson_subject.php
+++ app/Models/t_test_lesson_subject.php
@@ -1394,13 +1394,15 @@ class t_test_lesson_subject extends \App\Models\Zgen\z_t_test_lesson_subject
         $where_arr = [
             'ss.seller_resource_type = 0',
             's.origin_level IN (0,1,2,3,4)',
+            's.is_test_user = 0',
+            "ok.key1 = '今日头条'"
         ];
         $this->where_arr_add_time_range($where_arr, 'ss.add_time', $start_time, $end_time);
         $sql = $this->gen_sql_new(
             'select ss.phone from %s t '.
             'left join %s ss on ss.userid = t.userid '.
             'left join %s s on ss.userid = s.userid '.
-            "left join %s ok on ok.value = s.origin and ok.key1 = '今日头条' ".
+            "left join %s ok on ok.value = s.origin ".
             'where %s',
             self::DB_TABLE_NAME,
             t_seller_student_new::DB_TABLE_NAME,
