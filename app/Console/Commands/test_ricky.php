3ecfad162527894eaa2940b4312e878faf54f48a
ricky <ricky>
2018-01-25 12:50:04 +0800

    f

--- app/Console/Commands/test_ricky.php
+++ app/Console/Commands/test_ricky.php
@@ -59,18 +59,20 @@ class test_ricky extends Command
             echo $item."月".PHP_EOL;
             $info = $task->t_teacher_feedback_list->get_90_list($start_time, $end_time);
             foreach($info as $val) {
-                if (!($val["teacherid"] && $val["lessonid"])) continue;
-                echo $task->cache_get_teacher_nick($val["teacherid"]).",";
+                // if (!($val["teacherid"] && $val["lessonid"])) continue;
+                // echo $task->cache_get_teacher_nick($val["teacherid"]).",";
                 $lesson = $task->t_teacher_feedback_list->get_lesson_list($val["teacherid"], $val["lessonid"]);
                 
                 $userid = $lesson["userid"];
-                echo $task->cache_get_student_nick($userid).",";
-                echo $val["lessonid"].",";
-                echo $task->cache_get_assistant_nick($lesson["assistantid"]).",";
-                echo date("Y-m-d H:i:s", $lesson["lesson_start"]).",";
+                // echo $task->cache_get_student_nick($userid).",";
+                // echo $val["lessonid"].",";
+                // echo $task->cache_get_assistant_nick($lesson["assistantid"]).",";
+                // echo date("Y-m-d H:i:s", $lesson["lesson_start"]).",";
+                echo $userid;
                 $order = $task->t_teacher_feedback_list->get_order_list($userid);
                 echo date("Y-m-d H:i:s", $order).PHP_EOL;
             }
+            exit;
         }
 
         
