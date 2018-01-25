d093ac42e6f63756f09be5f7a3c704b94606e480
james <james@leoedu.com>
2018-01-25 15:20:59 +0800

    s

--- database/migrations/2018_01_25_135328_marker_poster_reset.php
+++ database/migrations/2018_01_25_135328_marker_poster_reset.php
@@ -14,25 +14,25 @@ class MarkerPosterReset extends Migration
     {
         //
         Schema::dropIfExists('t_personality_poster');
-        Schema::create('db_tool.t_personality_poster', function(Blueprint $table) {
-            t_comment($table,"市场部个性海报");
-            t_field($table->increments("id"), "");
-            t_field($table->integer("uid"), "分享人id");
-            t_field($table->integer("clickNum"), "家长点击次数");
-            t_field($table->integer("stuNum"), "学生数量");
-        });
+        // Schema::create('db_tool.t_personality_poster', function(Blueprint $table) {
+        //     t_comment($table,"市场部个性海报");
+        //     t_field($table->increments("id"), "");
+        //     t_field($table->integer("uid"), "分享人id");
+        //     t_field($table->integer("clickNum"), "家长点击次数");
+        //     t_field($table->integer("stuNum"), "学生数量");
+        // });
 
         Schema::dropIfExists('t_poster_share_log');
-        Schema::create('db_tool.t_poster_share_log', function(Blueprint $table) {
-            t_comment($table,"海报分享报名的学生链接");
-            t_field($table->integer("poster_id"), "海报id");
-            t_field($table->integer("uid"), "分享人id");
-            t_field($table->string("phone",100), "学生号码");
-            t_field($table->integer("studentid"), "学生id");
-            t_field($table->integer("add_time"), "添加时间");
-            $table->index('uid', 'uid');
-            $table->index('phone', 'phone');
-        });
+        // Schema::create('db_tool.t_poster_share_log', function(Blueprint $table) {
+        //     t_comment($table,"海报分享报名的学生链接");
+        //     t_field($table->integer("poster_id"), "海报id");
+        //     t_field($table->integer("uid"), "分享人id");
+        //     t_field($table->string("phone",100), "学生号码");
+        //     t_field($table->integer("studentid"), "学生id");
+        //     t_field($table->integer("add_time"), "添加时间");
+        //     $table->index('uid', 'uid');
+        //     $table->index('phone', 'phone');
+        // });
 
     }
 
