e52212e951eb3f323ce6b6aa52a6059e904751f2
jack <jack@leodeu.com>
2018-01-26 15:26:39 +0800

    fix

--- app/Http/Controllers/ajax_deal3.php
+++ app/Http/Controllers/ajax_deal3.php
@@ -277,7 +277,7 @@ class ajax_deal3 extends Controller
                 "revisit_reword_per"    =>$revisit_reword_per*100,
             ];
 
-        }elseif($type==3){
+        }elseif($type==4){
             list($performance_cr_new_list,$performance_cr_renew_list,$performance_cc_tran_list)= $this->get_ass_order_list_performance($start_time,$end_time);
             $update_arr=[
                "performance_cc_tran_num"  =>@$performance_cc_tran_list["performance_cc_tran_num"],
@@ -288,7 +288,7 @@ class ajax_deal3 extends Controller
                 "performance_cr_new_money" =>@$performance_cr_new_list[$adminid]["performance_cr_new_money"]
             ];
 
-        }elseif($type==4){
+        }elseif($type==3){
             list($first_week,$last_week,$n) = $this->get_seller_week_info($start_time, $end_time);//销售月拆解
             $registered_student_num=$this->get_register_student_list($first_week,$n);//销售月助教在册学生总数获取
             $seller_month_lesson_count = $this->t_manager_info->get_assistant_lesson_count_info($first_week,$last_week+7*86400,$assistantid);//销售月总课时
