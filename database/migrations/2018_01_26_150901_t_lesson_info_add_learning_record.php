f9abc0b9a4dee711f33c5abf2fd08af5fc3713a7
adrian <adrian@leoedu.com>
2018-01-26 15:20:43 +0800

    fix

---
+++ database/migrations/2018_01_26_150901_t_lesson_info_add_learning_record.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class TLessonInfoAddLearningRecord extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('db_weiyi.t_lesson_info', function( Blueprint $table)
+        {
+            t_field($table->integer("stu_check_video_time"),"学生查看视频回访时间");
+            t_field($table->integer("stu_check_performance_time"),"学生查看反馈时间");
+            t_field($table->integer("stu_check_homework_time"),"学生查看作业时间");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
