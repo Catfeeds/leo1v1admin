19c5a3fb51b5586e218e1181ddfa39ba9c02041d
james <james@leoedu.com>
2018-01-27 19:43:44 +0800

    s

--- app/Jobs/sendMsgForMarketTmp.php
+++ app/Jobs/sendMsgForMarketTmp.php
@@ -29,7 +29,6 @@ class sendMsgForMarketTmp extends Job implements ShouldQueue
     public function handle()
     {
         //
-                //
 
         /**
            9MXYC2KhG9bsIVl16cJgXFVsI35hIqffpSlSJFYckRU
@@ -45,57 +44,39 @@ class sendMsgForMarketTmp extends Job implements ShouldQueue
         $t_parent_info = new  \App\Models\t_parent_info();
         $t_parent_send_mgs_log  = new \App\Models\t_parent_send_mgs_log();
         $t_lesson_info_b3   = new \App\Models\t_lesson_info_b3();
-        // $send_list = $t_parent_info->get_stu();
+        $send_list = $t_parent_info->get_stu();
 
         $template_id = "9MXYC2KhG9bsIVl16cJgXFVsI35hIqffpSlSJFYckRU";
-        $send_list = [
-            [
-                "parentid" => 99999,
-                "wx_openid" => 'orwGAs_IqKFcTuZcU1xwuEtV3Kek',
-                "nick"      => "James",
-                "userid" => 1
-            ],
-            [
-                "parentid" => 99999,
-                "wx_openid" => 'orwGAs6OXWNkItnMgG-Y2_6ZkkX4',
-                "nick"      => "James",
-                "userid" => 1
-            ],
-        ];
 
 
         if(count($send_list)<7000){
             foreach($send_list as $i=> $item){
-                // $checkNeedSend = $t_lesson_info_b3->checkNeedSend($item['userid']);
-                // $checkNeedSend = 1; //测试
-                // if($checkNeedSend == 1){
+                $checkNeedSend = $t_lesson_info_b3->checkNeedSend($item['userid']);
+                if($checkNeedSend == 1){
 
 
-                $data = [
-                    "first"    => '99%的孩子都在学数学思维，到底为什么？特邀新东方数学思维名师来揭密！',
-                    "keyword1" => '1小时培养孩子的思维力',
-                    "keyword2" => '点击，识别二维码参与课程',
-                    "keyword3" => date('Y年m月d日'),
-                ];
-                $url = "https://mp.weixin.qq.com/s/Kyy2bgMpjlOMtqOpQBZb-Q";
-                $data['remark'] = '';
-                \App\Helper\Utils::send_wx_to_parent($item['wx_openid'] ,$template_id,$data,$url);
-                $t_parent_send_mgs_log->row_insert([
-                    "parentid"     => $item['parentid'],
-                    "create_time"  => time(),
-                    "is_send_flag" => 9 // 市场活动推送模板消息
-                ]);
-                // }else{
-                //     unset($send_list[$i]);
-                // }
+                // $data = [
+                //     "first"    => '99%的孩子都在学数学思维，到底为什么？特邀新东方数学思维名师来揭密！',
+                //     "keyword1" => '1小时培养孩子的思维力',
+                //     "keyword2" => '点击，识别二维码参与课程',
+                //     "keyword3" => date('Y年m月d日'),
+                // ];
+                // $url = "https://mp.weixin.qq.com/s/Kyy2bgMpjlOMtqOpQBZb-Q";
+                // $data['remark'] = '';
+                // \App\Helper\Utils::send_wx_to_parent($item['wx_openid'] ,$template_id,$data,$url);
+                // $t_parent_send_mgs_log->row_insert([
+                //     "parentid"     => $item['parentid'],
+                //     "create_time"  => time(),
+                //     "is_send_flag" => 9 // 市场活动推送模板消息
+                // ]);
+                }else{
+                    unset($send_list[$i]);
+                }
 
             }
-
-            // echo count($send_list);
+            echo count($send_list);
         }
     }
-
-
 }
 // $teacher_list = [
 //     [
@@ -111,5 +92,20 @@ class sendMsgForMarketTmp extends Job implements ShouldQueue
 //         "nick"      => "侯勇",
 //         "teacher_money_type" => 7,
 //         "teacher_type" => 1
+//     ],
+// ];
+
+// $send_list = [
+//     [
+//         "parentid" => 99999,
+//         "wx_openid" => 'orwGAs_IqKFcTuZcU1xwuEtV3Kek',
+//         "nick"      => "James",
+//         "userid" => 1
+//     ],
+//     [
+//         "parentid" => 99999,
+//         "wx_openid" => 'orwGAs6OXWNkItnMgG-Y2_6ZkkX4',
+//         "nick"      => "James",
+//         "userid" => 1
 //     ],
 // ];
