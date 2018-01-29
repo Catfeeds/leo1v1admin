13eee0f05b46dcafa96ac2092d60aa02d1527bdd
abner <abner@leoedu.com>
2018-01-29 13:43:23 +0800

    f

--- app/Console/Commands/seller_student_system_free.php
+++ app/Console/Commands/seller_student_system_free.php
@@ -69,7 +69,7 @@ class seller_student_system_free extends cmd_base
                     $opt_type=0;
                     $account="系统分配-回收例子";
                     $this->task->t_seller_student_new->set_admin_id_ex( $userid_list, $opt_adminid, 0,$account);
-                    print_r($print_arr);
+                    // print_r($print_arr);
                     if($check_hold_flag)
                         $this->task->t_seller_student_system_assign_log->update_check_flag($userid,$admin_revisiterid,$check_hold_flag);
                 }
@@ -97,7 +97,7 @@ class seller_student_system_free extends cmd_base
                     $account="系统分配-回收例子";
                     //echo "free $userid\n";
                     $this->task->t_seller_student_new->set_admin_id_ex( $userid_list, $opt_adminid, 0,$account);
-                    print_r($print_arr);
+                    // print_r($print_arr);
                     // $check_hold_flag = true;
                     // $this->task->t_seller_student_system_assign_log->update_check_flag($userid,$admin_revisiterid);
                 }
