3f447d07542eaf97e534eeda1279966e01b32de2
xcwen <xcwenn@qq.com>
2018-01-29 17:25:08 +0800

    ifx

---
+++ resources/views/tongji2/tongji_sys_assign_call_info.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.app')
+@section('content')
+
+    <section class="content ">
+
+        <div>
+            <div class="row  row-query-list" >
+                <div class="col-xs-12 col-md-5"  data-title="时间段">
+                    <div  id="id_date_range" >
+                    </div>
+                </div>
+
+
+                <div class="col-xs-6 col-md-2">
+                    <div class="input-group " >
+                        <span >拨打者</span>
+                        <input type="text" value=""  class="opt-change"  id="id_adminid"  placeholder=""  />
+                    </div>
+                </div>
+                <div class="col-xs-6 col-md-2">
+                    <div class="input-group " >
+                        <span >学生</span>
+                        <input type="text" value=""  class="opt-change"  id="id_userid"  placeholder=""  />
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr/>
+        <table     class="common-table"  >
+            <thead>
+                <tr>
+                    <td>字段1 </td>
+                    <td> 操作  </td>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ( $table_data_list as $var )
+                    <tr>
+                        <td>{{@$var[""]}} </td>
+                        <td>
+                            <div
+                                {!!  \App\Helper\Utils::gen_jquery_data($var )  !!}
+                            >
+                                <a class="fa fa-edit opt-edit"  title="编辑"> </a>
+                                <a class="fa fa-times opt-del" title="删除"> </a>
+
+                            </div>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+        @include("layouts.page")
+    </section>
+
+@endsection
