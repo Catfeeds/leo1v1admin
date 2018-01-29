7338493d1158724cb6b40c4e0c7fab33cd8b98e8
xcwen <xcwenn@qq.com>
2018-01-29 18:51:30 +0800

    fix

--- app/Models/t_seller_student_system_assign_log.php
+++ app/Models/t_seller_student_system_assign_log.php
@@ -85,19 +85,23 @@ class t_seller_student_system_assign_log extends \App\Models\Zgen\z_t_seller_stu
         return $this->main_update($sql);
     }
 
-    public function  get_list($page_info, $start_time, $end_time, $adminid,$userid )
+    public function  get_list($page_info, $order_by_str,  $start_time, $end_time, $adminid,$userid, $called_flag )
     {
         $where_arr=[
             ["userid=%u", $userid, -1 ],
             ["adminid=%u", $adminid, -1 ],
+            ["called_flag=%u", $called_flag, -1 ],
         ];
 
         $this->where_arr_add_time_range($where_arr, "logtime", $start_time, $end_time);
         $sql=$this->gen_sql_new(
-            "select *  "
-            ." from  %s "
-            ." where  %s ",
+            "select g.*, n.phone  "
+            ." from  %s g "
+            ." join  %s n on n.userid=g.userid "
+            ." where  %s "
+            . " $order_by_str ",
             self::DB_TABLE_NAME,
+            t_seller_student_new::DB_TABLE_NAME,
             $where_arr);
 
         return $this->main_get_list_by_page($sql,$page_info);
