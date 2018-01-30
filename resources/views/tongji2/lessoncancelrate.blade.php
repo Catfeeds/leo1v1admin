7ed6a463f78d426fd0439edc8934cc5cf5a6f909
james <james@leoedu.com>
2018-01-30 16:13:50 +0800

    s

--- resources/views/tongji2/lessoncancelrate.blade.php
+++ resources/views/tongji2/lessoncancelrate.blade.php
@@ -6,6 +6,7 @@
     <script type="text/javascript" src="/js/qiniu/qiniu.js"></script>
     <script type="text/javascript" src="/js/qiniu/highlight/highlight.js"></script>
     <script type="text/javascript" src="/js/jquery.md5.js"></script>
+    <script src="http://cdn.hcharts.cn/highcharts/highcharts.js"></script>
     <script type="text/javascript" >
      var dateArr= <?php  echo json_encode($dateArr); ?> ;
      var rateArr= <?php  echo json_encode($rateArr); ?> ;
@@ -22,43 +23,6 @@
             </div>
         </div>
         <hr/>
-        <table     class="common-table"  >
-            <thead>
-                <tr>
-                    <td>姓名</td>
-                    <td>分享链接打开次数</td>
-                    <td>注册新用户数量</td>
-                    <td> 操作  </td>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ( $table_data_list as $var )
-                    <tr>
-                        <td>{{@$var[""]}} </td>
-                        <td>{{@$var[""]}} </td>
-                        <td>{{@$var[""]}} </td>
-                        <td>
-                            <div
-                                {!!  \App\Helper\Utils::gen_jquery_data($var )  !!}
-                            >
-                            </div>
-                            <a class="showDetail">详情</a>
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-        @include("layouts.page")
-        <div style="display:none;" >
-            <div id="id_assign_log">
-                <table   class="table table-bordered "   >
-                    <tr>  <th> 学生id <th>电话 <th>注册时间   </tr>
-                        <tbody class="data-body">
-                        </tbody>
-                </table>
-            </div>
-        </div>
-
         <div id="container" style="min-width:400px;height:400px"></div>
     </section>
 
