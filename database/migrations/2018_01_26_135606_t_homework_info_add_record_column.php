fb67099df28ce323b4529ddac77d0e6b196254d9
gupeigen <gupeigen@leoedu.com>
2018-01-30 17:59:04 +0800

    teacher

--- database/migrations/2018_01_26_135606_t_homework_info_add_record_column.php
+++ database/migrations/2018_01_26_135606_t_homework_info_add_record_column.php
@@ -14,8 +14,8 @@ class THomeworkInfoAddRecordColumn extends Migration
     {
         Schema::table('db_weiyi.t_homework_info', function( Blueprint $table)
         {
-            t_field($table->integer("download_time"),"学生第一次下载作业时间");
-            t_field($table->integer("stu_check_time"),"学生第一次查看批改后作业时间");
+            // t_field($table->integer("download_time"),"学生第一次下载作业时间");
+            // t_field($table->integer("stu_check_time"),"学生第一次查看批改后作业时间");
         });
     }
 
