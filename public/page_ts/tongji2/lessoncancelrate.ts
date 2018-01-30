cfbd3786706acbc4513403277ce88c1efa816018
james <james@leoedu.com>
2018-01-30 16:43:37 +0800

    s

--- public/page_ts/tongji2/lessoncancelrate.ts
+++ public/page_ts/tongji2/lessoncancelrate.ts
@@ -13,58 +13,53 @@ function load_data(){
     });
 }
 $(function(){
-
     $('#id_date_range').select_date_range({
         'date_type' : g_args.date_type,
         'opt_date_type' : g_args.opt_date_type,
         'start_time'    : g_args.start_time,
         'end_time'      : g_args.end_time,
-        "th_input_id" : "th_date_range",
         date_type_config : JSON.parse( g_args.date_type_config),
         onQuery :function() {
             load_data();
         }
     });
 
-
-
-
-
-
     $(window).on("load",function(){
 
+        console.log(dateArr);
         var chart = Highcharts.chart('container', {
             chart: {
                 type: 'line'
             },
             title: {
-                text: '月平均气温'
-            },
-            subtitle: {
-                text: '数据来源: WorldClimate.com'
+                text: '课次取消率'
             },
             xAxis: {
-                categories: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月']
+                // categories: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月']
+                categories: dateArr
             },
             yAxis: {
                 title: {
-                    text: '气温 (°C)'
+                    text: '取消率(%)'
                 }
             },
             plotOptions: {
                 line: {
                     dataLabels: {
-                        enabled: true          // 开启数据标签
+                        enabled: true,// 开启数据标签
+                        format : '{y}%'
                     },
-                    enableMouseTracking: false // 关闭鼠标跟踪，对应的提示框、点击事件会失效
+                    enableMouseTracking: true // 关闭鼠标跟踪，对应的提示框、点击事件会失效
                 }
             },
+            tooltip: {
+                headerFormat: '<span style="font-size:11px">{point.key}</span><br>',
+                pointFormat: '<span >取消率</span>: <b>{point.y:.2f}%</b><br/>'
+            },
+
             series: [{
-                name: '东京',
-                data: [7.0, 6.9, 9.5, 14.5, 18.4, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
-            }, {
-                name: '伦敦',
-                data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+                name: '取消率',
+                data: rateArr
             }]
         });
 
