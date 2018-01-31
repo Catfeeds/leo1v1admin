f1796dec2b420d30c70e36982e5503da70640339
james <james@leoedu.com>
2018-01-31 16:59:27 +0800

    s

--- database/migrations/2018_01_31_163450_t_lesson_info_add_new_colum.php
+++ database/migrations/2018_01_31_163450_t_lesson_info_add_new_colum.php
@@ -14,8 +14,8 @@ class TLessonInfoAddNewColum extends Migration
     {
         //
         Schema::table('db_weiyi.t_lesson_info', function(Blueprint $table) {
-            t_field($table->string("uuid_stu"), "学生PPT讲义的uuid");
-            t_field($table->string("zip_url_stu"), "学生讲义压缩包链接");
+            t_field($table->string("uuid_stu","100"), "学生PPT讲义的uuid");
+            t_field($table->string("zip_url_stu","100"), "学生讲义压缩包链接");
             t_field($table->tinyInteger("ppt_status_stu"), "学生ppt转化状态 0:未处理 1:已成功 2:失败");
             t_field($table->tinyInteger("use_ppt_stu"), "学生讲义是否是ppt 0:ppf 1:ppt");
             $table->index('uuid_stu', 'uuid_stu_ppt');
