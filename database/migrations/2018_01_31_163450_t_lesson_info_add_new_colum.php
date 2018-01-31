f4ecfe0643e3e355ccd0d33c3f4f187fa7cedc43
james <james@leoedu.com>
2018-01-31 16:56:05 +0800

    s

--- database/migrations/2018_01_31_163450_t_lesson_info_add_new_colum.php
+++ database/migrations/2018_01_31_163450_t_lesson_info_add_new_colum.php
@@ -17,6 +17,7 @@ class TLessonInfoAddNewColum extends Migration
             t_field($table->string("uuid_stu"), "学生PPT讲义的uuid");
             t_field($table->string("zip_url_stu"), "学生讲义压缩包链接");
             t_field($table->tinyInteger("ppt_status_stu"), "学生ppt转化状态 0:未处理 1:已成功 2:失败");
+            t_field($table->tinyInteger("use_ppt_stu"), "学生讲义是否是ppt 0:ppf 1:ppt");
             $table->index('uuid_stu', 'uuid_stu_ppt');
         });
 
