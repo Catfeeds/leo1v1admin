95d5353787d469fb62cd7e6731a7ac0340bac29e
tom <tom@leoedu.com>
2018-02-01 10:03:55 +0800

    f

--- app/Http/Controllers/seller_student_new.php
+++ app/Http/Controllers/seller_student_new.php
@@ -778,7 +778,6 @@ class seller_student_new extends Controller
             if($userid_new){
                 return $this->output_err("有试听课成功未回访",["userid" =>$userid_new,'adminid'=>$adminid]);
             }
-            
 
             $row_data= $this->t_seller_student_new->field_get_list($userid,"competition_call_time, competition_call_adminid, admin_revisiterid,phone");
             $competition_call_time = $row_data["competition_call_time"];
@@ -1521,11 +1520,11 @@ class seller_student_new extends Controller
             $count=$cmd->load_data($last_get_time,time());
         }
         $count = $this->t_seller_get_new_log->get_cc_end_count($adminid,strtotime(date('Y-m-d',time())),time());
-        // if($count>=6 && in_array($this->get_account(), ['陈同','徐磊','田鹏程']) && ($this->t_manager_info->field_get_value($adminid, 'get_new_flag') == 0)){
-        //     return  $this->error_view([
-        //         "当日满6次通话未满60s主动挂断电话，禁止继续抢新"
-        //     ]);
-        // }
+        if($count>=6 && ($this->t_manager_info->field_get_value($adminid, 'get_new_flag') == 0)){
+            return  $this->error_view([
+                "当日满6次通话未满60s主动挂断电话，禁止继续抢新"
+            ]);
+        }
 
         //申明 js 变量
         $this->set_filed_for_js("phone", "","string");
@@ -1553,8 +1552,8 @@ class seller_student_new extends Controller
             $limit_arr=array( [0, 6*60]);
             //$limit_arr=array( [0, 10*60 ] );
         }
-        if(date('Y-m-d',time()) == '2018-01-05'){
-            $limit_arr=array( [0, 12*60+30]);
+        if(date('Y-m-d',time()) == '2018-02-01'){
+            $limit_arr=array( [0, 14*60]);
         }
         $seller_level=$this->t_manager_info->get_seller_level($this->get_account_id() );
         $this->set_filed_for_js("seller_level",$seller_level);
