06d7288b0eb7ed6780307d83d5d75481d865fe15
tom <tom@leoedu.com>
2018-02-01 16:42:21 +0800

    f

--- app/Models/t_seller_get_new_log.php
+++ app/Models/t_seller_get_new_log.php
@@ -13,7 +13,7 @@ class t_seller_get_new_log extends \App\Models\Zgen\z_t_seller_get_new_log
         $this->where_arr_add_int_field($where_arr, 'adminid', $adminid);
         $this->where_arr_add_int_field($where_arr, 'userid', $userid);
         $sql = $this->gen_sql_new(
-            "select id,called_count,no_called_count "
+            "select id,called_count,no_called_count,cc_end "
             ."from %s "
             ."where %s limit 1"
             ,self::DB_TABLE_NAME
