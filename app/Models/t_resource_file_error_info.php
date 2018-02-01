60bc9a2051cd3e01b701476cb22059265f17d5fb
sam <sam@leoedu.com>
2018-02-01 14:26:52 +0800

    fix

---
+++ app/Models/t_resource_file_error_info.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Models;
+use App\Enums as E;
+class t_resource_file_error_info extends \App\Models\Zgen\z_t_resource_file_error_info
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
