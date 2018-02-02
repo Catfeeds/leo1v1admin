e67b83159fd4bfa66810afba84e1fe40f7677807
abner <abner@leoedu.com>
2018-02-02 13:35:19 +0800

    f

--- app/Models/t_seller_student_new_b2.php
+++ app/Models/t_seller_student_new_b2.php
@@ -101,8 +101,9 @@ class t_seller_student_new_b2 extends \App\Models\Zgen\z_t_seller_student_new
             "n.seller_student_assign_type=1", // 系统分配k
             "n.seller_resource_type=0", // 新例子
             "n.admin_revisiterid=0", // 未分配
-            'n.origin_level <= 4' //s a b c 类例子
+            's.origin_level <= 4', //s a b c 类例子
         ];
+        $where_arr[] = '(s.seller_student_status in (1,2,101,102) and n.cc_no_called_count<=2)';
         $start_time = time(NULL) -86400*30;
         $end_time = time(NULL) ;
         $this->where_arr_add_time_range($where_arr, "add_time", $start_time, $end_time);
