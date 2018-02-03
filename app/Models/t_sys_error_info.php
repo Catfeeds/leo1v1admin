35afaf7346019d61c392faf87c8c1ca1901abc65
adrian <adrian@leoedu.com>
2018-02-03 11:55:24 +0800

    fix

--- app/Models/t_sys_error_info.php
+++ app/Models/t_sys_error_info.php
@@ -10,10 +10,10 @@ class t_sys_error_info extends \App\Models\Zgen\z_t_sys_error_info
 
     public function add( $report_error_from_type, $report_error_type, $error_msg ) {
         $this->row_insert([
-            "add_time" => time(NULL),
+            "add_time"               => time(NULL),
             "report_error_from_type" => $report_error_from_type ,
-            "report_error_type" => $report_error_type ,
-            "error_msg" => $error_msg,
+            "report_error_type"      => $report_error_type ,
+            "error_msg"              => $error_msg,
         ]);
         return $this->get_last_insertid();
     }
