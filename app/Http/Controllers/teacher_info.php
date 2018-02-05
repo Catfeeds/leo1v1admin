9219ea1e401d61957d353c52c188a04b5f93220b
sam <sam@leoedu.com>
2018-02-05 15:12:43 +0800

    fix

--- app/Http/Controllers/teacher_info.php
+++ app/Http/Controllers/teacher_info.php
@@ -3562,16 +3562,19 @@ class teacher_info extends Controller
         //send wx_message
         if($ret){   
             //search 
-            //$wx_openid = $this->t_resource_file->get_teacherinfo($file_id);
+            $info = $this->t_resource_file->get_teacherinfo($file_id);
+            $wx_openid = $info['wx_openid'];
+            $file_name = $info['file_title'];
+            $teacher_nick = $info['nick'];
             $wx_openid = "oJ_4fxH0imLIImSpAEOPqZjxWtDA";
             $teacher_url = ''; //待定
             $template_id_teacher  = "rSrEhyiqVmc2_NVI8L6fBSHLSCO9CJHly1AU-ZrhK-o";  // 待办事项
 
-            $data['first']      = " 讲义报错通知";
-            $data['keyword1']   = " 请及时检查并处理讲义的报错内容";
-            $data['keyword2']   = " 21";
-            $data['keyword3']   = date('Y-m-d H:i:s');
-            $data['remark']     = "22";
+            $data['first']      = " 您好，$nick老师，您负责的讲义\“$file_name\”被老师报错，请及时查看详情并处理。";
+            $data['keyword1']   = " 讲义报错通知";
+            $data['keyword2']   = " 请及时检查并处理讲义的报错内容";
+            $data['keyword3']   = date('Y-m-d');
+            $data['remark']     = "处理报错位置：理优管理系统——教研备课后台";
 
             \App\Helper\Utils::send_teacher_msg_for_wx($wx_openid,$template_id_teacher, $data,$teacher_url);
 
