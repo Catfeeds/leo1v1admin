73732c5657cc96cde038e68f1d82bf4b2895fa15
jack <jack@leodeu.com>
2018-02-06 18:56:35 +0800

    fix

--- resources/views/user_manage_new/get_order_channel_from_orderno_info.blade.php
+++ resources/views/user_manage_new/get_order_channel_from_orderno_info.blade.php
@@ -100,9 +100,7 @@
                             <div
                                 {!!  \App\Helper\Utils::gen_jquery_data($var )  !!}
                             >
-                                <a class="fa fa-edit opt-edit"  title="编辑"> </a>
-                                <a class="fa fa-times opt-del" title="删除"> </a>
-
+                               
                             </div>
                         </td>
                     </tr>
