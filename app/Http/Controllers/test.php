54fdacb0b47bae1d1fc7e57095db0a5662151450
xcwen <xcwenn@qq.com>
2018-02-09 14:09:54 +0800

    fix

--- app/Http/Controllers/test.php
+++ app/Http/Controllers/test.php
@@ -16,11 +16,9 @@ class test extends Controller
     public function tt() {
         $flow_type = 1;
         $config=\App\Helper\Utils::json_decode_as_array($this->t_flow_config->get_json_data($flow_type));
-        dd($config);
-        $gen_node_type= function( $type ) {
-            $arr=preg_split("/ /", $type);
-            return $arr[0];
-        };
+        $node_map=$this->t_flow_config->gen_node_map($config);
+        dd($config, $node_map);
+
 
         /*
         $this->check_and_switch_tongji_domain();
