7543ecffb68d9f7cffe3f2a3c6b8c3bdc750ce5d
abner <abner@leoedu.com>
2018-02-10 16:16:34 +0800

    f

--- app/Http/Controllers/login.php
+++ app/Http/Controllers/login.php
@@ -257,7 +257,6 @@ class login extends Controller
         if($ret_row['account_role'] == 2 && $ret_row['seller_student_assign_type'] == 1){
             $start_time = strtotime(date('Y-m-d'));
             $end_time = strtotime('+ 1 day',$start_time);
-            list($start_time, $end_time)=$this->task->get_in_date_range_day(0);
             $system_assign_count = $this->t_seller_student_system_assign_log->get_cc_assign_count($ret_row['uid'],$start_time, $end_time);
             \App\Helper\Utils::logger("system_assign_count:$system_assign_count");
             if(!$system_assign_count){
