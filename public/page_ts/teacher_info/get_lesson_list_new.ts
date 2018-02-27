ad7a3ff8357a470a2127345640737c9de98aa716
sam <sam@leoedu.com>
2018-02-27 13:51:28 +0800

    fix

--- public/page_ts/teacher_info/get_lesson_list_new.ts
+++ public/page_ts/teacher_info/get_lesson_list_new.ts
@@ -1678,8 +1678,9 @@ $(function(){
         if(get_cookie("popped")=='') {
             var info = "您有"+num+"节模拟试听课需要完成。模拟试听课程通过后，您将获得20元开课红包，赶紧开始吧。(才可以接正常试听课，老师加油！)"
             BootstrapDialog.alert(info);
-
-            document.cookie = "popped=yes" + ';expires=' + 86400;
+            var date=new Date(); 
+            date.setTime(date.getTime()+86400*12); 
+            document.cookie = "popped=yes" + ';expires=' + date.toGMTString();
         }
     }
 
