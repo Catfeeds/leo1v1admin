29c1bb85d44b8d978db9f1e8988bd0b677f23872
sam <sam@leoedu.com>
2018-03-01 19:50:58 +0800

    fix

--- app/Models/t_student_score_info.php
+++ app/Models/t_student_score_info.php
@@ -430,7 +430,7 @@ left join t_resource r on r.resource_id = f.resource_id";
 // where lesson_start < 1519833600
 // group by l.userid, l.subject
 // order by lesson_start desc";
-      $sql = "select l.userid, l.teacherid ,l.subject,l.lessonid, from_unixtime(l.lesson_start ) from t_lesson_info  l
+      $sql = "select l.userid, l.teacherid ,l.subject,l.lessonid, from_unixtime(l.lesson_start ) as time from t_lesson_info  l
 where lesson_start < 1519833600  and not exists ( select 1 from t_lesson_info where l.userid=userid and l.lesson_start<lesson_start)
 group by l.userid, l.subject";
        return $this->main_get_list($sql);
