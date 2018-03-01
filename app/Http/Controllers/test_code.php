92b7db23523206afc1948e37ad7dbf824492b4cc
adrian <adrian@leoedu.com>
2018-03-01 17:44:51 +0800

    fix

--- app/Http/Controllers/test_code.php
+++ app/Http/Controllers/test_code.php
@@ -501,7 +501,7 @@ class test_code extends Controller
     }
 
     public function test_money(){
-        $this->check_and_switch_tongji_domain();
+        $this->switch_tongji_database();
         $year  = $this->get_in_int_val("year",2017);
         $month = $this->get_in_int_val("month",1);
 
