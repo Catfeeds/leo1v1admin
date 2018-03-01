34b7afc5e6cbac85d15b4a883d9768857b335541
tom <tom@leoedu.com>
2018-03-01 11:26:55 +0800

    f

--- resources/views/seller_student_new/seller_student_list.blade.php
+++ resources/views/seller_student_new/seller_student_list.blade.php
@@ -572,16 +572,13 @@
                             <font color="green">课后回访:{{$var["last_revisit_time"]}}</font>
                             <br/>
                         @elseif($var["suc_no_call_flag"]==2)
-                            {{$var["last_succ_test_lessonid"]}}
-                            <font color="red">试听成功未回访[未拨打]</font>
+                            <font color="red">试听成功未回访[未拨打]{{$var["last_succ_test_lessonid"]}}</font>
                             <br/>
                         @elseif($var["suc_no_call_flag"]==3)
-                            {{$var["last_succ_test_lessonid"]}}
-                            <font color="red">试听成功未回访[未编辑]</font>
+                            <font color="red">试听成功未回访[未编辑]{{$var["last_succ_test_lessonid"]}}</font>
                             <br/>
                         @elseif($var["suc_no_call_flag"]==4)
-                            {{$var["last_succ_test_lessonid"]}}
-                            <font color="red">试听成功未回访[未拨打+未编辑]</font>
+                            <font color="red">试听成功未回访[未拨打+未编辑]{{$var["last_succ_test_lessonid"]}}</font>
                             <br/>
                         @else
                         @endif
