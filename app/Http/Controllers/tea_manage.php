ff314c3d85ececbe3d817609a18338d8a6e32c86
sam <sam@leoedu.com>
2018-03-02 11:21:48 +0800

    fix

--- app/Http/Controllers/tea_manage.php
+++ app/Http/Controllers/tea_manage.php
@@ -382,7 +382,6 @@ class tea_manage extends Controller
         }else{
             $is_group_leader_flag = 0;
         }
-        dd($ret_info);
         $ret_str  = $this->pageView(__METHOD__,$ret_info, [
             "lesson_count_all"      => $lesson_count_all,
             "lesson_count_fail_all" => $lesson_count_fail_all,
