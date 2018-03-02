6197e797c7e7f01480aa6973f375b631191b6c09
jim <jim@leoedu.com>
2018-03-02 14:37:51 +0800

    f

---
+++ app/Models/t_leave_lesson_log.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Models;
+use \App\Enums as E;
+class t_leave_lesson_log extends \App\Models\Zgen\z_t_leave_lesson_log
+{
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
