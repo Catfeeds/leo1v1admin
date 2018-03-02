4c220cd7b6ecbf9e3d815f920d2f69da77cee70b
jack <jack@leodeu.com>
2018-03-02 19:05:18 +0800

    fix

--- app/Http/Controllers/ss_deal.php
+++ app/Http/Controllers/ss_deal.php
@@ -3828,6 +3828,14 @@ class ss_deal extends Controller
 
             //分配给原来的销售
             $admin_revisiterid= $this->t_order_info-> get_last_seller_by_userid($origin_userid);
+            $del_flag = $this->t_manager_info->get_del_flag($admin_revisiterid);
+            if($del_flag==1){
+             
+                $cc_account = $this->t_manager_info->get_account($admin_revisiterid);
+                $new_account = $cc_account.".";
+                $admin_revisiterid  = $this->t_manager_info->get_id_by_account( $new_account);
+            }
+
             //$admin_revisiterid= $origin_assistantid;
 
             if ($admin_revisiterid) {
