f344dc964841c6673028e2275fae870431ad3d19
jack <jack@leodeu.com>
2018-03-06 17:23:41 +0800

    fix

--- app/Http/Controllers/common_new.php
+++ app/Http/Controllers/common_new.php
@@ -1788,7 +1788,7 @@ Bd6h4wrbbHA2XE1sq21ykja/Gqx7/IRia3zQfxGv/qEkyGOx+XALVoOlZqDwh76o
                         "jack",
                         "合同付款通知",
                         "合同付款通知",
-                        "学生:".$user_info["nick"]." 渠道:".$pay_channel.",订单号:".$orderNo,
+                        "学生:".$user_info["nick"]." 渠道:".wx_pub_qr.",订单号:".$orderNo,
                         ""
                     );
                     return "success";
