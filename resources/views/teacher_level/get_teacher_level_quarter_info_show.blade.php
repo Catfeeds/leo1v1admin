e9a1b52d3386c737261a18163bb31d3736dc8c77
jack <jack@leodeu.com>
2018-03-06 15:12:20 +0800

    fix

--- resources/views/teacher_level/get_teacher_level_quarter_info_show.blade.php
+++ resources/views/teacher_level/get_teacher_level_quarter_info_show.blade.php
@@ -8,6 +8,17 @@
 
                 <div class="col-xs-6 col-md-2">
                     <div class="input-group ">
+                        <span class="input-group-addon">季度</span>
+                        <select class="opt-change form-control " id="id_start_time" >
+                            @foreach($season_list as $k=>$v)
+                                <option value="{{$k}}">{{$v}}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>               
+
+                <div class="col-xs-6 col-md-2">
+                    <div class="input-group ">
                         <span >老师 </span>
                         <input type="text" value=""  class="opt-change"  id="id_teacherid"  placeholder="" />
                     </div>
